455a640e5699fd89bcfb7674354192fb5316032d
Rashmica Gupta <rashmica@linux.ibm.com>
2022-10-21 17:45:42 +1100

    libpldm: Convert to the kernel MCTP socket interface
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
    Change-Id: Id34c393d6ea66ebd794e79b62df29f92662172e0

--- src/requester/pldm.c
+++ src/requester/pldm.c
@@ -1,5 +1,6 @@
 #include "pldm.h"
 #include "base.h"
+#include "mctp.h"
 
 #include <errno.h>
 #include <stdlib.h>
@@ -15,21 +16,18 @@ pldm_requester_rc_t pldm_open()
 	int fd = -1;
 	int rc = -1;
 
-	fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+	fd = socket(AF_MCTP, SOCK_DGRAM, 0);
 	if (-1 == fd) {
 		return fd;
 	}
 
-	const char path[] = "\0mctp-mux";
-	struct sockaddr_un addr;
-	addr.sun_family = AF_UNIX;
-	memcpy(addr.sun_path, path, sizeof(path) - 1);
-	rc = connect(fd, (struct sockaddr *)&addr,
-		     sizeof(path) + sizeof(addr.sun_family) - 1);
-	if (-1 == rc) {
-		return PLDM_REQUESTER_OPEN_FAIL;
-	}
-	rc = write(fd, &MCTP_MSG_TYPE_PLDM, sizeof(MCTP_MSG_TYPE_PLDM));
+	struct sockaddr_mctp addr = {0};
+	addr.smctp_family = AF_MCTP;
+	addr.smctp_addr.s_addr = MCTP_ADDR_ANY;
+	addr.smctp_type = MCTP_MSG_TYPE_PLDM;
+	addr.smctp_tag = MCTP_TAG_OWNER;
+	addr.smctp_network = MCTP_NET_ANY;
+	rc = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
 	if (-1 == rc) {
 		return PLDM_REQUESTER_OPEN_FAIL;
 	}
@@ -58,7 +56,14 @@ static pldm_requester_rc_t mctp_recv(mctp_eid_t eid, int mctp_fd,
 {
 	ssize_t min_len = sizeof(eid) + sizeof(MCTP_MSG_TYPE_PLDM) +
 			  sizeof(struct pldm_msg_hdr);
-	ssize_t length = recv(mctp_fd, NULL, 0, MSG_PEEK | MSG_TRUNC);
+	struct sockaddr_mctp addr = {0};
+	addr.smctp_family = AF_MCTP;
+	addr.smctp_addr.s_addr = eid;
+	addr.smctp_type = MCTP_MSG_TYPE_PLDM;
+	addr.smctp_tag = MCTP_TAG_OWNER;
+	socklen_t addrlen = sizeof(addr);
+	ssize_t length = recvfrom(mctp_fd, NULL, 0, MSG_PEEK | MSG_TRUNC,
+				  (struct sockaddr *)&addr, &addrlen);
 	if (length <= 0) {
 		return PLDM_REQUESTER_RECV_FAIL;
 	} else if (length < min_len) {
@@ -84,11 +89,6 @@ static pldm_requester_rc_t mctp_recv(mctp_eid_t eid, int mctp_fd,
 		if (length != bytes) {
 			free(*pldm_resp_msg);
 			return PLDM_REQUESTER_INVALID_RECV_LEN;
-		}
-		if ((mctp_prefix[0] != eid) ||
-		    (mctp_prefix[1] != MCTP_MSG_TYPE_PLDM)) {
-			free(*pldm_resp_msg);
-			return PLDM_REQUESTER_NOT_PLDM_MSG;
 		}
 		*resp_msg_len = pldm_len;
 		return PLDM_REQUESTER_SUCCESS;
@@ -168,19 +168,16 @@ pldm_requester_rc_t pldm_send_recv(mctp_eid_t eid, int mctp_fd,
 pldm_requester_rc_t pldm_send(mctp_eid_t eid, int mctp_fd,
 			      const uint8_t *pldm_req_msg, size_t req_msg_len)
 {
-	uint8_t hdr[2] = {eid, MCTP_MSG_TYPE_PLDM};
+	struct sockaddr_mctp addr = {0};
+	addr.smctp_family = AF_MCTP;
+	addr.smctp_addr.s_addr = eid;
+	addr.smctp_type = MCTP_MSG_TYPE_PLDM;
+	addr.smctp_tag = MCTP_TAG_OWNER;
 
-	struct iovec iov[2];
-	iov[0].iov_base = hdr;
-	iov[0].iov_len = sizeof(hdr);
-	iov[1].iov_base = (uint8_t *)pldm_req_msg;
-	iov[1].iov_len = req_msg_len;
-
-	struct msghdr msg = {0};
-	msg.msg_iov = iov;
-	msg.msg_iovlen = sizeof(iov) / sizeof(iov[0]);
-
-	ssize_t rc = sendmsg(mctp_fd, &msg, 0);
+	int rc = sendto(mctp_fd, (uint8_t *)pldm_req_msg,
+		req_msg_len, 0,
+		(struct sockaddr *)&addr,
+		sizeof(addr));
 	if (rc == -1) {
 		return PLDM_REQUESTER_SEND_FAIL;
 	}
