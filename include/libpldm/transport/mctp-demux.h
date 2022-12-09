ba5293107be3a0c0af7fbf5ad6dbea06a8eac74f
Rashmica Gupta <rashmica@linux.ibm.com>
2022-12-09 16:24:45 +1100

    requester: Add new API to support multiple transports
    We are adding in new transports for PLDM (MCTP via the kernel and
    eventually nc-si) so the requester API needs updating to support
    multiple transports, as well as not have MCTP specific details in the
    API.
    
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
    Change-Id: I06e602831f360bbd0efda53d410bfb5080b3100d

---
+++ include/libpldm/transport/mctp-demux.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#ifndef LIBPLDM_DEMUX_H
+#define LIBPLDM_DEMUX_H
+#include "../config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct pldm_transport_mctp_demux;
+
+/* Init the transport backend */
+struct pldm_transport_mctp_demux *pldm_transport_mctp_demux_init(void);
+
+/* Destroy the transport backend */
+void pldm_transport_mctp_demux_destroy(struct pldm_transport_mctp_demux *ctx);
+
+/* Get the core pldm transport struct */
+struct pldm_transport *
+pldm_transport_mctp_demux_core(struct pldm_transport_mctp_demux *ctx);
+
+#ifdef PLDM_HAS_POLL
+struct pollfd;
+/* Init pollfd for async calls */
+int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport_mctp_demux *ctx,
+					  struct pollfd *pollfd);
+#endif
+
+/* Inserts a TID-to-EID mapping into the transport's device map */
+int pldm_transport_mctp_demux_map_tid(struct pldm_transport_mctp_demux *ctx,
+				      pldm_tid_t tid, mctp_eid_t eid);
+
+/* Removes a TID-to-EID mapping from the transport's device map */
+int pldm_transport_mctp_demux_unmap_tid(struct pldm_transport_mctp_demux *ctx,
+					pldm_tid_t tid, mctp_eid_t eid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBPLDM_DEMUX_H
