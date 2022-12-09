815f1bb23e28d78cef3efe77a0ba7a1343896652
Rashmica Gupta <rashmica@linux.ibm.com>
2022-12-09 16:17:44 +1100

    requester: Add new API to support multiple transports
    We are adding in new transports for PLDM (MCTP via the kernel and
    eventually nc-si) so the requester API needs updating to support
    multiple transports, as well as not have MCTP specific details in the
    API.
    
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
    Change-Id: I06e602831f360bbd0efda53d410bfb5080b3100d

---
+++ src/transport/transport_internal.h
@@ -0,0 +1,28 @@
+#ifndef TRANSPORT_INTERNAL_H
+#define TRANSPORT_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "libpldm/requester/pldm.h"
+struct pollfd;
+
+struct pldm_transport {
+	const char *name;
+	uint8_t version;
+	pldm_requester_rc_t (*recv)(struct pldm_transport *transport,
+				    pldm_tid_t tid, void **pldm_resp_msg,
+				    size_t *resp_msg_len);
+	pldm_requester_rc_t (*send)(struct pldm_transport *transport,
+				    pldm_tid_t tid, const void *pldm_req_msg,
+				    size_t req_msg_len);
+	int (*init_pollfd)(struct pldm_transport *transport,
+			   struct pollfd *pollfd);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TRANSPORT_INTERNAL_H
