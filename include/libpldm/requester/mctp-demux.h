f57188e19d6dbdc7cb35144e4c0bb14de84f6c90
Rashmica Gupta <rashmica@linux.ibm.com>
2022-12-09 16:17:44 +1100

    requester: Add new API to support multiple transports
    We are adding in new transports for PLDM (MCTP via the kernel and
    eventually nc-si) so the requester API needs updating to support
    multiple transports, as well as not have MCTP specific details in the
    API.
    
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
    Change-Id: I06e602831f360bbd0efda53d410bfb5080b3100d

---
+++ include/libpldm/requester/mctp-demux.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#ifndef _LIBPLDM_DEMUX_H
+#define _LIBPLDM_DEMUX_H
+
+struct pldm_transport_mctp_demux;
+
+/* Init the transport backend */
+struct pldm_transport_mctp_demux *pldm_transport_mctp_demux_init(void);
+
+/* Destroy the transport backend */
+void pldm_transport_mctp_demux_destroy(struct pldm_transport_mctp_demux *ctx);
+
+/* Get the core pldm transport struct */
+struct pldm_transport *
+pldm_transport_mctp_demux_core(struct pldm_transport_mctp_demux *ctx);
+
+#ifdef PLDM_HAS_POLL
+struct pollfd;
+/* Init pollfd for async calls */
+int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport_mctp_demux *ctx,
+					  struct pollfd *pollfd);
+#endif
+
+/* Inserts a TID-to-EID mapping into the transport's device map */
+int pldm_transport_mctp_map_tid(struct pldm_transport_mctp_demux *ctx,
+				pldm_tid_t tid, mctp_eid_t eid);
+
+/* Removes a TID-to-EID mapping from the transport's device map */
+int pldm_transport_mctp_unmap_tid(struct pldm_transport_mctp_demux *ctx,
+				  pldm_tid_t tid, mctp_eid_t eid);
+
+/* temporary functions to support the transition from the old api to the new */
+
+/* The old api allows for the user to setup a MCTP socket and then use the
+ * library */
+struct pldm_transport_mctp_demux *
+pldm_transport_mctp_demux_init_with_fd(int mctp_fd);
+
+/* The old api exposes the fd to the MCTP socket so we need a way to get it */
+int pldm_transport_mctp_get_socket_fd(struct pldm_transport *transport);
+
+/* Old api uses eids, new api uses tids */
+int pldm_transport_mctp_get_tid_from_eid(struct pldm_transport_mctp_demux *ctx,
+					 pldm_tid_t *tid, mctp_eid_t eid);
+#endif //_LIBPLDM_DEMUX_H
