2461615c92da090af5c4c2ffc17971eaf9d14554
Rashmica Gupta <rashmica@linux.ibm.com>
2022-12-09 16:27:38 +1100

    add afmctp transport
    Change-Id: I97641973fb85070876c11003a9758faa707f4a46
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

---
+++ include/libpldm/transport/afmctp.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#ifndef LIBPLDM_AF_MCTP_H
+#define LIBPLDM_AF_MCTP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct pldm_transport_afmctp;
+
+/* Init the transport backend */
+struct pldm_transport_afmctp *pldm_transport_afmctp_init(void);
+
+/* Destroy the transport backend */
+void pldm_transport_afmctp_destroy(struct pldm_transport_afmctp *ctx);
+
+/* Get the core pldm transport struct */
+struct pldm_transport *
+pldm_transport_afmctp_core(struct pldm_transport_afmctp *ctx);
+
+#ifdef PLDM_HAS_POLL
+struct pollfd;
+/* Init pollfd for async calls */
+int pldm_transport_afmctp_init_pollfd(struct pldm_transport_afmctp *ctx,
+				      struct pollfd *pollfd);
+#endif
+
+/* Inserts a TID-to-EID mapping into the transport's device map */
+int pldm_transport_afmctp_map_tid(struct pldm_transport_afmctp *ctx,
+				  pldm_tid_t tid, mctp_eid_t eid);
+
+/* Removes a TID-to-EID mapping from the transport's device map */
+int pldm_transport_afmctp_unmap_tid(struct pldm_transport_afmctp *ctx,
+				    pldm_tid_t tid, mctp_eid_t eid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBPLDM_AF MCTP*/
