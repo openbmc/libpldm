96de7f98b7c337ddc4fa6d4581fb93ae5805076a
kasunath <kasunath@google.com>
2022-12-14 18:06:24 -0800

    rde: Add NegotiateRedfishParameters request API
    Added an API for encoding/decoding NegotiateRedfishParameters requests
    as per DSP0218 v1.1.2 section 10.1
    
    Signed-off-by: Kasun Athukorala <kasunath@google.com>
    Change-Id: I1c4712a9564ffffc136e4a545fdbf716b9f1bb8a

---
+++ src/rde.c
@@ -0,0 +1,83 @@
+#include "rde.h"
+
+#include "base.h"
+#include "msgbuf.h"
+
+#include <endian.h>
+#include <stdio.h>
+#include <string.h>
+
+LIBPLDM_ABI_TESTING
+int encode_rde_negotiate_redfish_parameters_req(uint8_t instance_id,
+						uint8_t concurrency_support,
+						bitfield16_t *feature_support,
+						size_t payload_length,
+						struct pldm_msg *msg)
+{
+	struct pldm_msgbuf _buf;
+	struct pldm_msgbuf *buf = &_buf;
+	int rc;
+
+	if (msg == NULL || feature_support == NULL ||
+	    concurrency_support == 0) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	struct pldm_header_info header = { 0 };
+	header.instance = instance_id;
+	header.pldm_type = PLDM_RDE;
+	header.msg_type = PLDM_REQUEST;
+	header.command = PLDM_NEGOTIATE_REDFISH_PARAMETERS;
+	rc = pack_pldm_header(&header, &(msg->hdr));
+	if (rc != PLDM_SUCCESS) {
+		return rc;
+	}
+
+	rc = pldm_msgbuf_init(buf,
+			      PLDM_RDE_NEGOTIATE_REDFISH_PARAMETERS_REQ_SIZE,
+			      msg->payload, payload_length);
+	if (rc != PLDM_SUCCESS) {
+		return rc;
+	}
+
+	pldm_msgbuf_insert_uint8(buf, concurrency_support);
+	pldm_msgbuf_insert_uint16(buf, feature_support->value);
+
+	return pldm_msgbuf_destroy_consumed(buf);
+}
+
+LIBPLDM_ABI_TESTING
+int decode_rde_negotiate_redfish_parameters_req(
+	const struct pldm_msg *msg, size_t payload_length,
+	uint8_t *mc_concurrency_support, bitfield16_t *mc_feature_support)
+{
+	struct pldm_msgbuf _buf;
+	struct pldm_msgbuf *buf = &_buf;
+	int rc;
+
+	if (msg == NULL || mc_concurrency_support == NULL ||
+	    mc_feature_support == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	rc = pldm_msgbuf_init(buf,
+			      PLDM_RDE_NEGOTIATE_REDFISH_PARAMETERS_REQ_SIZE,
+			      msg->payload, payload_length);
+	if (rc != PLDM_SUCCESS) {
+		return rc;
+	}
+
+	uint16_t feature_support;
+	pldm_msgbuf_extract(buf, mc_concurrency_support);
+	pldm_msgbuf_extract(buf, &feature_support);
+
+	if (*mc_concurrency_support == 0) {
+		fprintf(stderr,
+			"Concurrency support has to be greater than 0\n");
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	mc_feature_support->value = feature_support;
+
+	return pldm_msgbuf_destroy_consumed(buf);
+}
