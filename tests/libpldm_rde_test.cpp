0cb799460db0a1652c97dac5a04f4fc9e90068ea
kasunath <kasunath@google.com>
2022-12-14 18:06:24 -0800

    Add NegotiateRedfishParameters request support
    Added encode/decode API for NegotiateRedfishParameters request as per
    DSP0218 v1.1.1 section 11.1
    
    Signed-off-by: Kasun Athukorala <kasunath@google.com>
    Change-Id: I1c4712a9564ffffc136e4a545fdbf716b9f1bb8a

---
+++ tests/libpldm_rde_test.cpp
@@ -0,0 +1,68 @@
+#include <endian.h>
+
+#include "libpldm/pldm_rde.h"
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+TEST(NegotiateRedfishParametersTest, EncodeRequestSuccess)
+{
+	uint8_t instanceId = 11;
+	uint8_t mcConcurrencySupport = 13;
+	bitfield16_t mcFeatureSupport = { .value = 0x7389 };
+
+	std::array<
+		uint8_t,
+		sizeof(struct pldm_msg_hdr) +
+			sizeof(struct pldm_rde_negotiate_redfish_parameters_req)>
+		requestMsg{};
+	auto request = reinterpret_cast<pldm_msg *>(requestMsg.data());
+	auto req_payload =
+		reinterpret_cast<pldm_rde_negotiate_redfish_parameters_req *>(
+			request->payload);
+
+	EXPECT_EQ(encode_negotiate_redfish_parameters_req(
+			  instanceId, mcConcurrencySupport, &mcFeatureSupport,
+			  request),
+		  PLDM_SUCCESS);
+
+	EXPECT_EQ(request->hdr.instance_id, instanceId);
+	EXPECT_EQ(request->hdr.type, PLDM_RDE);
+	EXPECT_EQ(request->hdr.request, 1);
+	EXPECT_EQ(request->hdr.command, PLDM_NEGOTIATE_REDFISH_PARAMETERS);
+	EXPECT_EQ(req_payload->mc_concurrency_support, mcConcurrencySupport);
+	EXPECT_EQ(le16toh(req_payload->mc_feature_support.value),
+		  mcFeatureSupport.value);
+}
+
+TEST(NegotiateRedfishParametersTest, DecodeRequestSuccess)
+{
+	uint8_t mcConcurrencySupport = 1;
+	bitfield16_t mcFeatureSupport = { .value = 0x7389 };
+
+	std::array<
+		uint8_t,
+		sizeof(struct pldm_msg_hdr) +
+			sizeof(struct pldm_rde_negotiate_redfish_parameters_req)>
+		requestMsg{};
+	auto request = reinterpret_cast<pldm_msg *>(requestMsg.data());
+	auto req_payload =
+		reinterpret_cast<pldm_rde_negotiate_redfish_parameters_req *>(
+			request->payload);
+
+	req_payload->mc_concurrency_support = mcConcurrencySupport;
+	req_payload->mc_feature_support.value = htole16(mcFeatureSupport.value);
+
+	uint8_t decodedMcConcurrencySupport;
+	bitfield16_t decodedMcFeatureSupport;
+	EXPECT_EQ(
+		decode_negotiate_redfish_parameters_req(
+			request,
+			sizeof(struct pldm_rde_negotiate_redfish_parameters_req),
+			&decodedMcConcurrencySupport, &decodedMcFeatureSupport),
+		PLDM_SUCCESS);
+
+	EXPECT_EQ(decodedMcConcurrencySupport, mcConcurrencySupport);
+	EXPECT_EQ(decodedMcFeatureSupport.value, mcFeatureSupport.value);
+}
