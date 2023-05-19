dc3abbdbb9c972dd2f87c953c730bf05e0ac5b7c
Pavithra Barithaya <pavithra.b@ibm.com>
2023-05-19 09:24:36 -0500

    Add new oem-ibm libpldm APIs
    This commit adds few oem-ibm libpldm APIs used for
    certain IBM-specific operations.
    
    Change-Id: If28325869d4c5c797c33bc4efd41bee16777a7aa
    Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

---
+++ include/libpldm/oem/ibm/pdr_oem_ibm.h
@@ -0,0 +1,29 @@
+#ifndef PDR_OEM_IBM_H
+#define PDR_OEM_IBM_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "platform.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/** @brief method to check if the record handle is within the HostBoot range
+ *  or not
+ *  
+ *  @param[in] record_handle - record handle of the pdr
+ */
+bool isHBRange(uint32_t record_handle);
+
+/** @brief find the container ID of the contained entity
+ *
+ *  @param[in] repo - opaque pointer acting as a PDR repo handle
+ *  @param[in] entityType - entity type
+ *  @param[in] entityInstance - instance of the entity
+ */
+uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
+				uint16_t entityInstance);
+#ifdef __cplusplus
+}
+#endif
+#endif /* PDR_OEM_IBM_H */
