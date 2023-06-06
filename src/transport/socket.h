c4b9e487d7608048acc6e0529e7636bc72244a01
Rashmica Gupta <rashmica@linux.ibm.com>
2023-06-06 17:13:19 +1000

    transports: Resize socket send buffer if needed
    This was originally added in openbmc/pldm to make the socket send buffer
    big enough to send a message if the message is longer than the current
    send buffer size.
    
    When you call set_sock_opt with buffer size X, given that X is within a certain
    range, the send buffer is actually set to 2*X. get_sock_opt returns the
    2*X value, not X. So add in some helper functions.
    
    Change-Id: I8ded9357db4268cd264cf0ecfb80479223106c09
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

---
+++ src/transport/socket.h
@@ -0,0 +1,14 @@
+#ifndef LIBPLDM_SRC_TRANSPORT_SOCKET_H
+#define LIBPLDM_SRC_TRANSPORT_SOCKET_H
+
+struct pldm_socket_sndbuf {
+	int size;
+	int max_size;
+};
+
+int pldm_socket_sndbuf_init(struct pldm_socket_sndbuf *ctx);
+int pldm_socket_sndbuf_set(struct pldm_socket_sndbuf *ctx, int socket,
+			   int msg_len);
+int pldm_socket_sndbuf_get(struct pldm_socket_sndbuf *ctx, int socket);
+
+#endif // LIBPLDM_SRC_TRANSPORT_SOCKET_H
