2d650cee7e23032beb92a6cc9f7c6b896c3e391c
Rashmica Gupta <rashmica@linux.ibm.com>
2023-06-06 17:13:19 +1000

    transports: Resize socket send buffer if needed
    This was originally added in openbmc/pldm to make the socket send buffer
    big enough to send a message if the message is longer than the current
    send buffer size.
    
    When you call set_sock_opt with buffer size X, given that X is within a certain
    range, the send buffer is actually set to 2*X. get_sock_opt returns the
    2*X value, not X. So add in some helper functions.
    
    Change-Id: I8ded9357db4268cd264cf0ecfb80479223106c09
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

---
+++ src/transport/socket.c
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+int get_max_buf_size(void)
+{
+	FILE *fp;
+	int max_buf_size;
+	fp = fopen("/proc/sys/net/core/wmem_max", "r");
+	if (fp == NULL) {
+		return -1;
+	}
+
+	int rc = fscanf(fp, "%d", &max_buf_size);
+	if (rc != 1) {
+		fclose(fp);
+		return -1;
+	}
+	fclose(fp);
+	return max_buf_size;
+}
+
+int set_socket_send_buf(int socket, int max_buf_size, int curr_buf_size,
+			int msg_len)
+{
+	/* If message is bigger than the max size, don't return a failure. Set
+	 * the buffer to the max size and see what happens. We don't know how
+	 * much of the extra space the kernel actually uses so let it tell us if
+	 * there wasn't enough space */
+	if (msg_len > max_buf_size) {
+		msg_len = max_buf_size;
+	}
+	if (curr_buf_size == max_buf_size) {
+		return curr_buf_size;
+	}
+	int rc = setsockopt(socket, SOL_SOCKET, SO_SNDBUF, &(msg_len),
+			    sizeof(msg_len));
+	if (rc == -1) {
+		return -1;
+	}
+	return msg_len;
+}
+
+int get_socket_send_buf_size(int socket)
+{
+	/* size returned by getsockopt is the actual size of the buffer - twice
+	 * the size of the value used by setsockopt. So for consistency, return
+	 * half of the buffer size */
+	int buf_size;
+	socklen_t optlen = sizeof(buf_size);
+	int rc =
+		getsockopt(socket, SOL_SOCKET, SO_SNDBUF, &(buf_size), &optlen);
+	if (rc == -1) {
+		return -1;
+	}
+	return buf_size / 2;
+}
