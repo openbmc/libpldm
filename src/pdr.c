efe1b4238dae02dac8be0eed5552bcf7e3cb39dd
Pavithra Barithaya <pavithra.b@ibm.com>
2023-06-22 04:36:19 -0500

    pdr: Rename pldm_pdr_find_container_id_range_exclude() API
    The API was returning the child container id, so renamed the API as
    pldm_pdr_find_child_container_id_range_exclude().
    As per spec DSP0248 (section 11.3) all the child containedEntitys have the
    same container ids. So if we return the first child contained entity it will
    be the same for all the contained entities for that particular parent.
    
    This commit addresses the comment in
    https://gerrit.openbmc.org/c/openbmc/libpldm/+/63615/comment/439b3560_78f6dbbc/
    
    Fixes: 5dc025719dc3 (“pdr: Add pldm_pdr_find_container_id_range_exclude() API”)
    Change-Id: I811105d9ccc64e6f5ab28133e75ff63fdcc2c6bc
    Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

--- src/pdr.c
+++ src/pdr.c
@@ -374,7 +374,7 @@ static bool pldm_record_handle_in_range(uint32_t record_handle,
 }
 
 LIBPLDM_ABI_TESTING
-int pldm_pdr_find_container_id_range_exclude(
+int pldm_pdr_find_child_container_id_range_exclude(
 	const pldm_pdr *repo, uint16_t entity_type, uint16_t entity_instance,
 	uint32_t range_exclude_start_handle, uint32_t range_exclude_end_handle,
 	uint16_t *container_id)
