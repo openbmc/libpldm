openbmc/libpldm

commit 39f883259956e7b2d80581bedf471ae2ccd531d5

requester: Make pldm_open() return existing fd
Considering how this is used, it makes more sense to return the fd of
the open transport rather than error out.

Fixes: c1b66f420912 ("requester: Add new APIs to support multiple transports")
Change-Id: Ibff526a013d0f4000217d8b0b1004ff51814d820
Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

src/requester/pldm.c
@@ -31,7 +31,8 @@ pldm_requester_rc_t pldm_open(void)
 	int rc;
 
 	if (open_transport) {
-		return -1;
+		fd = pldm_transport_mctp_demux_get_socket_fd(open_transport);
+		return fd;
 	}
 
 	struct pldm_transport_mctp_demux *demux = NULL;

commit 4e1ba8a736a4272e15f8e4541858407821d6b59e

requester: Return PLDM_REQUESTER_OPEN_FAIL from pldm_open() on error
As it stood the reimplementation of pldm_open() passed back the return
value of the pldm_transport_mctp_demux_*() APIs, which don't align with
the specified behaviour of pldm_open()'s return values.

Rework the return values such that PLDM_REQUESTER_OPEN_FAIL is always
returned on error. This fixes error handling in at least
openpower-occ-control, which only tested for that value and considered
all other values as success.

Further, handle any external close(2) of the returned file descriptor.
This again caters to openpower-occ-control which issues close() in its
response handler.

Fixes: 39f883259956 ("requester: Make pldm_open() return existing fd")
Fixes: c1b66f420912 ("requester: Add new APIs to support multiple transports")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I7144f6ecf0fdfbbc3a2a418a651207c012e0db54

src/requester/pldm.c
@@ -3,6 +3,7 @@
 #include "libpldm/transport.h"
 
 #include <bits/types/struct_iovec.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,23 +29,30 @@ static struct pldm_transport_mctp_demux *open_transport;
 LIBPLDM_ABI_STABLE
 pldm_requester_rc_t pldm_open(void)
 {
-	int fd;
-	int rc;
+	int fd = PLDM_REQUESTER_OPEN_FAIL;
 
 	if (open_transport) {
 		fd = pldm_transport_mctp_demux_get_socket_fd(open_transport);
-		return fd;
+
+		/* If someone has externally issued close() on fd then we need to start again. Use
+		 * `fcntl(..., F_GETFD)` to test whether fd is valid. */
+		if (fd < 0 || fcntl(fd, F_GETFD) < 0) {
+			pldm_close();
+		}
 	}
 
-	struct pldm_transport_mctp_demux *demux = NULL;
-	rc = pldm_transport_mctp_demux_init(&demux);
-	if (rc) {
-		return rc;
+	/* We retest open_transport as it may have been set to NULL by pldm_close() above. */
+	if (!open_transport) {
+		struct pldm_transport_mctp_demux *demux = NULL;
+
+		if (pldm_transport_mctp_demux_init(&demux) < 0) {
+			return PLDM_REQUESTER_OPEN_FAIL;
+		}
+
+		open_transport = demux;
+
+		fd = pldm_transport_mctp_demux_get_socket_fd(open_transport);
 	}
-
-	fd = pldm_transport_mctp_demux_get_socket_fd(demux);
-
-	open_transport = demux;
 
 	return fd;
 }