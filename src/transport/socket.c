openbmc/libpldm

commit 04273e9f689514cabe8dbe5340a1ec024db6098c

transports: Resize socket send buffer if needed
This was originally added in openbmc/pldm to make the socket send buffer
big enough to send a message if the message is longer than the current
send buffer size.

When you call set_sock_opt with buffer size X, given that X is within a
certain range, the send buffer is actually set to 2*X. get_sock_opt
returns the 2*X value, not X. So add in some helper functions.

Change-Id: I8ded9357db4268cd264cf0ecfb80479223106c09
Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

src/transport/socket.c
@@ -1,0 +1,87 @@
+#include "socket.h"
+
+#include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int pldm_socket_sndbuf_init(struct pldm_socket_sndbuf *ctx, int socket)
+{
+	FILE *fp;
+	long max_buf_size;
+	char line[128];
+	char *endptr;
+
+	if (socket == -1) {
+		return -1;
+	}
+	ctx->socket = socket;
+
+	fp = fopen("/proc/sys/net/core/wmem_max", "r");
+	if (fp == NULL || fgets(line, sizeof(line), fp) == NULL) {
+		fclose(fp);
+		return -1;
+	}
+
+	errno = 0;
+	max_buf_size = strtol(line, &endptr, 10);
+	if (errno != 0 || endptr == line) {
+		fclose(fp);
+		return -1;
+	}
+
+	fclose(fp);
+
+	if (max_buf_size > INT_MAX) {
+		max_buf_size = INT_MAX;
+	}
+	ctx->max_size = (int)max_buf_size;
+
+	if (pldm_socket_sndbuf_get(ctx)) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int pldm_socket_sndbuf_accomodate(struct pldm_socket_sndbuf *ctx, int msg_len)
+{
+	if (msg_len < ctx->size) {
+		return 0;
+	}
+	/* If message is bigger than the max size, don't return a failure. Set
+	 * the buffer to the max size and see what happens. We don't know how
+	 * much of the extra space the kernel actually uses so let it tell us if
+	 * there wasn't enough space */
+	if (msg_len > ctx->max_size) {
+		msg_len = ctx->max_size;
+	}
+	if (ctx->size == ctx->max_size) {
+		return 0;
+	}
+	int rc = setsockopt(ctx->socket, SOL_SOCKET, SO_SNDBUF, &(msg_len),
+			    sizeof(msg_len));
+	if (rc == -1) {
+		return -1;
+	}
+	ctx->size = msg_len;
+	return 0;
+}
+
+int pldm_socket_sndbuf_get(struct pldm_socket_sndbuf *ctx)
+{
+	/* size returned by getsockopt is the actual size of the buffer - twice
+	 * the size of the value used by setsockopt. So for consistency, return
+	 * half of the buffer size */
+	int buf_size;
+	socklen_t optlen = sizeof(buf_size);
+	int rc = getsockopt(ctx->socket, SOL_SOCKET, SO_SNDBUF, &(buf_size),
+			    &optlen);
+	if (rc == -1) {
+		return -1;
+	}
+	ctx->size = buf_size / 2;
+	return 0;
+}

src/transport/af-mctp.c
@@ -4,9 +4,11 @@
 #include "container-of.h"
 #include "libpldm/pldm.h"
 #include "libpldm/transport.h"
+#include "socket.h"
 #include "transport.h"
 
 #include <errno.h>
+#include <limits.h>
 #include <linux/mctp.h>
 #include <poll.h>
 #include <stdlib.h>
@@ -21,6 +23,7 @@ struct pldm_transport_af_mctp {
 	struct pldm_transport transport;
 	int socket;
 	pldm_tid_t tid_eid_map[MCTP_MAX_NUM_EID];
+	struct pldm_socket_sndbuf socket_send_buf;
 };
 
 #define transport_to_af_mctp(ptr)                                              \
@@ -119,6 +122,12 @@ static pldm_requester_rc_t pldm_transport_af_mctp_send(struct pldm_transport *t,
 	addr.smctp_type = MCTP_MSG_TYPE_PLDM;
 	addr.smctp_tag = MCTP_TAG_OWNER;
 
+	if (req_msg_len > INT_MAX ||
+	    pldm_socket_sndbuf_accomodate(&(af_mctp->socket_send_buf),
+					  (int)req_msg_len)) {
+		return PLDM_REQUESTER_SEND_FAIL;
+	}
+
 	ssize_t rc = sendto(af_mctp->socket, pldm_req_msg, req_msg_len, 0,
 			    (struct sockaddr *)&addr, sizeof(addr));
 	if (rc == -1) {
@@ -149,6 +158,14 @@ int pldm_transport_af_mctp_init(struct pldm_transport_af_mctp **ctx)
 		free(af_mctp);
 		return -1;
 	}
+
+	if (pldm_socket_sndbuf_init(&af_mctp->socket_send_buf,
+				    af_mctp->socket)) {
+		close(af_mctp->socket);
+		free(af_mctp);
+		return -1;
+	}
+
 	*ctx = af_mctp;
 	return 0;
 }

src/transport/mctp-demux.c
@@ -4,9 +4,11 @@
 #include "container-of.h"
 #include "libpldm/pldm.h"
 #include "libpldm/transport.h"
+#include "socket.h"
 #include "transport.h"
 
 #include <errno.h>
+#include <limits.h>
 #include <poll.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,6 +26,7 @@ struct pldm_transport_mctp_demux {
 	/* In the future this probably needs to move to a tid-eid-uuid/network
 	 * id mapping for multi mctp networks */
 	pldm_tid_t tid_eid_map[MCTP_MAX_NUM_EID];
+	struct pldm_socket_sndbuf socket_send_buf;
 };
 
 #define transport_to_demux(ptr)                                                \
@@ -181,6 +184,12 @@ pldm_transport_mctp_demux_send(struct pldm_transport *t, pldm_tid_t tid,
 	msg.msg_iov = iov;
 	msg.msg_iovlen = sizeof(iov) / sizeof(iov[0]);
 
+	if (req_msg_len > INT_MAX ||
+	    pldm_socket_sndbuf_accomodate(&(demux->socket_send_buf),
+					  (int)req_msg_len)) {
+		return PLDM_REQUESTER_SEND_FAIL;
+	}
+
 	ssize_t rc = sendmsg(demux->socket, &msg, 0);
 	if (rc == -1) {
 		return PLDM_REQUESTER_SEND_FAIL;
@@ -211,6 +220,13 @@ int pldm_transport_mctp_demux_init(struct pldm_transport_mctp_demux **ctx)
 		free(demux);
 		return -1;
 	}
+
+	if (pldm_socket_sndbuf_init(&demux->socket_send_buf, demux->socket)) {
+		close(demux->socket);
+		free(demux);
+		return -1;
+	}
+
 	*ctx = demux;
 	return 0;
 }
@@ -249,6 +265,13 @@ pldm_transport_mctp_demux_init_with_fd(int mctp_fd)
 		free(demux);
 		return NULL;
 	}
+
+	if (pldm_socket_sndbuf_init(&demux->socket_send_buf, demux->socket)) {
+		close(demux->socket);
+		free(demux);
+		return NULL;
+	}
+
 	return demux;
 }
 

src/transport/meson.build
@@ -1,5 +1,6 @@
 libpldm_sources += files(
   'af-mctp.c',
   'mctp-demux.c',
+  'socket.c',
   'transport.c'
 )

src/transport/socket.h
@@ -1,0 +1,14 @@
+#ifndef LIBPLDM_SRC_TRANSPORT_SOCKET_H
+#define LIBPLDM_SRC_TRANSPORT_SOCKET_H
+
+struct pldm_socket_sndbuf {
+	int size;
+	int socket;
+	int max_size;
+};
+
+int pldm_socket_sndbuf_init(struct pldm_socket_sndbuf *ctx, int socket);
+int pldm_socket_sndbuf_accomodate(struct pldm_socket_sndbuf *ctx, int msg_len);
+int pldm_socket_sndbuf_get(struct pldm_socket_sndbuf *ctx);
+
+#endif // LIBPLDM_SRC_TRANSPORT_SOCKET_H

commit aee8046bd7f562d684bfeb5ab67c5db76993d94e

transport: Fix possible NULL ptr deref in pldm_socket_sndbuf_init()
From cppcheck:
```
src/transport/socket.c:24:10: warning: Either the condition 'fp==NULL'
is redundant or there is possible null pointer dereference: fp.
[nullPointerRedundantCheck]
  fclose(fp);
         ^
src/transport/socket.c:23:9: note: Assuming that condition 'fp==NULL' is
not redundant
 if (fp == NULL || fgets(line, sizeof(line), fp) == NULL) {
        ^
src/transport/socket.c:24:10: note: Null pointer dereference
  fclose(fp);
         ^
```

Fixes: 04273e9f6895 ("Resize socket send buffer if needed")
Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>
Change-Id: Iac053a3d3c784e3592eea027f25f005dba024850

src/transport/socket.c
@@ -20,7 +20,11 @@ int pldm_socket_sndbuf_init(struct pldm_socket_sndbuf *ctx, int socket)
 	ctx->socket = socket;
 
 	fp = fopen("/proc/sys/net/core/wmem_max", "r");
-	if (fp == NULL || fgets(line, sizeof(line), fp) == NULL) {
+	if (fp == NULL) {
+		return -1;
+	}
+
+	if (fgets(line, sizeof(line), fp) == NULL) {
 		fclose(fp);
 		return -1;
 	}