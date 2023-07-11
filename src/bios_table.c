openbmc/libpldm

commit 0088a6ae4cec3eb2e8e4dece6fb54e535111790c

bios_table: Deprecate pldm_bios_table_attr_value_entry_encode_integer()
Users should prefer
pldm_bios_table_attr_value_entry_encode_integer_check() which returns an
error code rather than preventing misbehaviour using assert().

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: If7b0290d93d96c893fe3e790ba7738ececc3d4df

src/bios_table.c
@@ -796,15 +796,10 @@ void pldm_bios_table_attr_value_entry_encode_integer(void *entry,
 						     uint8_t attr_type,
 						     uint64_t cv)
 {
-	size_t length =
-		pldm_bios_table_attr_value_entry_encode_integer_length();
-	assert(length <= entry_length);
-
-	struct pldm_bios_attr_val_table_entry *table_entry = entry;
-	table_entry->attr_handle = htole16(attr_handle);
-	table_entry->attr_type = attr_type;
-	cv = htole64(cv);
-	memcpy(table_entry->value, &cv, sizeof(uint64_t));
+	int rc = pldm_bios_table_attr_value_entry_encode_integer_check(
+		entry, entry_length, attr_handle, attr_type, cv);
+	(void)rc;
+	assert(rc == PLDM_SUCCESS);
 }
 
 LIBPLDM_ABI_STABLE
@@ -819,8 +814,11 @@ int pldm_bios_table_attr_value_entry_encode_integer_check(void *entry,
 		pldm_bios_table_attr_value_entry_encode_integer_length();
 	ATTR_TYPE_EXPECT(attr_type, PLDM_BIOS_INTEGER);
 	BUFFER_SIZE_EXPECT(entry_length, length);
-	pldm_bios_table_attr_value_entry_encode_integer(
-		entry, entry_length, attr_handle, attr_type, cv);
+	struct pldm_bios_attr_val_table_entry *table_entry = entry;
+	table_entry->attr_handle = htole16(attr_handle);
+	table_entry->attr_type = attr_type;
+	cv = htole64(cv);
+	memcpy(table_entry->value, &cv, sizeof(uint64_t));
 	return PLDM_SUCCESS;
 }
 

include/libpldm/bios_table.h
@@ -608,7 +608,9 @@ uint64_t pldm_bios_table_attr_value_entry_integer_decode_cv(
  *  @param[in] attr_type - Type of this attribute in the BIOS Attribute Value
  * Table
  *  @param[in] cv - Current Value
- *  @return pldm_completion_codes
+ *  @return PLDM_SUCCESS on success. PLDM_ERROR_INVALID_DATA if entry is NULL or attr_type is not
+ *  	    PLDM_BIOS_INTEGER. PLDM_ERROR_INVALID_LENGTH if entry_length lacks capacity to encode cv
+ *  	    in entry.
  */
 int pldm_bios_table_attr_value_entry_encode_integer_check(void *entry,
 							  size_t entry_length,

tests/libpldm_bios_table_test.cpp
@@ -727,7 +727,7 @@ TEST(AttrValTable, integerEntryEncodeTest)
     EXPECT_DEATH(pldm_bios_table_attr_value_entry_encode_integer(
                      encodeEntry.data(), encodeEntry.size() - 1, 0,
                      PLDM_BIOS_INTEGER, 10),
-                 "length <= entry_length");
+                 "rc == PLDM_SUCCESS");
 
     auto rc = pldm_bios_table_attr_value_entry_encode_integer_check(
         encodeEntry.data(), encodeEntry.size(), 0, PLDM_BIOS_INTEGER, 10);

commit 9e33be96896a8e44bcd1b7740aef7db803871ef3

bios_table: Annotate pldm_bios_table_attr_value_entry_encode_integer()
0088a6ae4cec ("bios_table: Deprecate
pldm_bios_table_attr_value_entry_encode_integer()") did the work to
deprecate the function but failed to actually mark it as such. Fix that
now.

Fixes: 0088a6ae4cec ("bios_table: Deprecate pldm_bios_table_attr_value_entry_encode_integer()")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I84e067a3b6ad8a39100774b25feac76703153e93

src/bios_table.c
@@ -823,7 +823,7 @@ size_t pldm_bios_table_attr_value_entry_encode_integer_length(void)
 	       sizeof(uint64_t);
 }
 
-LIBPLDM_ABI_STABLE
+LIBPLDM_ABI_DEPRECATED
 void pldm_bios_table_attr_value_entry_encode_integer(void *entry,
 						     size_t entry_length,
 						     uint16_t attr_handle,