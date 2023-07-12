openbmc/libpldm

commit c1b66f420912dd659a4159ebd176af18347958f4

requester: Add new APIs to support multiple transports
This patch has two goals: (1) enable consumers to send PLDM messages
over different transports and (2) do this in a way that allows us to
move towards a cleaner and more complete set of requester APIs.

The sole transport option of MCTP via the userspace mctp-demux-daemon is
being deprecated. New transports are being added: MCTP via the kernel
(AF_MCTP) and eventually NC-SI. As such, the requester APIs need
updating to support multiple transports, as well as not having MCTP
specific details in the APIs. To avoid a flag day, the current APIs
(pldm_send, etc) have been rewritten terms of the new APIs.

The current APIs operate at the transport level - they don't implement
all of the behaviour necessary for a requester. As such, the new APIs to
send/recv a message have the prefix `pldm_transport`, rather than
`pldm_requester`. Given the level that these APIs are operating at,
these only send and receive a PLDM message.  Any additional logic, such
as looking for a response with a particular instance ID, belongs at the
requester abstraction level.

Some of the missing behaviours to fully be a PLDM requester are:
assigning instance IDs, request retransmission, implementing timeouts,
and enforcing only one PLDM request to a specific TID at a time. These
things are currently implemented in pldmd, meaning any consumer other
than pldmd using the libpldm "requester" APIs doesn't get the full
functionality of a requester and has to implement these things
themselves.

We would like to eventually move these behaviours into libpldm so the
libpldm requester APIs actually implement what is required to be a PLDM
requester.

The next steps to add in a full set of requester APIs, while enabling
the use of multiple transports looks something like this:

1) add instance id APIs into libpldm.
2) convert pldmd to use libpldm instance id APIs - so all users of PLDM
   are still using the same instance id allocation method.

src/transport/transport.c
@@ -1,0 +1,130 @@
+#include "libpldm/transport.h"
+#include "base.h"
+#include "libpldm/requester/pldm.h"
+#include "transport.h"
+
+#ifdef PLDM_HAS_POLL
+#include <poll.h>
+#endif
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef PLDM_HAS_POLL
+struct pollfd {
+	int fd;	       /* file descriptor */
+	short events;  /* requested events */
+	short revents; /* returned events */
+};
+
+static inline int poll(struct pollfd *fds __attribute__((unused)),
+		       int nfds __attribute__((unused)),
+		       int timeout __attribute__((unused)))
+{
+	return 0;
+}
+#endif
+
+pldm_requester_rc_t pldm_transport_poll(struct pldm_transport *transport,
+					int timeout)
+{
+	struct pollfd pollfd;
+	int rc = 0;
+	if (!transport) {
+		return PLDM_REQUESTER_INVALID_SETUP;
+	}
+	if (!transport->init_pollfd) {
+		return PLDM_REQUESTER_SUCCESS;
+	}
+
+	transport->init_pollfd(transport, &pollfd);
+	rc = poll(&pollfd, 1, timeout);
+	if (rc < 0) {
+		return PLDM_REQUESTER_POLL_FAIL;
+	}
+
+	return PLDM_REQUESTER_SUCCESS;
+}
+
+pldm_requester_rc_t pldm_transport_send_msg(struct pldm_transport *transport,
+					    pldm_tid_t tid,
+					    const void *pldm_req_msg,
+					    size_t req_msg_len)
+{
+	if (!transport || !pldm_req_msg) {
+		return PLDM_REQUESTER_INVALID_SETUP;
+	}
+
+	if (req_msg_len < sizeof(struct pldm_msg_hdr)) {
+		return PLDM_REQUESTER_NOT_REQ_MSG;
+	}
+
+	const struct pldm_msg_hdr *hdr = pldm_req_msg;
+	if (!hdr->request) {
+		return PLDM_REQUESTER_NOT_REQ_MSG;
+	}
+
+	return transport->send(transport, tid, pldm_req_msg, req_msg_len);
+}
+
+pldm_requester_rc_t pldm_transport_recv_msg(struct pldm_transport *transport,
+					    pldm_tid_t tid,
+					    void **pldm_resp_msg,
+					    size_t *resp_msg_len)
+{
+	if (!transport || !resp_msg_len) {
+		return PLDM_REQUESTER_INVALID_SETUP;
+	}
+
+	pldm_requester_rc_t rc =
+	    transport->recv(transport, tid, pldm_resp_msg, resp_msg_len);
+	if (rc != PLDM_REQUESTER_SUCCESS) {
+		return rc;
+	}
+
+	struct pldm_msg_hdr *hdr = *pldm_resp_msg;
+	if (hdr->request || hdr->datagram) {
+		free(*pldm_resp_msg);
+		*pldm_resp_msg = NULL;
+		return PLDM_REQUESTER_NOT_RESP_MSG;
+	}
+
+	uint8_t pldm_rc = 0;
+	if (*resp_msg_len < (sizeof(struct pldm_msg_hdr) + sizeof(pldm_rc))) {
+		free(*pldm_resp_msg);
+		*pldm_resp_msg = NULL;
+		return PLDM_REQUESTER_RESP_MSG_TOO_SMALL;
+	}
+
+	return PLDM_REQUESTER_SUCCESS;
+}
+
+pldm_requester_rc_t
+pldm_transport_send_recv_msg(struct pldm_transport *transport, pldm_tid_t tid,
+			     const void *pldm_req_msg, size_t req_msg_len,
+			     void **pldm_resp_msg, size_t *resp_msg_len)
+
+{
+	if (!resp_msg_len) {
+		return PLDM_REQUESTER_INVALID_SETUP;
+	}
+
+	pldm_requester_rc_t rc =
+	    pldm_transport_send_msg(transport, tid, pldm_req_msg, req_msg_len);
+	if (rc != PLDM_REQUESTER_SUCCESS) {
+		return rc;
+	}
+
+	while (1) {
+		rc = pldm_transport_poll(transport, -1);
+		if (rc != PLDM_REQUESTER_SUCCESS) {
+			break;
+		}
+		rc = pldm_transport_recv_msg(transport, tid, pldm_resp_msg,
+					     resp_msg_len);
+		if (rc == PLDM_REQUESTER_SUCCESS) {
+			break;
+		}
+	}
+
+	return rc;
+}

include/libpldm/base.h
@@ -89,6 +89,7 @@ typedef enum {
 #define PLDM_INSTANCE_MAX 31
 #define PLDM_MAX_TYPES 64
 #define PLDM_MAX_CMDS_PER_TYPE 256
+#define PLDM_MAX_TIDS 256
 
 /* Message payload lengths */
 #define PLDM_GET_COMMANDS_REQ_BYTES 5

include/libpldm/meson.build
@@ -32,5 +32,9 @@ if get_option('requester-api').enabled()
     'requester/instance-id.h',
     'requester/pldm.h'
     )
+   libpldm_transport_headers += files(
+    'transport.h',
+    'transport/mctp-demux.h'
+   )
 endif
 

include/libpldm/requester/pldm.h
@@ -8,6 +8,7 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
+/* Delete when deleting old api */
 typedef uint8_t mctp_eid_t;
 
 typedef enum pldm_requester_error_codes {
@@ -21,8 +22,12 @@ typedef enum pldm_requester_error_codes {
 	PLDM_REQUESTER_SEND_FAIL = -7,
 	PLDM_REQUESTER_RECV_FAIL = -8,
 	PLDM_REQUESTER_INVALID_RECV_LEN = -9,
+	PLDM_REQUESTER_SETUP_FAIL = -10,
+	PLDM_REQUESTER_INVALID_SETUP = -11,
+	PLDM_REQUESTER_POLL_FAIL = -12,
 } pldm_requester_rc_t;
 
+/* ------ Old API ---- deprecated */
 /**
  * @brief Connect to the MCTP socket and provide an fd to it. The fd can be
  *        used to pass as input to other APIs below, or can be polled.

include/libpldm/transport.h
@@ -1,0 +1,117 @@
+#ifndef TRANSPORT_PLDM_H
+#define TRANSPORT_PLDM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "libpldm/base.h"
+#include "libpldm/pldm.h"
+#include <stddef.h>
+
+struct pldm_transport;
+
+/**
+ * @brief Waits for a PLDM event.
+ *
+ * @pre The pldm transport instance must be initialised; otherwise,
+ * 	PLDM_REQUESTER_INVALID_SETUP is returned. This should be called after
+ * 	pldm_transport_send_msg has been called.
+ *
+ * @return pldm_requester_rc_t (errno may be set)
+ */
+pldm_requester_rc_t pldm_transport_poll(struct pldm_transport *transport,
+					int timeout);
+
+/**
+ * @brief Asynchronously send a PLDM message. Control is immediately returned to
+ * 	  the caller.
+ *
+ * @pre The pldm transport instance must be initialised; otherwise,
+ * 	PLDM_REQUESTER_INVALID_SETUP is returned. If the transport requires a
+ * 	TID to transport specific identifier mapping, this must already be set
+ * 	up.
+ *
+ * @param[in] ctx - pldm transport instance
+ * @param[in] tid - destination PLDM TID
+ * @param[in] pldm_req_msg - caller owned pointer to PLDM request msg or async
+ *            notification. If this is NULL, PLDM_REQUESTER_INVALID_SETUP is
+ * 	      returned.
+ * @param[in] req_msg_len - size of PLDM request msg. If this is less than the
+ * 	      minimum size of a PLDM msg PLDM_REQUESTER_NOT_REQ_MSG is returned.
+ * 	      Otherwise, if this is not the correct length of the PLDM msg,
+ * 	      behaviour is undefined.
+ *
+ * @return pldm_requester_rc_t (errno may be set)
+ */
+pldm_requester_rc_t pldm_transport_send_msg(struct pldm_transport *transport,
+					    pldm_tid_t tid,
+					    const void *pldm_req_msg,
+					    size_t req_msg_len);
+
+/**
+ * @brief Asynchronously get a PLDM response message for the given TID
+ * 	  regardless of instance ID. Control is immediately returned to the
+ * 	  caller.
+ *
+ * @pre The pldm transport instance must be initialised; otherwise,
+ * 	PLDM_REQUESTER_INVALID_SETUP is returned. If the transport requires a
+ * 	TID to transport specific identifier mapping, this must already be set
+ * 	up.
+ *
+ * @param[in] ctx - pldm transport instance
+ * @param[in] tid - destination PLDM TID
+ * @param[out] pldm_resp_msg - *pldm_resp_msg will point to PLDM response msg if
+ * 	       return code is PLDM_REQUESTER_SUCCESS; otherwise, NULL. On
+ * 	       success this function allocates memory, caller to
+ * 	       free(*pldm_resp_msg).
+ * @param[out] resp_msg_len - caller owned pointer that will be made to point to
+ *             the size of the PLDM response msg. If NULL,
+ * 	       PLDM_REQUESTER_INVALID_SETUP is returned.
+ *
+ * @return pldm_requester_rc_t (errno may be set). Failure is returned if no
+ * 	   PLDM response messages are available.
+ *
+ */
+pldm_requester_rc_t pldm_transport_recv_msg(struct pldm_transport *transport,
+					    pldm_tid_t tid,
+					    void **pldm_resp_msg,
+					    size_t *resp_msg_len);
+
+/**
+ * @brief Synchronously send a PLDM request and receive the response. Control is
+ * 	  returned to the caller once the response is received.
+ *
+ * @pre The pldm transport instance must be initialised; otherwise,
+ * 	PLDM_REQUESTER_INVALID_SETUP is returned. If the transport requires a
+ * 	TID to transport specific identifier mapping, this must already be set
+ * 	up.
+ *
+ * @param[in] ctx - pldm transport instance with a registered transport
+ * @param[in] tid - destination PLDM TID
+ * @param[in] pldm_req_msg - caller owned pointer to PLDM request msg or async
+ * 	      notification. If NULL, PLDM_REQUESTER_INVALID_SETUP is returned.
+ * @param[in] req_msg_len - size of PLDM request msg. If this is less than the
+ * 	      minimum size of a PLDM msg PLDM_REQUESTER_NOT_REQ_MSG is returned.
+ * 	      Otherwise, if this is not the correct length of the PLDM msg,
+ * 	      behaviour is undefined.
+ * @param[out] pldm_resp_msg - *pldm_resp_msg will point to PLDM response msg if
+ * 	       return code is PLDM_REQUESTER_SUCCESS; otherwise, NULL. On
+ * 	       success this function allocates memory, caller to
+ * 	       free(*pldm_resp_msg).
+ * @param[out] resp_msg_len - caller owned pointer that will be made to point to
+ *             the size of the PLDM response msg. If NULL,
+ * 	       PLDM_REQUESTER_INVALID_SETUP is returned.
+ *
+ * @return pldm_requester_rc_t (errno may be set)
+ */
+pldm_requester_rc_t
+pldm_transport_send_recv_msg(struct pldm_transport *transport, pldm_tid_t tid,
+			     const void *pldm_req_msg, size_t req_msg_len,
+			     void **pldm_resp_msg, size_t *resp_msg_len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TRANSPORT_PLDM_H */

include/libpldm/transport/mctp-demux.h
@@ -1,0 +1,43 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#ifndef LIBPLDM_DEMUX_H
+#define LIBPLDM_DEMUX_H
+#include "../config.h"
+#include "libpldm/base.h"
+#include "libpldm/pldm.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct pldm_transport_mctp_demux;
+
+/* Init the transport backend */
+int pldm_transport_mctp_demux_init(struct pldm_transport_mctp_demux **ctx);
+
+/* Destroy the transport backend */
+void pldm_transport_mctp_demux_destroy(struct pldm_transport_mctp_demux *ctx);
+
+/* Get the core pldm transport struct */
+struct pldm_transport *
+pldm_transport_mctp_demux_core(struct pldm_transport_mctp_demux *ctx);
+
+#ifdef PLDM_HAS_POLL
+struct pollfd;
+/* Init pollfd for async calls */
+int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport_mctp_demux *ctx,
+					  struct pollfd *pollfd);
+#endif
+
+/* Inserts a TID-to-EID mapping into the transport's device map */
+int pldm_transport_mctp_demux_map_tid(struct pldm_transport_mctp_demux *ctx,
+				      pldm_tid_t tid, mctp_eid_t eid);
+
+/* Removes a TID-to-EID mapping from the transport's device map */
+int pldm_transport_mctp_demux_unmap_tid(struct pldm_transport_mctp_demux *ctx,
+					pldm_tid_t tid, mctp_eid_t eid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBPLDM_DEMUX_H

meson.build
@@ -18,6 +18,7 @@ subdir('src')
 
 libpldm_include_dir = ['include', 'src']
 libpldm_headers = files()
+libpldm_transport_headers = files()
 
 subdir('include/libpldm')
 
@@ -35,6 +36,11 @@ install_headers(
   subdir:'libpldm'
   )
 
+install_headers(
+  libpldm_transport_headers,
+  subdir:'libpldm/transport'
+  )
+
 libpldm_dep = declare_dependency(
   include_directories: libpldm_include_dir,
   link_with: libpldm)
@@ -44,6 +50,15 @@ import('pkgconfig').generate(
   description: 'PLDM protocol encode/decode C lib',
   version: meson.project_version(),
   libraries: libpldm)
+
+compiler = meson.get_compiler('c')
+conf = configuration_data()
+if compiler.has_header('poll.h')
+  conf.set('PLDM_HAS_POLL', 1)
+endif
+configure_file(output: 'config.h',
+  configuration: conf
+)
 
 if get_option('tests').enabled()
   subdir('tests')

src/mctp-defines.h
@@ -1,0 +1,15 @@
+#ifndef MCTP_DEFINES_H
+#define MCTP_DEFINES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MCTP_MSG_TYPE_PLDM 1
+#define MCTP_MAX_NUM_EID 256
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MCTP_DEFINES_H

commit b3c52ad25509a59ddb91f3f351457821f70c55a2

transport: pldm_transport_poll(): Don't return success on timeout
Prior, a timeout in pldm_transport_poll() would result in a
pldm_transport_recv() call being issued in pldm_transport_send_recv().
That's not the right course of action as it will translate to a blocking
recv() system call for either of the current transport implementations.
As there's no data present the recv() blocks indefinitely, violating the
timeout constraint.

To mitigate that, also return PLDM_REQUESTER_POLL_FAIL on timeout to
indicate the lack of success.

Fixes: c1b66f420912 ("requester: Add new APIs to support multiple transports")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: Id75e8dad49a0ea6081830d0825896094bff0e827

src/transport/transport.c
@@ -44,10 +44,17 @@ pldm_requester_rc_t pldm_transport_poll(struct pldm_transport *transport,
 
 	transport->init_pollfd(transport, &pollfd);
 	rc = poll(&pollfd, 1, timeout);
+	/* Invocation error */
 	if (rc < 0) {
 		return PLDM_REQUESTER_POLL_FAIL;
 	}
 
+	/* Timeout */
+	if (rc == 0) {
+		return PLDM_REQUESTER_POLL_FAIL;
+	}
+
+	/* Otherwise, the transport is ready for action */
 	return PLDM_REQUESTER_SUCCESS;
 }
 