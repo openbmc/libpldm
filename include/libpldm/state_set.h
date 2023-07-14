e31103e53d3296355862006bd6aa344a093d14b7
Pavithra Barithaya <pavithra.b@ibm.com>
2023-07-14 01:22:23 -0500

    state-set: Add the enum for Operational Fault Status state
    As per the spec DSP0249 the PLDM state set Operational Fault Status
    [Set ID: 10] can have three enumeration values defined. This commit
    adds the enum for the same.
    
    Change-Id: I37962d53c8baf48d0af7beb5ea891c0d2350f4c2
    Signed-off-by: Pavithra Barithaya <pavithra.b@ibm.com>

--- include/libpldm/state_set.h
+++ include/libpldm/state_set.h
@@ -149,11 +149,19 @@ enum pldm_state_set_availability_values {
 	PLDM_STATE_SET_AVAILABILITY_REBOOTING = 8
 };
 
-/* @brief List of states for the Operational Fault status (ID 10).
+/* @brief List of states for the Operational Stress status (ID 9).
  */
-enum pldm_state_set_operational_fault_status_values {
+enum pldm_state_set_operational_stress_status_values {
 	PLDM_STATE_SET_OPERATIONAL_FAULT_STATUS_NORMAL = 1,
 	PLDM_STATE_SET_OPERATIONAL_FAULT_STATUS_STRESSED = 2,
+};
+
+/* @brief List of states for Operational Fault status (ID 10).
+ */
+enum pldm_state_set_operational_fault_status_values {
+	PLDM_STATE_SET_OPERATIONAL_NORMAL = 1,
+	PLDM_STATE_SET_OPERATIONAL_ERROR = 2,
+	PLDM_STATE_SET_OPERATIONAL_NON_RECOVERABLE_ERROR = 3,
 };
 
 /* @brief List of states for the Operational Running Status state set (ID 11).
