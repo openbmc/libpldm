openbmc/libpldm

commit 572a39507d1ee2bb137fbcc234b00295e27eb204

pdr: Introduce pldm_pdr_add_check()
pldm_pdr_add() relied on assert() to communicate failure to the caller.
Introduce pldm_pdr_add_check() which instead returns a value indicating
success or failure. pldm_pdr_add() will be deprecated once
pldm_pdr_add_check() is stabilised.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I43542e8e7411edb32a7afa242f1ae68d6fe3ad89

src/pdr.c
@@ -39,40 +39,68 @@ uint32_t pldm_pdr_add(pldm_pdr *repo, const uint8_t *data, uint32_t size,
 		      uint32_t record_handle, bool is_remote,
 		      uint16_t terminus_handle)
 {
+	int rc = pldm_pdr_add_check(repo, data, size, is_remote,
+				    terminus_handle, &record_handle);
+	(void)rc;
+	assert(!rc);
+	return record_handle;
+}
+
+LIBPLDM_ABI_TESTING
+int pldm_pdr_add_check(pldm_pdr *repo, const uint8_t *data, uint32_t size,
+		       bool is_remote, uint16_t terminus_handle,
+		       uint32_t *record_handle)
+{
 	assert(repo != NULL);
 	assert(data != NULL);
 	assert(size != 0);
+	assert(record_handle != NULL);
+	if (!repo || !data || !size || !record_handle) {
+		return -EINVAL;
+	}
 
 	pldm_pdr_record *record = malloc(sizeof(pldm_pdr_record));
 	assert(record != NULL);
+	if (!record) {
+		return -ENOMEM;
+	}
 
-	if (record_handle) {
-		record->record_handle = record_handle;
-	} else {
-		uint32_t curr = repo->last ? repo->last->record_handle : 0;
-		assert(curr != UINT32_MAX);
-		record->record_handle = curr + 1;
+	if (data) {
+		record->data = malloc(size);
+		assert(record->data != NULL);
+		if (!record->data) {
+			free(record);
+			return -ENOMEM;
+		}
+		memcpy(record->data, data, size);
 	}
 
 	record->size = size;
 	record->is_remote = is_remote;
 	record->terminus_handle = terminus_handle;
-	if (data != NULL) {
-		record->data = malloc(size);
-		assert(record->data != NULL);
-		memcpy(record->data, data, size);
-		/* If record handle is 0, that is an indication for this API to
-		 * compute a new handle. For that reason, the computed handle
-		 * needs to be populated in the PDR header. For a case where the
-		 * caller supplied the record handle, it would exist in the
-		 * header already.
-		 */
-		if (!record_handle) {
-			struct pldm_pdr_hdr *hdr =
-				(struct pldm_pdr_hdr *)(record->data);
+
+	if (*record_handle) {
+		record->record_handle = *record_handle;
+	} else {
+		uint32_t curr = repo->last ? repo->last->record_handle : 0;
+		assert(curr != UINT32_MAX);
+		if (curr == UINT32_MAX) {
+			return -EOVERFLOW;
+		}
+		record->record_handle = curr + 1;
+
+		if (data != NULL) {
+			/* If record handle is 0, that is an indication for this API to
+			 * compute a new handle. For that reason, the computed handle
+			 * needs to be populated in the PDR header. For a case where the
+			 * caller supplied the record handle, it would exist in the
+			 * header already.
+			 */
+			struct pldm_pdr_hdr *hdr = (void *)record->data;
 			hdr->record_handle = htole32(record->record_handle);
 		}
 	}
+
 	record->next = NULL;
 
 	assert(!repo->first == !repo->last);
@@ -87,7 +115,9 @@ uint32_t pldm_pdr_add(pldm_pdr *repo, const uint8_t *data, uint32_t size,
 	repo->size += record->size;
 	++repo->record_count;
 
-	return record->record_handle;
+	*record_handle = record->record_handle;
+
+	return 0;
 }
 
 LIBPLDM_ABI_STABLE

include/libpldm/pdr.h
@@ -77,6 +77,25 @@ uint32_t pldm_pdr_add(pldm_pdr *repo, const uint8_t *data, uint32_t size,
 		      uint32_t record_handle, bool is_remote,
 		      uint16_t terminus_handle);
 
+/** @brief Add a PDR record to a PDR repository, or return an error
+ *
+ *  @param[in/out] repo - opaque pointer acting as a PDR repo handle
+ *  @param[in] data - pointer to a PDR record, pointing to a PDR definition as
+ *  per DSP0248. This data is memcpy'd.
+ *  @param[in] size - size of input PDR record in bytes
+ *  @param[in] is_remote - if true, then the PDR is not from this terminus
+ *  @param[in] terminus_handle - terminus handle of the input PDR record
+ *  @param[in,out] record_handle - record handle of input PDR record. If this is set to 0 then a
+ *  record handle is computed. The computed handle is assigned to both the PDR record and back into
+ *  record_handle for the caller to consume.
+ *
+ *  @return 0 on success, -EINVAL if the arguments are invalid, -ENOMEM if an internal memory
+ *  allocation fails, or -EOVERFLOW if a record handle could not be allocated
+ */
+int pldm_pdr_add_check(pldm_pdr *repo, const uint8_t *data, uint32_t size,
+		       bool is_remote, uint16_t terminus_handle,
+		       uint32_t *record_handle);
+
 /** @brief Get record handle of a PDR record
  *
  *  @pre repo must point to a valid object

commit 1e9c3ed6778482688ab7b654f4a45983bcd1afb8

pdr: Hoist record handle overflow test to avoid memory leak
The rework in 572a39507d1e ("pdr: Introduce pldm_pdr_add_check()") left
pldm_pdr_add_check() vulnerable to a memory leak. If the record handle
counter overflowed then the early-exit reporting the fault left the
memory pointed to by `record` allocated.

Hoist the overflow check over the dynamic allocation so that we don't
have to clean up in the error path.

Fixes: 572a39507d1e ("pdr: Introduce pldm_pdr_add_check()")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I72ba43d009a4e76aa585590909edf6c70d64b7af

src/pdr.c
@@ -54,8 +54,22 @@ int pldm_pdr_add_check(pldm_pdr *repo, const uint8_t *data, uint32_t size,
 		       bool is_remote, uint16_t terminus_handle,
 		       uint32_t *record_handle)
 {
+	uint32_t curr;
+
 	if (!repo || !data || !size || !record_handle) {
 		return -EINVAL;
+	}
+
+	if (*record_handle) {
+		curr = *record_handle;
+	} else if (repo->last) {
+		curr = repo->last->record_handle;
+		if (curr == UINT32_MAX) {
+			return -EOVERFLOW;
+		}
+		curr += 1;
+	} else {
+		curr = 1;
 	}
 
 	pldm_pdr_record *record = malloc(sizeof(pldm_pdr_record));
@@ -75,26 +89,17 @@ int pldm_pdr_add_check(pldm_pdr *repo, const uint8_t *data, uint32_t size,
 	record->size = size;
 	record->is_remote = is_remote;
 	record->terminus_handle = terminus_handle;
+	record->record_handle = curr;
 
-	if (*record_handle) {
-		record->record_handle = *record_handle;
-	} else {
-		uint32_t curr = repo->last ? repo->last->record_handle : 0;
-		if (curr == UINT32_MAX) {
-			return -EOVERFLOW;
-		}
-		record->record_handle = curr + 1;
-
-		if (data != NULL) {
-			/* If record handle is 0, that is an indication for this API to
-			 * compute a new handle. For that reason, the computed handle
-			 * needs to be populated in the PDR header. For a case where the
-			 * caller supplied the record handle, it would exist in the
-			 * header already.
-			 */
-			struct pldm_pdr_hdr *hdr = (void *)record->data;
-			hdr->record_handle = htole32(record->record_handle);
-		}
+	if (!*record_handle && data) {
+		/* If record handle is 0, that is an indication for this API to
+		 * compute a new handle. For that reason, the computed handle
+		 * needs to be populated in the PDR header. For a case where the
+		 * caller supplied the record handle, it would exist in the
+		 * header already.
+		 */
+		struct pldm_pdr_hdr *hdr = (void *)record->data;
+		hdr->record_handle = htole32(record->record_handle);
 	}
 
 	record->next = NULL;