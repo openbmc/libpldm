openbmc/libpldm

commit c821a7004e9e6a8679704ae824d8d9110ec535c5

pdr: Introduce pldm_pdr_add_fru_record_set_check()
pldm_pdr_add_fru_record_set() relied on assert() to communicate failure
to the caller. Introduce pldm_pdr_add_fru_record_set_check() which
instead returns a value indicating success or failure.
pldm_pdr_add_fru_record_set() will be deprecated once
pldm_pdr_add_fru_record_set_check() is stabilised.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: I1e09a5663895f1e09b5e40afe5db4b4864d3496e

src/pdr.c
@@ -293,13 +293,28 @@ uint32_t pldm_pdr_add_fru_record_set(pldm_pdr *repo, uint16_t terminus_handle,
 				     uint16_t container_id,
 				     uint32_t bmc_record_handle)
 {
+	int rc = pldm_pdr_add_fru_record_set_check(
+		repo, terminus_handle, fru_rsi, entity_type,
+		entity_instance_num, container_id, &bmc_record_handle);
+	(void)rc;
+	assert(!rc);
+	return bmc_record_handle;
+}
+
+LIBPLDM_ABI_TESTING
+int pldm_pdr_add_fru_record_set_check(pldm_pdr *repo, uint16_t terminus_handle,
+				      uint16_t fru_rsi, uint16_t entity_type,
+				      uint16_t entity_instance_num,
+				      uint16_t container_id,
+				      uint32_t *bmc_record_handle)
+{
 	uint32_t size = sizeof(struct pldm_pdr_hdr) +
 			sizeof(struct pldm_pdr_fru_record_set);
 	uint8_t data[size];
 
 	struct pldm_pdr_hdr *hdr = (struct pldm_pdr_hdr *)&data;
 	hdr->version = 1;
-	hdr->record_handle = bmc_record_handle;
+	hdr->record_handle = *bmc_record_handle;
 	hdr->type = PLDM_PDR_FRU_RECORD_SET;
 	hdr->record_change_num = 0;
 	hdr->length = htole16(sizeof(struct pldm_pdr_fru_record_set));
@@ -312,8 +327,8 @@ uint32_t pldm_pdr_add_fru_record_set(pldm_pdr *repo, uint16_t terminus_handle,
 	fru->entity_instance_num = htole16(entity_instance_num);
 	fru->container_id = htole16(container_id);
 
-	return pldm_pdr_add(repo, data, size, bmc_record_handle, false,
-			    terminus_handle);
+	return pldm_pdr_add_check(repo, data, size, false, terminus_handle,
+				  bmc_record_handle);
 }
 
 LIBPLDM_ABI_STABLE

include/libpldm/pdr.h
@@ -260,6 +260,29 @@ uint32_t pldm_pdr_add_fru_record_set(pldm_pdr *repo, uint16_t terminus_handle,
 				     uint16_t container_id,
 				     uint32_t bmc_record_handle);
 
+/** @brief Add a FRU record set PDR record to a PDR repository, or return an error
+ *
+ *  @param[in/out] repo - opaque pointer acting as a PDR repo handle
+ *  @param[in] terminus_handle - PLDM terminus handle of terminus owning the PDR
+ *  record
+ *  @param[in] fru_rsi - FRU record set identifier
+ *  @param[in] entity_type - entity type of FRU
+ *  @param[in] entity_instance_num - entity instance number of FRU
+ *  @param[in] container_id - container id of FRU
+ *  @param[in,out] bmc_record_handle - A pointer to the handle used to construct the next record. If
+ *  		   the value is zero on input then a new handle is automatically allocated.
+ *  		   Otherwise, the provided handle is used. If a new handle is automatically
+ *  		   allocated then the object pointed to by bmc_record_handle will contain its value
+ *  		   as output.
+ *  @return 0 on success, -EINVAL if the arguments are invalid, or -ENOMEM if an internal allocation
+ *  	    fails.
+ */
+int pldm_pdr_add_fru_record_set_check(pldm_pdr *repo, uint16_t terminus_handle,
+				      uint16_t fru_rsi, uint16_t entity_type,
+				      uint16_t entity_instance_num,
+				      uint16_t container_id,
+				      uint32_t *bmc_record_handle);
+
 /** @brief Find a FRU record set PDR by FRU record set identifier
  *
  *  @param[in] repo - opaque pointer acting as a PDR repo handle

commit b6339cfb18aacef8b5d4884c64039677cc11d30b

pdr: Validate arguments to pldm_pdr_add_fru_record_set_check()
Previously bmc_record_handle were dereferenced without validation. Fix
that by testing both pointer arguments are not NULL.

Fixes: c821a7004e9e ("pdr: Introduce pldm_pdr_add_fru_record_set_check()")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: Ie964cfe115d9f88def64092c06ccea8f26e1b7a4

src/pdr.c
@@ -299,6 +299,10 @@ int pldm_pdr_add_fru_record_set_check(pldm_pdr *repo, uint16_t terminus_handle,
 				      uint16_t container_id,
 				      uint32_t *bmc_record_handle)
 {
+	if (!repo || !bmc_record_handle) {
+		return -EINVAL;
+	}
+
 	uint32_t size = sizeof(struct pldm_pdr_hdr) +
 			sizeof(struct pldm_pdr_fru_record_set);
 	uint8_t data[size];