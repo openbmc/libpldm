openbmc/libpldm

commit 0411b712b74686a6152155a8310c77d30cb4bf2c

transport: Make APIs work for all types of messages
These transport APIs were written with the idea of being used by a
requester. However they don't need to be, so remove these checks so we
can send and receive both requests and responses.

Put the checks into the old API calls so we don't break anyone.

Change-Id: I194e22341025a00388fa7e031d2c6db05976874f
Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

src/requester/pldm.c
@@ -53,11 +53,10 @@ pldm_requester_rc_t pldm_open(void)
 /* This macro does the setup and teardown required for the old API to use the
  * new API. Since the setup/teardown logic is the same for all four send/recv
  * functions, it makes sense to only define it once. */
-#define PLDM_REQ_FN(eid, fd, fn, ...)                                            \
+#define PLDM_REQ_FN(eid, fd, fn, rc, ...)                                        \
 	do {                                                                     \
 		struct pldm_transport_mctp_demux *demux;                         \
 		bool using_open_transport = false;                               \
-		pldm_requester_rc_t rc;                                          \
 		pldm_tid_t tid = 1;                                              \
 		struct pldm_transport *ctx;                                      \
 		/* The fd can be for a socket we opened or one the consumer    \
@@ -85,15 +84,32 @@ pldm_requester_rc_t pldm_open(void)
 		if (!using_open_transport) {                                     \
 			pldm_transport_mctp_demux_destroy(demux);                \
 		}                                                                \
-		return rc;                                                       \
+		break;                                                           \
 	} while (0)
 
 LIBPLDM_ABI_STABLE
 pldm_requester_rc_t pldm_recv_any(mctp_eid_t eid, int mctp_fd,
 				  uint8_t **pldm_resp_msg, size_t *resp_msg_len)
 {
-	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_recv_msg,
+	pldm_requester_rc_t rc = 0;
+	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_recv_msg, rc,
 		    (void **)pldm_resp_msg, resp_msg_len);
+	struct pldm_msg_hdr *hdr = (struct pldm_msg_hdr *)(*pldm_resp_msg);
+	if (rc != PLDM_REQUESTER_SUCCESS) {
+		return rc;
+	}
+	if (hdr && (hdr->request || hdr->datagram)) {
+		free(*pldm_resp_msg);
+		*pldm_resp_msg = NULL;
+		return PLDM_REQUESTER_NOT_RESP_MSG;
+	}
+	uint8_t pldm_cc = 0;
+	if (*resp_msg_len < (sizeof(struct pldm_msg_hdr) + sizeof(pldm_cc))) {
+		free(*pldm_resp_msg);
+		*pldm_resp_msg = NULL;
+		return PLDM_REQUESTER_RESP_MSG_TOO_SMALL;
+	}
+	return rc;
 }
 
 LIBPLDM_ABI_STABLE
@@ -119,16 +135,38 @@ pldm_requester_rc_t pldm_send_recv(mctp_eid_t eid, int mctp_fd,
 				   size_t req_msg_len, uint8_t **pldm_resp_msg,
 				   size_t *resp_msg_len)
 {
-	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_send_recv_msg, pldm_req_msg,
-		    req_msg_len, (void **)pldm_resp_msg, resp_msg_len);
+	pldm_requester_rc_t rc = 0;
+	struct pldm_msg_hdr *hdr = (struct pldm_msg_hdr *)pldm_req_msg;
+	if (hdr && !hdr->request) {
+		return PLDM_REQUESTER_NOT_REQ_MSG;
+	}
+	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_send_recv_msg, rc,
+		    pldm_req_msg, req_msg_len, (void **)pldm_resp_msg,
+		    resp_msg_len);
+	if (rc != PLDM_REQUESTER_SUCCESS) {
+		return rc;
+	}
+	hdr = (struct pldm_msg_hdr *)pldm_resp_msg;
+	if (hdr && (hdr->request || hdr->datagram)) {
+		free(*pldm_resp_msg);
+		*pldm_resp_msg = NULL;
+		return PLDM_REQUESTER_NOT_RESP_MSG;
+	}
+	return rc;
 }
 
 LIBPLDM_ABI_STABLE
 pldm_requester_rc_t pldm_send(mctp_eid_t eid, int mctp_fd,
 			      const uint8_t *pldm_req_msg, size_t req_msg_len)
 {
-	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_send_msg, (void *)pldm_req_msg,
-		    req_msg_len);
+	pldm_requester_rc_t rc = 0;
+	struct pldm_msg_hdr *hdr = (struct pldm_msg_hdr *)pldm_req_msg;
+	if (!hdr->request) {
+		return PLDM_REQUESTER_NOT_REQ_MSG;
+	}
+	PLDM_REQ_FN(eid, mctp_fd, pldm_transport_send_msg, rc,
+		    (void *)pldm_req_msg, req_msg_len);
+	return rc;
 }
 
 /* Adding this here for completeness in the case we can't smoothly

src/transport/transport.c
@@ -66,11 +66,6 @@ pldm_requester_rc_t pldm_transport_send_msg(struct pldm_transport *transport,
 		return PLDM_REQUESTER_NOT_REQ_MSG;
 	}
 
-	const struct pldm_msg_hdr *hdr = pldm_req_msg;
-	if (!hdr->request) {
-		return PLDM_REQUESTER_NOT_REQ_MSG;
-	}
-
 	return transport->send(transport, tid, pldm_req_msg, req_msg_len);
 }
 
@@ -90,20 +85,11 @@ pldm_requester_rc_t pldm_transport_recv_msg(struct pldm_transport *transport,
 		return rc;
 	}
 
-	struct pldm_msg_hdr *hdr = *pldm_resp_msg;
-	if (hdr->request || hdr->datagram) {
+	if (*resp_msg_len < sizeof(struct pldm_msg_hdr)) {
 		free(*pldm_resp_msg);
 		*pldm_resp_msg = NULL;
-		return PLDM_REQUESTER_NOT_RESP_MSG;
+		return PLDM_REQUESTER_INVALID_RECV_LEN;
 	}
-
-	uint8_t pldm_rc = 0;
-	if (*resp_msg_len < (sizeof(struct pldm_msg_hdr) + sizeof(pldm_rc))) {
-		free(*pldm_resp_msg);
-		*pldm_resp_msg = NULL;
-		return PLDM_REQUESTER_RESP_MSG_TOO_SMALL;
-	}
-
 	return PLDM_REQUESTER_SUCCESS;
 }
 

commit d8d2990171296e09e4effb009c4c37d378e12f83

requester: Fix response buffer cast in pldm_send_recv()
With the latest libpldm code version 0.4, sometime the calling
"pldmtool platform GetPDR" command while polling the sensors will be
ended with error `Failed to receive RC = 3`. This error code is
printed when the pldm_send_recv() responses
PLDM_REQUESTER_NOT_RESP_MSG.
In the pldm_send_recv(), hdr is `struct pldm_msg_hdr` pointer and
pldm_resp_msg is double pointer, type casting double pointer to
pointer is incorrected.

Tested:
1. Call `pldmtool platform GetPDR` command while running the sensor
reading.
2. No `Failed to receive RC = 3`

Fixes: 0411b712b746 ("transport: Make APIs work for all types of
messages")

Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
Change-Id: I1efadc80bbd8803d0b97cb634eb8bd7df9d279b9

src/requester/pldm.c
@@ -153,7 +153,7 @@ pldm_requester_rc_t pldm_send_recv(mctp_eid_t eid, int mctp_fd,
 	if (rc != PLDM_REQUESTER_SUCCESS) {
 		return rc;
 	}
-	hdr = (struct pldm_msg_hdr *)pldm_resp_msg;
+	hdr = (struct pldm_msg_hdr *)(*pldm_resp_msg);
 	if (hdr && (hdr->request || hdr->datagram)) {
 		free(*pldm_resp_msg);
 		*pldm_resp_msg = NULL;