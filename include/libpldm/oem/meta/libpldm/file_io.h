dd9942f42297b045edd0776289739570c630dbff
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2023-10-27 11:05:01 +0800

    Support OEM-META write file req decode
    Support decode_write_file_io_req_oem_meta cmd to
    decode the incoming post code.
    
    PLDM OEM META Write File IO cmd:
      Example:
    	Request:
    	   Btye 0: 0x3F (OEM cmd)
    	   Byte 1: 0x02 (FILE IO)
    	   Byte 2 : 0x00 (File io type : POST CODE)
    	   Byte 3-6: 0x04 (Data length)
    	   Byte 7-10: 0x93 0xE0 0x00 0xEA (post code)
    
    	Response:
    	   Byte 0: 0x00 (success)
    
    Tested:
    - Unit Tests passed.
    
    Change-Id: I85437698642dd3cbe6084acf1feada842d206eac
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

---
+++ include/libpldm/oem/meta/libpldm/file_io.h
@@ -0,0 +1,45 @@
+/*
+ Copyright META
+
+ SPDX - License - Identifier : MIT
+*/
+
+#ifndef LIBPLDM_OEM_META_FILE_IO_H
+#define LIBPLDM_OEM_META_FILE_IO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+#include "utils.h"
+
+struct pldm_msg;
+/** @brief PLDM Commands in OEM META type
+ */
+
+enum pldm_oem_meta_fileio_commands {
+	PLDM_WRITE_FILE = 0x2,
+	PLDM_READ_FILE = 0x3,
+};
+
+enum pldm_oem_meta_file_io_type {
+	POST_CODE = 0x00,
+};
+
+struct pldm_write_file_req {
+	uint8_t file_handle;
+	uint32_t length;
+	uint8_t file_data[1];
+};
+
+int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
+				size_t payload_length, uint8_t *file_handle,
+				uint32_t *length, uint8_t *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*LIBPLDM_OEM_META_FILE_IO_H*/
