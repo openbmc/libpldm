f8fb698f7f773e8840f10fb9c064eb68deaf44f1
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2023-10-27 11:05:01 +0800

    Support OEM-META write file req decode
    Support decode_oem_meta_file_io_req() cmd to decode the incoming post
    code.
    
    PLDM OEM META Write File IO cmd:
    Example:
      Request:
        Byte 0: 0x3F (OEM cmd)
        Byte 1: 0x02 (FILE IO)
        Byte 2: 0x00 (File io type : POST CODE)
        Byte 3-6: 0x04 (Data length)
        Byte 7-10: 0x93 0xE0 0x00 0xEA (post code)
    
      Response:
        Byte 0: 0x00 (success)
    
    Tested:
    - Unit Tests passed.
    
    Change-Id: I85437698642dd3cbe6084acf1feada842d206eac
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

---
+++ include/libpldm/oem/meta/file_io.h
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#ifndef LIBPLDM_OEM_META_FILE_IO_H
+#define LIBPLDM_OEM_META_FILE_IO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+struct pldm_msg;
+/** @brief PLDM Commands in OEM META type
+ */
+
+enum pldm_oem_meta_fileio_commands {
+	PLDM_WRITE_FILE = 0x2,
+	PLDM_READ_FILE = 0x3,
+};
+
+enum pldm_oem_meta_file_io_type {
+	POST_CODE = 0x00,
+};
+
+struct pldm_oem_meta_write_file_req {
+	uint8_t file_handle;
+	uint32_t length;
+	uint8_t file_data[1];
+};
+
+/** @brief Decode OEM meta file io req
+ *
+ *  @param[in] msg - Pointer to PLDM request message
+ *  @param[in] payload_length - Length of request payload
+ *  @param[out] file_handle - The handle of data
+ *  @param[out] length - Total size of data
+ *  @param[out] data - Message will be written to this
+ *  @return pldm_completion_codes
+ */
+int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
+				size_t payload_length, uint8_t *file_handle,
+				uint32_t *length, uint8_t *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*LIBPLDM_OEM_META_FILE_IO_H*/
---
+++ src/oem/meta/file_io.c
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#include <libpldm/oem/meta/file_io.h>
+#include <endian.h>
+#include <string.h>
+#include <stdio.h>
+#include "msgbuf.h"
+
+#define PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE 6
+LIBPLDM_ABI_TESTING
+int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
+				size_t payload_length, uint8_t *file_handle,
+				uint32_t *length, uint8_t *data)
+{
+	struct pldm_msgbuf _buf;
+	struct pldm_msgbuf *buf = &_buf;
+	int rc;
+
+	if (msg == NULL || file_handle == NULL || length == NULL ||
+	    data == NULL) {
+		return PLDM_ERROR_INVALID_DATA;
+	}
+
+	rc = pldm_msgbuf_init(buf, PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE,
+			      msg->payload, payload_length);
+	if (rc) {
+		return rc;
+	}
+
+	pldm_msgbuf_extract(buf, file_handle);
+	pldm_msgbuf_extract(buf, length);
+	pldm_msgbuf_extract_array_uint8(buf, data, *length);
+
+	return pldm_msgbuf_destroy(buf);
+}
