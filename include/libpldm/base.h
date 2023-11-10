openbmc/libpldm

commit b0c1d20a5bdea758dcd28ae5c1295cbd9485ba0f

libpldm: Fix header use
The headers need to work whether we're building libpldm in the repo or
we're building another project depending on the headers in the system
include directory.

Tidy up the paths involved and switch to defining the public headers as
system headers for the purpose of the build.

Change-Id: I49413988c94d393ea5761bc4684edcd2c2482a98
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/requester/pldm.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
-#include "libpldm/requester/pldm.h"
-#include "base.h"
-#include "libpldm/transport.h"
+#include <libpldm/base.h>
+#include <libpldm/requester/pldm.h>
+#include <libpldm/transport.h>
 
 #include <bits/types/struct_iovec.h>
 #include <fcntl.h>

include/libpldm/meson.build
@@ -1,5 +1,3 @@
-libpldm_include_dir += include_directories('.')
-
 libpldm_headers += files(
   'base.h',
   'bios.h',
@@ -24,7 +22,6 @@ libpldm_transport_headers += files(
 )
 
 if get_option('oem-ibm').allowed()
-  libpldm_include_dir += include_directories('oem/ibm')
   libpldm_headers += files(
     'oem/ibm/libpldm/entity_oem_ibm.h',
     'oem/ibm/libpldm/file_io.h',

include/libpldm/oem/ibm/libpldm/host.h
@@ -6,10 +6,10 @@
 extern "C" {
 #endif
 
+#include <libpldm/base.h>
+
 #include <stddef.h>
 #include <stdint.h>
-
-#include "base.h"
 
 /* Maximum size for request */
 #define PLDM_GET_ALERT_STATUS_REQ_BYTES 1

include/libpldm/oem/ibm/libpldm/platform_oem_ibm.h
@@ -6,7 +6,8 @@
 extern "C" {
 #endif
 
-#include "base.h"
+#include <libpldm/base.h>
+
 #include <stddef.h>
 #include <stdint.h>
 

include/meson.build
@@ -1,0 +1,1 @@
+subdir('libpldm')

meson.build
@@ -43,20 +43,22 @@ config = configure_file(output: 'config.h',
 
 add_project_arguments('-include', '@0@'.format(config), language: 'c')
 
-libpldm_sources = files()
-subdir('src')
-
-libpldm_include_dir = ['.', 'include', 'src']
+libpldm_include_dir = include_directories('include', is_system: true)
 libpldm_headers = files()
 libpldm_transport_headers = files()
+subdir('include')
 
-subdir('include/libpldm')
+libpldm_sources = files()
+subdir('src')
 
 libpldm = library(
   'pldm',
    libpldm_sources,
    implicit_include_directories: false,
-   include_directories: libpldm_include_dir,
+   include_directories: [
+     libpldm_include_dir,
+     include_directories('src')
+   ],
    version: meson.project_version(),
    gnu_symbol_visibility: 'hidden',
    install: true
@@ -73,7 +75,8 @@ install_headers(
   )
 
 libpldm_dep = declare_dependency(
-  include_directories: libpldm_include_dir,
+  include_directories: libpldm_include_dir
+,
   link_with: libpldm)
 
 import('pkgconfig').generate(

src/base.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
-#include "base.h"
-#include "pldm_types.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm_types.h>
+
 #include <endian.h>
 #include <stdint.h>
 #include <string.h>

src/bios.c
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
-#include "bios.h"
-#include "base.h"
-#include "utils.h"
+#include <libpldm/base.h>
+#include <libpldm/bios.h>
+#include <libpldm/utils.h>
+
 #include <endian.h>
 #include <string.h>
 

commit 6bf67e65acfe4f64b1d92c3a01bb173040ef1f11

libpldm: More fixes for header use
b0c1d20a5bde ("libpldm: Fix header use") only did half the job,
addressing compilation concerns for the library itself. As it turns out
there were still plenty of places that used local includes for paths to
the public headers.

Apply the rest of the header cleanups to hopefully get us into a
consistent state.

Fixes: b0c1d20a5bde ("libpldm: Fix header use")
Change-Id: I8d4b59c9241770583abb30389452af0a32b18b99
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

include/libpldm/base.h
@@ -6,12 +6,12 @@
 extern "C" {
 #endif
 
+#include <libpldm/pldm_types.h>
+
 #include <asm/byteorder.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-#include "pldm_types.h"
 
 typedef uint8_t pldm_tid_t;
 

include/libpldm/bios_table.h
@@ -6,7 +6,8 @@
 extern "C" {
 #endif
 
-#include "bios.h"
+#include <libpldm/bios.h>
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>

include/libpldm/firmware_update.h
@@ -6,8 +6,9 @@
 extern "C" {
 #endif
 
-#include "base.h"
-#include "pldm_types.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm_types.h>
+
 #include "stdbool.h"
 #include <stddef.h>
 #include <stdint.h>

include/libpldm/fru.h
@@ -6,12 +6,12 @@
 extern "C" {
 #endif
 
+#include <libpldm/base.h>
+#include <libpldm/utils.h>
+
 #include <asm/byteorder.h>
 #include <stddef.h>
 #include <stdint.h>
-
-#include "base.h"
-#include "utils.h"
 
 #define PLDM_GET_FRU_RECORD_TABLE_METADATA_REQ_BYTES  0
 #define PLDM_GET_FRU_RECORD_TABLE_METADATA_RESP_BYTES 19

include/libpldm/instance-id.h
@@ -6,7 +6,8 @@
 extern "C" {
 #endif
 
-#include "libpldm/base.h"
+#include <libpldm/base.h>
+
 #include <stdint.h>
 
 typedef uint8_t pldm_instance_id_t;

include/libpldm/platform.h
@@ -9,9 +9,9 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
-#include "base.h"
-#include "pdr.h"
-#include "pldm_types.h"
+#include <libpldm/base.h>
+#include <libpldm/pdr.h>
+#include <libpldm/pldm_types.h>
 
 /* Maximum size for request */
 #define PLDM_SET_STATE_EFFECTER_STATES_REQ_BYTES  19

include/libpldm/states.h
@@ -6,7 +6,7 @@
 extern "C" {
 #endif
 
-#include "pldm_types.h"
+#include <libpldm/pldm_types.h>
 
 /** @brief PLDM enums for the boot progress state set
  */

include/libpldm/transport.h
@@ -6,8 +6,9 @@
 extern "C" {
 #endif
 
-#include "libpldm/base.h"
-#include "libpldm/pldm.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm.h>
+
 #include <stddef.h>
 
 struct pldm_transport;

include/libpldm/transport/af-mctp.h
@@ -2,8 +2,8 @@
 #ifndef LIBPLDM_AF_MCTP_H
 #define LIBPLDM_AF_MCTP_H
 
-#include "libpldm/base.h"
-#include "libpldm/pldm.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm.h>
 
 #ifdef __cplusplus
 extern "C" {

include/libpldm/transport/mctp-demux.h
@@ -2,8 +2,8 @@
 #ifndef LIBPLDM_DEMUX_H
 #define LIBPLDM_DEMUX_H
 
-#include "libpldm/base.h"
-#include "libpldm/pldm.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm.h>
 
 #ifdef __cplusplus
 extern "C" {

include/libpldm/utils.h
@@ -6,7 +6,8 @@
 extern "C" {
 #endif
 
-#include "pldm_types.h"
+#include <libpldm/pldm_types.h>
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>

src/requester/pldm.c
@@ -13,7 +13,7 @@
 #include <unistd.h>
 
 /* Temporary for old api */
-#include "libpldm/transport/mctp-demux.h"
+#include <libpldm/transport/mctp-demux.h>
 extern int
 pldm_transport_mctp_demux_get_socket_fd(struct pldm_transport_mctp_demux *ctx);
 extern struct pldm_transport_mctp_demux *

src/transport/test.h
@@ -2,7 +2,7 @@
 #ifndef LIBPLDM_TRANSPORT_TEST_H
 #define LIBPLDM_TRANSPORT_TEST_H
 
-#include "libpldm/base.h"
+#include <libpldm/base.h>
 
 #include <stddef.h>
 #include <stdint.h>

src/transport/transport.c
@@ -1,8 +1,9 @@
 /* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
-#include "libpldm/transport.h"
-#include "libpldm/base.h"
-#include "libpldm/pldm.h"
 #include "transport.h"
+
+#include <libpldm/transport.h>
+#include <libpldm/base.h>
+#include <libpldm/pldm.h>
 
 #include <errno.h>
 #include <limits.h>

src/transport/transport.h
@@ -2,8 +2,8 @@
 #ifndef LIBPLDM_SRC_TRANSPORT_TRANSPORT_H
 #define LIBPLDM_SRC_TRANSPORT_TRANSPORT_H
 
-#include "libpldm/base.h"
-#include "libpldm/pldm.h"
+#include <libpldm/base.h>
+#include <libpldm/pldm.h>
 struct pollfd;
 
 /**

tests/instance-id.cpp
@@ -1,11 +1,10 @@
-#include "libpldm/instance-id.h"
+#include <libpldm/base.h>
+#include <libpldm/instance-id.h>
 
 #include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
-
-#include "libpldm/base.h"
 
 #include <gtest/gtest.h>
 

tests/libpldm_bios_table_test.cpp
@@ -1,4 +1,8 @@
 #include <endian.h>
+#include <libpldm/base.h>
+#include <libpldm/bios.h>
+#include <libpldm/bios_table.h>
+#include <libpldm/utils.h>
 
 #include <algorithm>
 #include <cstdint>
@@ -7,11 +11,6 @@
 #include <string>
 #include <utility>
 #include <vector>
-
-#include "libpldm/base.h"
-#include "libpldm/bios.h"
-#include "libpldm/bios_table.h"
-#include "libpldm/utils.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>

tests/libpldm_bios_test.cpp
@@ -1,13 +1,12 @@
 #include <endian.h>
+#include <libpldm/base.h>
+#include <libpldm/bios.h>
+#include <libpldm/utils.h>
 
 #include <array>
 #include <cstdint>
 #include <cstring>
 #include <memory>
-
-#include "libpldm/base.h"
-#include "libpldm/bios.h"
-#include "libpldm/utils.h"
 
 #include <gtest/gtest.h>
 

tests/libpldm_pdr_test.cpp
@@ -1,13 +1,12 @@
 #include <endian.h>
+#include <libpldm/pdr.h>
+#include <libpldm/platform.h>
 
 #include <array>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
 #include <vector>
-
-#include "libpldm/pdr.h"
-#include "libpldm/platform.h"
 
 #include <gtest/gtest.h>
 

tests/transport.cpp
@@ -1,4 +1,4 @@
-#include "libpldm/transport.h"
+#include <libpldm/transport.h>
 
 #include "array.h"
 #include "transport/test.h"

tests/transport/send_recv_one.cpp
@@ -1,5 +1,6 @@
+#include <libpldm/transport.h>
+
 #include "array.h"
-#include "libpldm/transport.h"
 #include "transport/test.h"
 
 #include <gtest/gtest.h>

tests/transport/send_recv_timeout.cpp
@@ -1,5 +1,6 @@
+#include <libpldm/transport.h>
+
 #include "array.h"
-#include "libpldm/transport.h"
 #include "transport/test.h"
 
 #include <gtest/gtest.h>

tests/transport/send_recv_unwanted.cpp
@@ -1,5 +1,6 @@
+#include <libpldm/transport.h>
+
 #include "array.h"
-#include "libpldm/transport.h"
 #include "transport/test.h"
 
 #include <gtest/gtest.h>

tests/transport/send_recv_wrong_command_code.cpp
@@ -1,5 +1,6 @@
+#include <libpldm/transport.h>
+
 #include "array.h"
-#include "libpldm/transport.h"
 #include "transport/test.h"
 
 #include <gtest/gtest.h>

tests/transport/send_recv_wrong_pldm_type.cpp
@@ -1,5 +1,6 @@
+#include <libpldm/transport.h>
+
 #include "array.h"
-#include "libpldm/transport.h"
 #include "transport/test.h"
 
 #include <gtest/gtest.h>