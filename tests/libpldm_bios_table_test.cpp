e2bbf87703a51fb28bb5c8ac21894113c2450194
Sagar Srinivas <sagar.srinivas@ibm.com>
2023-12-12 05:50:24 -0600

    Change BaseBIOStable type
    This commit adds changes to BaseBIOStable type
    as per the changes done in bios-setting-mgr app at -
    https://gerrit.openbmc.org/c/openbmc/bios-settings-mgr/+/63061
    
    Currently, the upstream code is broken in pldm due to the above
    change.
    
    Change-Id: Ic10fc82d0d9e9736effdb42d63ab870c6a069e5c
    Signed-off-by: Sagar Srinivas <sagar.srinivas@ibm.com>

--- tests/libpldm_bios_table_test.cpp
+++ tests/libpldm_bios_table_test.cpp
@@ -46,7 +46,10 @@ TEST(AttrTable, HeaderDecodeTest)
         2, 0, /* possible value handle */
         3, 0, /* possible value handle */
         1,    /* number of default value */
-        0     /* defaut value string handle index */
+        0,    /* defaut value string handle index */
+        2,    /* number of value display names*/
+        2, 0, /* value display names handle*/
+        3, 0  /* value display names handle*/
     };
     auto entry =
         reinterpret_cast<struct pldm_bios_attr_table_entry*>(enumEntry.data());
@@ -68,7 +71,10 @@ TEST(AttrTable, EnumEntryDecodeTest)
         2, 0, /* possible value handle */
         3, 0, /* possible value handle */
         1,    /* number of default value */
-        1     /* defaut value string handle index */
+        1,    /* defaut value string handle index */
+        2,    /* number of value display names*/
+        2, 0, /* value display names handle*/
+        3, 0  /* value display names handle*/
     };
 
     auto entry =
@@ -130,7 +136,46 @@ TEST(AttrTable, EnumEntryDecodeTest)
     rc = pldm_bios_table_attr_entry_enum_decode_def_num_check(entry, nullptr);
     EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
 
+    uint8_t vdnNumber;
+    ASSERT_EQ(
+        pldm_bios_table_attr_entry_enum_decode_vdn_num_check(entry, &vdnNumber),
+        PLDM_SUCCESS);
+    EXPECT_EQ(vdnNumber, 2);
+    vdnNumber = 0;
+    rc =
+        pldm_bios_table_attr_entry_enum_decode_vdn_num_check(entry, &vdnNumber);
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+    EXPECT_EQ(vdnNumber, 2);
+
+    std::vector<uint16_t> vdnHandles(vdnNumber, 0);
+    ASSERT_EQ(pldm_bios_table_attr_entry_enum_decode_vdn_hdls_check(
+                  entry, vdnHandles.data(), vdnHandles.size()),
+              PLDM_SUCCESS);
+    EXPECT_EQ(vdnNumber, 2);
+    EXPECT_EQ(vdnHandles[0], 2);
+    EXPECT_EQ(vdnHandles[1], 3);
+    vdnHandles.resize(1);
+    ASSERT_EQ(pldm_bios_table_attr_entry_enum_decode_vdn_hdls_check(
+                  entry, vdnHandles.data(), vdnHandles.size()),
+              PLDM_SUCCESS);
+    EXPECT_EQ(vdnHandles[0], 2);
+
+    vdnHandles.resize(2);
+    rc = pldm_bios_table_attr_entry_enum_decode_vdn_hdls_check(
+        entry, vdnHandles.data(), vdnHandles.size());
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+    EXPECT_EQ(vdnHandles[0], 2);
+    EXPECT_EQ(vdnHandles[1], 3);
+    rc = pldm_bios_table_attr_entry_enum_decode_vdn_hdls_check(
+        entry, vdnHandles.data(), 1);
+    EXPECT_EQ(rc, PLDM_SUCCESS);
+
+    rc = pldm_bios_table_attr_entry_enum_decode_vdn_num_check(nullptr,
+                                                              &vdnNumber);
+    EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
     entry->attr_type = PLDM_BIOS_STRING;
+
     rc = pldm_bios_table_attr_entry_enum_decode_pv_num_check(entry, &pvNumber);
     EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
 
@@ -138,7 +183,15 @@ TEST(AttrTable, EnumEntryDecodeTest)
         pldm_bios_table_attr_entry_enum_decode_def_num_check(entry, &defNumber);
     EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
     rc =
+        pldm_bios_table_attr_entry_enum_decode_vdn_num_check(entry, &vdnNumber);
+    EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    rc =
         pldm_bios_table_attr_entry_enum_decode_pv_hdls_check(entry, nullptr, 0);
+    EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
+
+    rc = pldm_bios_table_attr_entry_enum_decode_vdn_hdls_check(entry, nullptr,
+                                                               0);
     EXPECT_EQ(rc, PLDM_ERROR_INVALID_DATA);
 }
 
@@ -152,10 +205,14 @@ TEST(AttrTable, EnumEntryEncodeTest)
         2, 0, /* possible value handle */
         3, 0, /* possible value handle */
         1,    /* number of default value */
-        0     /* defaut value string handle index */
+        0,    /* defaut value string handle index */
+        2,    /* number of value display names */
+        2, 0, /* value display name handle*/
+        3, 0  /* value display name handle*/
     };
 
     std::vector<uint16_t> pv_hdls{2, 3};
+    std::vector<uint16_t> vdn_hdls{2, 3};
     std::vector<uint8_t> defs{0};
 
     struct pldm_bios_table_attr_entry_enum_info info = {
@@ -164,9 +221,10 @@ TEST(AttrTable, EnumEntryEncodeTest)
         2,              /* pv number */
         pv_hdls.data(), /* pv handle */
         1,              /*def number */
-        defs.data()     /*def index*/
-    };
-    auto encodeLength = pldm_bios_table_attr_entry_enum_encode_length(2, 1);
+        defs.data(),    /*def index*/
+        2,
+        vdn_hdls.data()};
+    auto encodeLength = pldm_bios_table_attr_entry_enum_encode_length(2, 1, 2);
     EXPECT_EQ(encodeLength, enumEntry.size());
 
     std::vector<uint8_t> encodeEntry(encodeLength, 0);
@@ -443,7 +501,10 @@ TEST(AttrTable, ItearatorTest)
         2, 0, /* possible value handle */
         3, 0, /* possible value handle */
         1,    /* number of default value */
-        0     /* defaut value string handle index */
+        0,    /* defaut value string handle index */
+        2,    /* number of value display names */
+        2, 0, /* value display name handle */
+        3, 0  /* value display name handle */
     };
     std::vector<uint8_t> stringEntry{
         1,   0,       /* attr handle */
@@ -503,7 +564,10 @@ TEST(AttrTable, FindTest)
         2, 0, /* possible value handle */
         3, 0, /* possible value handle */
         1,    /* number of default value */
-        0     /* defaut value string handle index */
+        0,    /* defaut value string handle index */
+        2,    /* number of value display names */
+        2, 0, /* value display name handle*/
+        3, 0  /* value display name handle*/
     };
     std::vector<uint8_t> stringEntry{
         1,   0,       /* attr handle */
