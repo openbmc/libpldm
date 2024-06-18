97f0669deab3b29a6911a08c65e68f19b13932de
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-06-18 15:51:42 +0800

    oem: meta: Support decoding and encoding for the Http Boot feature
    Decode Http Boot certification requests and encode their responses.
    
    Change-Id: Iae3c7f24128bc25c5af6951f34fdc6d8a7b90381
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

--- src/oem/meta/file_io.c
+++ src/oem/meta/file_io.c
@@ -3,23 +3,29 @@
 #include <endian.h>
 #include <string.h>
 #include <stdio.h>
+
+#include "api.h"
 #include "msgbuf.h"
+#include "dsp/base.h"
 
 #define PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE 6
+#define PLDM_OEM_META_DECODE_READ_FILE_IO_MIN_SIZE  1
+
 LIBPLDM_ABI_STABLE
-int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
-				size_t payload_length, uint8_t *file_handle,
-				uint32_t *length, uint8_t *data)
+int decode_oem_meta_write_file_io_req(const struct pldm_msg *msg,
+				      size_t payload_length,
+				      uint8_t *file_handle, uint32_t *length,
+				      uint8_t *data)
 {
 	struct pldm_msgbuf _buf;
 	struct pldm_msgbuf *buf = &_buf;
 
 	if (msg == NULL || file_handle == NULL || length == NULL ||
 	    data == NULL) {
-		return PLDM_ERROR_INVALID_DATA;
+		return -EINVAL;
 	}
 
-	int rc = pldm_msgbuf_init_cc(
+	int rc = pldm_msgbuf_init_errno(
 		buf, PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE, msg->payload,
 		payload_length);
 	if (rc) {
@@ -31,4 +37,20 @@ int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
 	pldm_msgbuf_extract_array_uint8(buf, data, *length);
 
 	return pldm_msgbuf_destroy_consumed(buf);
+}
+
+LIBPLDM_ABI_DEPRECATED
+int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
+				size_t payload_length, uint8_t *file_handle,
+				uint32_t *length, uint8_t *data)
+{
+	int rc;
+
+	rc = decode_oem_meta_write_file_io_req(msg, payload_length, file_handle,
+					       length, data);
+	if (rc < 0) {
+		return pldm_xlate_errno(rc);
+	}
+
+	return 0;
 }
