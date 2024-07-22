c230be03eba5c514eda2fcb80f4d4296f6f8adf1
Lora Lin <lora.lin.wiwynn@gmail.com>
2024-07-22 09:46:18 +0800

    oem: meta: Add decode_oem_meta_file_io_write_req()
    Add decode_oem_meta_file_io_write_req function.
    Deprecate decode_oem_meta_file_io_req function.
    
    The difference between functions decode_oem_meta_file_io_req and
    decode_oem_meta_file_io_write_req:
    - decode_oem_meta_file_io_write_req:
    return 0 on success and return a negative errno value on failure.
    input parameters is structure.
    check the return code from the function of pldm_msgbuf_extract.
    add req_length parameter to check whether the total length of
    the request is buffer overflow.
    - decode_oem_meta_file_io_req:
    return PLDM_SUCCESS on success and return another PLDM completion
    code on failure.
    input parameters is passing individual pointers.
    non-check the return code from the function of pldm_msgbuf_extract_p.
    
    Change-Id: Iae3c7f24128bc25c5af6951f34fdc6d8a7b90381
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>
    Signed-off-by: Lora Lin <lora.lin.wiwynn@gmail.com>

--- include/libpldm/oem/meta/file_io.h
+++ include/libpldm/oem/meta/file_io.h
@@ -21,10 +21,24 @@ enum pldm_oem_meta_fileio_commands {
 struct pldm_oem_meta_write_file_req {
 	uint8_t file_handle;
 	uint32_t length;
-	uint8_t file_data[1];
+	uint8_t *file_data;
 };
 
-/** @brief Decode OEM meta file io req
+/** @brief Decode OEM meta write file io req
+ *
+ *  @param[in] msg - Pointer to PLDM request message
+ *  @param[in] payload_length - Length of request payload
+ *  @param[out] req - Pointer to the structure to store the decoded response data
+ *  @param[in] req_length - Length of request structure
+ *  @param[out] data - Message will be written to this
+ *  @return 0 on success, negative errno value on failure
+ */
+int decode_oem_meta_file_io_write_req(const struct pldm_msg *msg,
+				      size_t payload_length,
+				      struct pldm_oem_meta_write_file_req *req,
+				      uint32_t req_length);
+
+/** @brief Deprecated decoder for OEM meta write file io req
  *
  *  @param[in] msg - Pointer to PLDM request message
  *  @param[in] payload_length - Length of request payload
