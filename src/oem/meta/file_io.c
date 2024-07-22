5c9075f633128a4534916832f7b85736f0984d29
Lora Lin <lora.lin.wiwynn@gmail.com>
2024-07-22 09:46:18 +0800

    oem: meta: Add decode_oem_meta_file_io_write_req()
    Add decode_oem_meta_file_io_write_req function.
    Deprecate decode_oem_meta_file_io_req function.
    Add a new parameter to the decode_oem_meta_file_io_req function so
    that it can use the decode_oem_meta_file_io_write_req function
    correctly.
    
    The difference between functions decode_oem_meta_file_io_req and
    decode_oem_meta_file_io_write_req:
    - decode_oem_meta_file_io_write_req:
    return 0 on success and return a negative errno value on failure.
    input parameters is structure.
    check the return code from the function of pldm_msgbuf_extract.
    add req_length parameter to check whether the total length of
    the request is buffer overflow.
    - decode_oem_meta_file_io_req:
    return PLDM_SUCCESS on success and return another PLDM completion
    code on failure.
    input parameters is passing individual pointers.
    
    Change-Id: Iae3c7f24128bc25c5af6951f34fdc6d8a7b90381
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>
    Signed-off-by: Lora Lin <lora.lin.wiwynn@gmail.com>

--- src/oem/meta/file_io.c
+++ src/oem/meta/file_io.c
@@ -3,32 +3,69 @@
 #include <endian.h>
 #include <string.h>
 #include <stdio.h>
+
+#include "api.h"
 #include "msgbuf.h"
+#include "dsp/base.h"
 
 #define PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE 6
-LIBPLDM_ABI_STABLE
-int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
-				size_t payload_length, uint8_t *file_handle,
-				uint32_t *length, uint8_t *data)
+LIBPLDM_ABI_TESTING
+int decode_oem_meta_file_io_write_req(const struct pldm_msg *msg,
+				      size_t payload_length,
+				      struct pldm_oem_meta_write_file_req *req,
+				      size_t req_length)
 {
 	struct pldm_msgbuf _buf;
 	struct pldm_msgbuf *buf = &_buf;
 
-	if (msg == NULL || file_handle == NULL || length == NULL ||
-	    data == NULL) {
-		return PLDM_ERROR_INVALID_DATA;
+	if (msg == NULL || req == NULL || req->file_data == NULL) {
+		return -EINVAL;
 	}
 
-	int rc = pldm_msgbuf_init_cc(
+	int rc = pldm_msgbuf_init_errno(
 		buf, PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE, msg->payload,
 		payload_length);
 	if (rc) {
 		return rc;
 	}
 
-	pldm_msgbuf_extract_p(buf, file_handle);
-	pldm_msgbuf_extract_p(buf, length);
-	pldm_msgbuf_extract_array_uint8(buf, data, *length);
+	pldm_msgbuf_extract(buf, req->file_handle);
+	rc = pldm_msgbuf_extract(buf, req->length);
+	if (rc) {
+		return rc;
+	}
+
+	if ((req->length + PLDM_OEM_META_DECODE_WRITE_FILE_IO_MIN_SIZE) >
+	    req_length) {
+		// The total length of the request message exceeds the length provided
+		return -EOVERFLOW;
+	}
+	pldm_msgbuf_extract_array_uint8(buf, req->file_data, req->length);
 
 	return pldm_msgbuf_destroy_consumed(buf);
+}
+
+LIBPLDM_ABI_DEPRECATED
+int decode_oem_meta_file_io_req(const struct pldm_msg *msg,
+				size_t payload_length, uint8_t *file_handle,
+				uint32_t *length, uint8_t *data)
+{
+	if (msg == NULL || file_handle == NULL || length == NULL ||
+	    data == NULL) {
+		return pldm_xlate_errno(-EINVAL);
+	}
+
+	int rc;
+	struct pldm_oem_meta_write_file_req request_msg;
+	request_msg.file_data = data;
+
+	rc = decode_oem_meta_file_io_write_req(msg, payload_length,
+					       &request_msg, UINT32_MAX);
+	if (rc < 0) {
+		return pldm_xlate_errno(rc);
+	}
+
+	*file_handle = request_msg.file_handle;
+	*length = request_msg.length;
+	return 0;
 }
