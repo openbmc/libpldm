11e2e87af9931e3ef5b85694209581053608b49e
Patrick Williams <patrick@stwcx.xyz>
2024-08-16 15:22:26 -0400

    clang-format: re-format for clang-18
    clang-format-18 isn't compatible with the clang-format-17 output, so we
    need to reformat the code with the latest version.  The way clang-18
    handles lambda formatting also changed, so we have made changes to the
    organization default style format to better handle lambda formatting.
    
    See I5e08687e696dd240402a2780158664b7113def0e for updated style.
    See Iea0776aaa7edd483fa395e23de25ebf5a6288f71 for clang-18 enablement.
    
    Change-Id: I0af2024502c08b1060bb6beb0895108459f00d48
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- src/transport/container-of.h
+++ src/transport/container-of.h
@@ -4,7 +4,7 @@
 
 #ifndef container_of
 #define container_of(ptr, type, member)                                        \
-	(type *)((char *)(ptr)-offsetof(type, member))
+	(type *)((char *)(ptr) - offsetof(type, member))
 #endif
 
 #endif
