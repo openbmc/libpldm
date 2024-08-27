c722c47b412313097fea02d1ad05ab7791220f4b
Riya Dixit <riyadixitagra@gmail.com>
2024-08-27 01:57:26 -0500

    pdr: Adding API for updating container IDs
    This commit introduces a new api for finding container Id for Effecter
    Id after receiving updated container Id value from remote
    terminus/endpoint.
    
    pldm_find_container_id - Finds the updated container id of an effecter
    from Entity Association PDR.
    
    Tested: Verified the containers id getting updated in Numeric Effecter
    PDRs.
    
    Change-Id: I2710e08538762cf81b35c2fa4682faa0b3a88bab
    Signed-off-by: Riya Dixit <riyadixitagra@gmail.com>

--- src/dsp/pdr.c
+++ src/dsp/pdr.c
@@ -462,6 +462,38 @@ typedef struct pldm_entity_node {
 	uint8_t association_type;
 } pldm_entity_node;
 
+LIBPLDM_ABI_TESTING
+uint16_t pldm_find_container_id(const pldm_pdr *repo, uint16_t entityType,
+				uint16_t entityInstance)
+{
+	assert(repo != NULL);
+
+	pldm_pdr_record *record = repo->first;
+
+	while (record != NULL) {
+		struct pldm_pdr_hdr *hdr = (struct pldm_pdr_hdr *)record->data;
+		if (hdr->type == PLDM_PDR_ENTITY_ASSOCIATION) {
+			struct pldm_pdr_entity_association *pdr =
+				(struct pldm_pdr_entity_association
+					 *)((uint8_t *)record->data +
+					    sizeof(struct pldm_pdr_hdr));
+			struct pldm_entity *child =
+				(struct pldm_entity *)(&pdr->children[0]);
+			for (int i = 0; i < pdr->num_children; ++i) {
+				if (pdr->container.entity_type == entityType &&
+				    pdr->container.entity_instance_num ==
+					    entityInstance) {
+					uint16_t id =
+						child->entity_container_id;
+					return id;
+				}
+			}
+		}
+		record = record->next;
+	}
+	return 0;
+}
+
 static inline uint16_t next_container_id(pldm_entity_association_tree *tree)
 {
 	assert(tree != NULL);
