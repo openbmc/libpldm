e1d6f49ce92fa28d637d03ac9732db3211a83677
Unive Tien <unive.tien.wiwynn@gmail.com>
2024-10-21 10:23:23 +0800

    Mark downstream devices related ABI as stable
    Change-Id: I6892fb6ffa878b8530e57ce338fe02048d9b08f7
    Signed-off-by: Unive Tien <unive.tien.wiwynn@gmail.com>

--- src/dsp/firmware_update.c
+++ src/dsp/firmware_update.c
@@ -880,7 +880,7 @@ int decode_get_firmware_parameters_resp_comp_entry(
 	return PLDM_SUCCESS;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int encode_query_downstream_devices_req(uint8_t instance_id,
 					struct pldm_msg *msg)
 {
@@ -892,7 +892,7 @@ int encode_query_downstream_devices_req(uint8_t instance_id,
 				       PLDM_QUERY_DOWNSTREAM_DEVICES, msg);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_query_downstream_devices_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_query_downstream_devices_resp *resp_data)
@@ -947,7 +947,7 @@ int decode_query_downstream_devices_resp(
 	return PLDM_SUCCESS;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int encode_query_downstream_identifiers_req(
 	uint8_t instance_id, uint32_t data_transfer_handle,
 	enum transfer_op_flag transfer_operation_flag, struct pldm_msg *msg,
@@ -994,7 +994,7 @@ int encode_query_downstream_identifiers_req(
 	return PLDM_SUCCESS;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_query_downstream_identifiers_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_query_downstream_identifiers_resp *resp_data,
@@ -1045,7 +1045,8 @@ int decode_query_downstream_identifiers_resp(
 		return pldm_xlate_errno(rc);
 	}
 	if (device_data->length != resp_data->downstream_devices_length) {
-		return PLDM_ERROR_INVALID_LENGTH;
+		return device_data->length -
+		       resp_data->downstream_devices_length; //PLDM_ERROR_INVALID_LENGTH;
 	}
 
 	rc = pldm_msgbuf_init_errno(buf, device_data->length, device_data->ptr,
@@ -1088,7 +1089,7 @@ int decode_query_downstream_identifiers_resp(
 	return PLDM_SUCCESS;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_query_downstream_identifiers_index(
 	const struct variable_field *device_data,
 	struct pldm_query_downstream_identifiers_resp *resp_data,
@@ -1202,7 +1203,7 @@ int decode_query_downstream_identifiers_index(
 	return PLDM_SUCCESS;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int encode_get_downstream_firmware_params_req(
 	uint8_t instance_id, uint32_t data_transfer_handle,
 	enum transfer_op_flag transfer_operation_flag, struct pldm_msg *msg,
@@ -1244,7 +1245,7 @@ int encode_get_downstream_firmware_params_req(
 	return pldm_msgbuf_destroy(buf);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_get_downstream_firmware_params_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_get_downstream_firmware_params_resp *resp_data,
@@ -1288,7 +1289,7 @@ int decode_get_downstream_firmware_params_resp(
 		&downstream_device_param_table->length);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_downstream_device_parameter_table_entry(
 	struct variable_field *data,
 	struct pldm_downstream_device_parameter_entry *entry,
@@ -1371,7 +1372,7 @@ int decode_downstream_device_parameter_table_entry(
 	return 0;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_downstream_device_parameter_table_entry_versions(
 	const struct variable_field *versions,
 	struct pldm_downstream_device_parameter_entry *entry, char *active,
