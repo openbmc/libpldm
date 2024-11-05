39a34e5bf7b7a04be7d703510484a26c6dee4e89
Matt Johnston <matt@codeconstruct.com.au>
2024-11-05 18:11:32 +0800

    firmware fd: Add PLDM Control version/commands
    Change-Id: I6a4840a820bf2a1a7967ab9fe927f69681f375d8
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- include/libpldm/control.h
+++ include/libpldm/control.h
@@ -10,3 +10,12 @@ struct pldm_control;
 pldm_requester_rc_t pldm_control_handle_msg(struct pldm_control *control,
 					    const void *req_msg, size_t req_len,
 					    void *resp_msg, size_t *resp_len);
+
+pldm_requester_rc_t pldm_control_setup(struct pldm_control *control,
+				       size_t pldm_control_size);
+
+pldm_requester_rc_t pldm_control_add_type(struct pldm_control *control,
+					  uint8_t pldm_type,
+					  const void *versions,
+					  size_t versions_count,
+					  const bitfield8_t *commands);
