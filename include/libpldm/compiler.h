750fa0bee7bf3c067cfb57d4d49517a7bbef6111
Andrew Jeffery <andrew@codeconstruct.com.au>
2024-11-07 16:30:36 +1030

    wip: dsp: firmware_update: Iterators for downstream devices
    Change-Id: I6e79454b94868da73f318635bcaae57cd51fbf97
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

--- include/libpldm/compiler.h
+++ include/libpldm/compiler.h
@@ -5,8 +5,16 @@
 
 #if defined __has_attribute
 
+#if __has_attribute(always_inline)
+#define LIBPLDM_CC_ALWAYS_INLINE __attribute__((always_inline)) static inline
+#endif
+
 #if __has_attribute(counted_by)
 #define LIBPLDM_CC_COUNTED_BY(x) __attribute__((counted_by(x)))
+#endif
+
+#if __has_attribute(nonnull)
+#define LIBPLDM_CC_NONNULL __attribute__((nonnull))
 #endif
 
 #if __has_attribute(unavailable)
@@ -15,12 +23,22 @@
 
 #endif
 
+#ifndef LIBPLDM_CC_ALWAYS_INLINE
+#define LIBPLDM_CC_ALWAYS_INLINE static inline
+#endif
+
 #ifndef LIBPLDM_CC_COUNTED_BY
 #define LIBPLDM_CC_COUNTED_BY(x)
+#endif
+
+#ifndef LIBPLDM_CC_NONNULL
+#define LIBPLDM_CC_NONNULL
 #endif
 
 #ifndef LIBPLDM_CC_UNAVAILABLE
 #define LIBPLDM_CC_UNAVAILABLE
 #endif
+
+#define LIBPLDM_ITERATOR LIBPLDM_CC_NONNULL LIBPLDM_CC_ALWAYS_INLINE
 
 #endif
