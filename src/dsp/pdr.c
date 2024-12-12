5609f87579136c8e302d89f6002fc3dc9cf38152
Archana Kakani <archana.kakani@ibm.com>
2024-12-12 08:22:55 -0600

    pdr: Handle removal of the last contained entity from EA PDR
    After removing the contained entity from the entity association
    PDR, if there is no other container entity present, we need to remove
    the entity association PDR.
    
    Tested:
    Updated the existing testcase.
    
    Change-Id: If426314a571e91d178e8ae802a49bb7a3746080e
    Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

--- src/dsp/pdr.c
+++ src/dsp/pdr.c
@@ -1852,8 +1852,9 @@ int pldm_entity_association_pdr_remove_contained_entity(
 
 	// Initialize new PDR record with data from original PDR record.
 	// Start with adding the header of original PDR
-	rc = pldm_msgbuf_init_errno(dst, PDR_ENTITY_ASSOCIATION_MIN_SIZE,
-				    new_record->data, new_record->size);
+	rc = pldm_msgbuf_init_errno(
+		dst, (PDR_ENTITY_ASSOCIATION_MIN_SIZE - sizeof(pldm_entity)),
+		new_record->data, new_record->size);
 	if (rc) {
 		goto cleanup_new_record_data;
 	}
@@ -1885,9 +1886,9 @@ int pldm_entity_association_pdr_remove_contained_entity(
 		goto cleanup_new_record_data;
 	}
 	if (num_children == 1) {
-		prev->next = record->next;
-		free(record->data);
-		free(record);
+		// This is the last child which is getting removed so we need to delete the Entity Association PDR.
+                prev = pldm_pdr_get_prev_record(repo, record);
+	        pldm_pdr_remove_record(repo, record, prev);
 		goto cleanup_new_record_data;
 	} else if (num_children < 1) {
 		rc = -EOVERFLOW;
@@ -2029,6 +2030,75 @@ static int pldm_pdr_remove_record(pldm_pdr *repo, pldm_pdr_record *record,
 	free(record);
 
 	return 0;
+}
+
+LIBPLDM_ABI_STABLE
+void pldm_delete_by_record_handle(pldm_pdr *repo, uint32_t record_handle,
+                                  bool is_remote)
+{
+        assert(repo != NULL);
+
+        pldm_pdr_record *record = repo->first;
+        pldm_pdr_record *prev = NULL;
+        while (record != NULL) {
+                pldm_pdr_record *next = record->next;
+                struct pldm_pdr_hdr *hdr = (struct pldm_pdr_hdr *)record->data;
+                if ((record->is_remote == is_remote) &&
+                    (hdr->record_handle == record_handle)) {
+                        if (repo->first == record) {
+                                repo->first = next;
+                        } else {
+                                prev->next = next;
+                        }
+                        if (repo->last == record) {
+                                repo->last = prev;
+                        }
+                        if (record->data) {
+                                free(record->data);
+                        }
+                        --repo->record_count;
+                        repo->size -= record->size;
+                        free(record);
+                        break;
+                }
+                prev = record;
+                record = next;
+        }
+}
+
+/* API to remove PLDM PDR record from a PLDM PDR repository
+ */
+static int pldm_pdr_remove_record(pldm_pdr *repo, pldm_pdr_record *record,
+                                  pldm_pdr_record *prev)
+{
+        if (!repo || !record) {
+                return -EINVAL;
+        }
+        if (!is_prev_record_present(repo, record)) {
+                return -EINVAL;
+        }
+        assert(repo->size >= record->size);
+        if (repo->size < record->size) {
+                return -EOVERFLOW;
+        }
+        if (repo->first == record) {
+                repo->first = record->next;
+        } else {
+                if (prev != NULL) {
+                        prev->next = record->next;
+                }
+        }
+        if (repo->last == record) {
+                repo->last = prev;
+                if (prev != NULL) {
+                        prev->next = NULL;
+                }
+        }
+        repo->record_count -= 1;
+        repo->size -= record->size;
+        free(record->data);
+        free(record);
+        return 0;
 }
 
 LIBPLDM_ABI_TESTING
