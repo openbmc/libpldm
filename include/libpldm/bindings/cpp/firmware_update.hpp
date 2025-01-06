3cfa5f85c1c7b4e814b4d3b9cb6b6f6be4e2f9a7
Alexander Hansen <alexander.hansen@9elements.com>
2025-01-06 15:06:25 +0100

    bindings: C++ binding for parsing PLDM fw package
    This binding enables convenient parsing of the PLDM fw update package in
    C++ code. The entire package header is parsed and can be queried.
    
    It has since been modified to remove exceptions and rely less on mutable
    instance variables.
    
    The ABI compatibility of the C++ bindings is controlled in the same way
    as the C code, with the LIBPLDM_ABI_* annotations on all public
    functions.
    
    References:
    [1] https://github.com/openbmc/pldm/blob/a1871174860e8927d19a0d57ac4cf9d55b0514ca/fw-update/package_parser.cpp
    
    Change-Id: Ib6cbff302be0e52d30125bcf08687ba42f7fc93f
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ include/libpldm/bindings/cpp/firmware_update.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "types.hpp"
+
+#include <libpldm/firmware_update.h>
+
+#include <cstdint>
+#include <stdexcept>
+#include <vector>
+#include <expected>
+#include <memory>
+
+namespace pldm
+{
+
+namespace fw_update
+{
+
+	class PackageParserError {
+	    public:
+		PackageParserError(std::string &s);
+
+		std::string msg;
+	};
+
+	struct Package {
+		const std::vector<FirmwareDeviceIDRecord> fwDeviceIDRecords;
+
+		const std::vector<ComponentImageInfo> componentImageInfos;
+	};
+
+	class PackageParser {
+	    public:
+		PackageParser() = delete;
+		PackageParser(const PackageParser &) = delete;
+		PackageParser(PackageParser &&) = default;
+		PackageParser &operator=(const PackageParser &) = delete;
+		PackageParser &operator=(PackageParser &&) = delete;
+		virtual ~PackageParser() = default;
+
+		/** @brief Parse the firmware update package
+		 *
+		 *  @param[in] pkg - Pointer to pldm package
+		 *  @param[in] pkgSize - Size of the firmware update package
+		 *
+		 *  @returns an error value if parsing fails
+		 *  @returns a unique_ptr to Package struct on success
+		 */
+		LIBPLDM_ABI_TESTING
+		static std::expected<std::unique_ptr<Package>,
+				     PackageParserError>
+		parse(const std::vector<uint8_t> &pkg, size_t pkgSize) noexcept;
+	};
+
+} // namespace fw_update
+
+} // namespace pldm
