openbmc/libpldm

commit e42490575af6f6114b09fde852b9be3d731aeffc

oem: ibm: Add PCIe slot sensor/effector StateSetId
Adds sensor and effecter state set IDs for all the
PCIe Slots & adapters present on BMC. Possible
conditions of the PLDM_ENTITY_SLOT type of entity
are defined by PLDM_OEM_IBM_SLOT_ENABLE_SENSOR_STATE.

Slot sensor state set ID is required to find the sensor
state of the PCIE slot. Sensor state value of a PCIE
slot can be enabled, disabled or unknown.

PCIE slot is enabled with the effector state set ID
defined with this commit.

Tested By: Using pldmtool set effecter state and read
sensor state for PCIE slot was verified for this change

1.pldmtool platform SetStateEffecterStates -i 124 -c 1 -d 01 02 -v
2.pldmtool platform GetStateSensorReadings -i 208 -r 01

Change-Id: I87f4f76ff6f3dc6e15b4677ad42e7b28e1c40aa2
Signed-off-by: Varsha Kaverappa <vkaverap@in.ibm.com>

include/libpldm/oem/ibm/state_set.h
@@ -13,7 +13,22 @@ enum ibm_oem_pldm_state_set_ids {
 	PLDM_OEM_IBM_VERIFICATION_STATE = 32770,
 	PLDM_OEM_IBM_SYSTEM_POWER_STATE = 32771,
 	PLDM_OEM_IBM_SBE_MAINTENANCE_STATE = 32772,
+	PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_STATE = 32773,
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE = 32774,
 	PLDM_OEM_IBM_SBE_HRESET_STATE = 32776,
+};
+
+enum pldm_oem_ibm_pcie_slot_effecter_state {
+	PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_ADD = 0x1,
+	PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_REMOVE = 0x2,
+	PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_REPLACE = 0x3,
+};
+
+enum pldm_oem_ibm_pcie_slot_sensor_state {
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE_UNKOWN = 0x0,
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE_ENABLED = 0x1,
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE_DISABLED = 0x2,
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE_ERROR = 0x3,
 };
 
 enum ibm_oem_pldm_state_set_firmware_update_state_values {

commit b2df05da6737068c98b129522352a8f52739fa73

oem: ibm: Update the slot sensor and effecter state set id
Changing the state sets ids for slot sensor and effecter to match
the IBM id used by remote IBM PLDM terminus.

Change-Id: If5316538f5396c564b407d03e20f56a19722a673
gitlint-ignore: B1, UC1
Fixes: e42490575af6("oem: ibm: Add PCIe slot sensor/effector StateSetId")
Signed-off-by: Archana Kakani <archana.kakani@ibm.com>

include/libpldm/oem/ibm/state_set.h
@@ -13,9 +13,9 @@ enum ibm_oem_pldm_state_set_ids {
 	PLDM_OEM_IBM_VERIFICATION_STATE = 32770,
 	PLDM_OEM_IBM_SYSTEM_POWER_STATE = 32771,
 	PLDM_OEM_IBM_SBE_MAINTENANCE_STATE = 32772,
-	PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_STATE = 32773,
-	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE = 32774,
 	PLDM_OEM_IBM_SBE_HRESET_STATE = 32776,
+        PLDM_OEM_IBM_PCIE_SLOT_EFFECTER_STATE = 32779,
+	PLDM_OEM_IBM_PCIE_SLOT_SENSOR_STATE = 32780
 	PLDM_OEM_IBM_PCIE_TOPOLOGY_ACTIONS = 32781,
 };
 