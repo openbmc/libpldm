9a028e82e8fe4379b8836f6917bd25975c4d5dad
Chau Ly <chaul@amperecomputing.com>
2025-03-26 08:26:49 +0000

    dsp: file: Add encode req & decode resp for DfHeartbeat command
    Added encode/decode APIs for DfHeartbeat command(0x03)
    which is defined in DSP0242 Version 1.0.0 Section: 9.6.
    
    Change-Id: Icf8ccbb57da74182f4fd0cc0cf49a49861abfbc1
    Signed-off-by: Chau Ly <chaul@amperecomputing.com>

--- src/dsp/file.c
+++ src/dsp/file.c
@@ -124,3 +124,67 @@ int decode_df_close_resp(const struct pldm_msg *msg, size_t payload_length,
 
 	return 0;
 }
+
+LIBPLDM_ABI_TESTING
+int encode_df_heartbeat_req(uint8_t instance_id,
+			    const struct pldm_file_df_heartbeat_req *req,
+			    struct pldm_msg *msg, size_t payload_length)
+{
+	PLDM_MSGBUF_DEFINE_P(buf);
+	int rc;
+
+	if (!req || !msg) {
+		return -EINVAL;
+	}
+
+	struct pldm_header_info header = { 0 };
+	header.instance = instance_id;
+	header.msg_type = PLDM_REQUEST;
+	header.pldm_type = PLDM_FILE;
+	header.command = PLDM_FILE_CMD_DF_HEARTBEAT;
+
+	rc = pack_pldm_header_errno(&header, &(msg->hdr));
+	if (rc) {
+		return rc;
+	}
+
+	rc = pldm_msgbuf_init_errno(buf, PLDM_DF_HEARTBEAT_REQ_BYTES,
+				    msg->payload, payload_length);
+	if (rc) {
+		return rc;
+	}
+
+	pldm_msgbuf_insert(buf, req->file_descriptor);
+	pldm_msgbuf_insert(buf, req->requester_max_interval);
+
+	return pldm_msgbuf_complete(buf);
+}
+
+LIBPLDM_ABI_TESTING
+int decode_df_heartbeat_resp(const struct pldm_msg *msg, size_t payload_length,
+			     struct pldm_file_df_heartbeat_resp *resp)
+{
+	PLDM_MSGBUF_DEFINE_P(buf);
+	int rc;
+
+	if (!msg || !resp) {
+		return -EINVAL;
+	}
+
+	rc = pldm_msg_has_error(msg, payload_length);
+	if (rc) {
+		resp->completion_code = rc;
+		return 0;
+	}
+
+	rc = pldm_msgbuf_init_errno(buf, PLDM_DF_HEARTBEAT_RESP_BYTES,
+				    msg->payload, payload_length);
+	if (rc) {
+		return rc;
+	}
+
+	pldm_msgbuf_extract(buf, resp->completion_code);
+	pldm_msgbuf_extract(buf, resp->responder_max_interval);
+
+	return pldm_msgbuf_complete_consumed(buf);
+}
