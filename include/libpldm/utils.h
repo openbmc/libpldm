1978579a2beaaa9484a2c7d2f27a75000fee4d2d
Kasun Athukorala <kasunath@google.com>
2025-06-19 01:02:38 +0000

    utils: Add an API to calculate cumulative crc32
    Change-Id: I6c6fce214b98bc8f7284bc3e58319a9ef659e1b0
    Signed-off-by: Kasun Athukorala <kasunath@google.com>

--- include/libpldm/utils.h
+++ include/libpldm/utils.h
@@ -38,6 +38,16 @@ uint8_t pldm_edac_crc8(const void *data, size_t size);
  */
 uint32_t pldm_edac_crc32(const void *data, size_t size);
 
+/** @brief Compute cumulative crc32 (same as the one used by IEEE802.3)
+ *
+ *  @param[in] data - Pointer to the target data
+ *  @param[in] size - Size of the data
+ *  @param[in] crc - crc value intialized through pldm_edac_crc32
+ *  @return The checksum
+ */
+uint32_t pldm_edac_cumulative_crc32(const void *data, size_t size,
+				    uint32_t crc);
+
 /** @brief Convert ver32_t to string
  *  @param[in] version - Pointer to ver32_t
  *  @param[out] buffer - Pointer to the buffer
