c720bc6db780c89455d0dcb4b65e90d1c3e81fef
Carter Chen <carter.chen.wiwynn@gmail.com>
2025-06-25 10:33:31 +0800

    Added the CRC checksum validation functionality
    - Added the validation functions for `pldm_edac_crc32_validate`.
    
    Change-Id: Iffd245fe5cbcadb1037cd1f5065ccc1da6502562
    Signed-off-by: Carter Chen <carter.chen.wiwynn@gmail.com>

---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#include <errno.h>
+/**
+ * @brief Validate the CRC32 checksum of the given data.
+ *
+ * @param[in] expected  The expected CRC32 value.
+ * @param[in] data      Pointer to the data to validate.
+ * @param[in] size      Size of the data in bytes.
+ * @return  0           if the checksum matches,
+ *          -EUCLEAN    if the checksum mismatches,
+ *          -EFAULT     if the arguments are invalid
+ *                     (e.g., data is NULL and size is not zero)
+ */
+int pldm_edac_crc32_validate(uint32_t expected, const void *data, size_t size);
