9b097a6f5e17ad276743deba075724139a1f7696
Amithash Prasad <amithash@meta.com>
2025-09-10 14:44:08 -0700

    utils: Refactor time utils from transport to utils
    Moves the time (get/validate) functions out of transport layer and
    into a common utils layer so they may be explicitly overridden and
    tested.
    
    Change-Id: I615b25805fc7a24eb5d67e87237bf35c8e262b93
    Signed-off-by: Amithash Prasad <amithash@meta.com>

--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,7 @@
 #include <libpldm/utils.h>
 #include "utils.h"
 
+#include <time.h>
 #include <limits.h>
 #include <stdio.h>
 
@@ -274,4 +275,47 @@ bool is_transfer_flag_valid(uint8_t transfer_flag)
 	default:
 		return false;
 	}
+}
+
+static void timespec_to_timeval(const struct timespec *ts, struct timeval *tv)
+{
+	tv->tv_sec = ts->tv_sec;
+	tv->tv_usec = ts->tv_nsec / 1000;
+}
+
+/* Overflow safety must be upheld before call */
+long libpldm_timeval_to_msec(const struct timeval *tv)
+{
+	return tv->tv_sec * 1000 + tv->tv_usec / 1000;
+}
+
+/* If calculations on `tv` don't overflow then operations on derived
+ * intervals can't either.
+ */
+bool libpldm_timeval_is_valid(const struct timeval *tv)
+{
+	if (tv->tv_sec < 0 || tv->tv_usec < 0 || tv->tv_usec >= 1000000) {
+		return false;
+	}
+
+	if (tv->tv_sec > (LONG_MAX - tv->tv_usec / 1000) / 1000) {
+		return false;
+	}
+
+	return true;
+}
+
+int __attribute__((weak)) libpldm_clock_gettimeval(struct timeval *tv)
+{
+	struct timespec now;
+	int rc;
+
+	rc = clock_gettime(CLOCK_MONOTONIC, &now);
+	if (rc < 0) {
+		return rc;
+	}
+
+	timespec_to_timeval(&now, tv);
+
+	return 0;
 }
