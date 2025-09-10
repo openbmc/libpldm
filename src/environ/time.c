107469cf2120c135eba5500c3909f42d7d66ddb7
Amithash Prasad <amithash@meta.com>
2025-09-10 14:44:08 -0700

    transport: Extract time-related APIs
    Move the time-related functions out of transport layer so they may be
    explicitly overridden and tested.
    
    Change-Id: I615b25805fc7a24eb5d67e87237bf35c8e262b93
    Signed-off-by: Amithash Prasad <amithash@meta.com>
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

---
+++ src/environ/time.c
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later */
+#include "environ/time.h"
+
+#include <time.h>
+#include <limits.h>
+
+static void timespec_to_timeval(const struct timespec *ts, struct timeval *tv)
+{
+	tv->tv_sec = ts->tv_sec;
+	tv->tv_usec = ts->tv_nsec / 1000;
+}
+
+LIBPLDM_ABI_TESTING
+__attribute__((weak)) int libpldm_clock_gettimeval(struct timeval *tv)
+{
+	struct timespec now;
+	int rc;
+
+	rc = clock_gettime(CLOCK_MONOTONIC, &now);
+	if (rc < 0) {
+		return rc;
+	}
+
+	timespec_to_timeval(&now, tv);
+
+	return 0;
+}
