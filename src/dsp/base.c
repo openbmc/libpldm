openbmc/libpldm

commit cb987c5c8863875c23c96abab7d9c11d9f92873b

dsp: base: Added decode_pldm_base_negotiate_transfer_params_req()
This command is based on DSP0240 v1.2.0 section 9.6.2
NegotiateTransferParameters (0x07).

gitlint-ignore: T1
Change-Id: I4fa193444df9ba5b109ca063163d7f7d109fcbda
Signed-off-by: Kasun Athukorala <kasunath@google.com>

src/dsp/base.c
@@ -832,7 +832,39 @@ int encode_pldm_base_negotiate_transfer_params_req(
 	return pldm_msgbuf_complete_used(buf, *payload_length, payload_length);
 }
 
-LIBPLDM_ABI_STABLE
+LIBPLDM_ABI_TESTING
+int decode_pldm_base_negotiate_transfer_params_req(
+	const struct pldm_msg *msg, size_t payload_length,
+	struct pldm_base_negotiate_transfer_params_req *req)
+{
+	PLDM_MSGBUF_RO_DEFINE_P(buf);
+	int rc;
+
+	if (msg == NULL || req == NULL) {
+		return -EINVAL;
+	}
+
+	rc = pldm_msgbuf_init_errno(
+		buf, PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES,
+		msg->payload, payload_length);
+	if (rc) {
+		return rc;
+	}
+
+	pldm_msgbuf_extract(buf, req->requester_part_size);
+
+	rc = pldm_msgbuf_extract_array(
+		buf, sizeof(req->requester_protocol_support),
+		(uint8_t *)req->requester_protocol_support,
+		sizeof(req->requester_protocol_support));
+	if (rc) {
+		return pldm_msgbuf_discard(buf, rc);
+	}
+
+	return pldm_msgbuf_complete_consumed(buf);
+}
+
+LIBPLDM_ABI_TESTING
 int decode_pldm_base_negotiate_transfer_params_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_base_negotiate_transfer_params_resp *resp)

include/libpldm/base.h
@@ -815,6 +815,24 @@ int encode_pldm_base_negotiate_transfer_params_req(
 	const struct pldm_base_negotiate_transfer_params_req *req,
 	struct pldm_msg *msg, size_t *payload_length);
 
+/** @brief Decode a PLDM Negotiate Transfer Parameters request message
+ *
+ *  @param[in] msg - Request message
+ *  @param[in] payload_length - length of request message payload
+ *  @param[out] req - pointer to the decoded request message
+ *  @return 0 on success
+ *          -EINVAL if the input parameters' memory are not allocated
+ *          -EOVERFLOW if the input message buffer is too short for the output
+ *          request struct
+ *          -EBADMSG if the input message buffer is too large for the output
+ *          request struct
+ *  @note  Caller is responsible for memory alloc and dealloc of param
+ *         'msg.payload'
+ */
+int decode_pldm_base_negotiate_transfer_params_req(
+	const struct pldm_msg *msg, size_t payload_length,
+	struct pldm_base_negotiate_transfer_params_req *req);
+
 /** @brief Decode a PLDM Negotiate Transfer Parameters response message
  *
  *  @param[in] msg - Response message

tests/dsp/base.cpp
@@ -1741,3 +1741,95 @@ TEST(DecodeNegotiateTransferParamsResponse,
     EXPECT_EQ(rc, -EOVERFLOW);
 }
 #endif
+
+#ifdef LIBPLDM_API_TESTING
+TEST(NegotiateTransferParams, TestDecodeNegotiateTransferParamsReqPass)
+{
+    // Prepare a sample request message
+    uint8_t instance_id = 0x0A;
+    uint16_t requester_part_size = 1024; // 0x0400
+    bitfield8_t requester_protocol_support[8];
+    for (int i = 0; i < 8; ++i)
+    {
+        requester_protocol_support[i].byte = static_cast<uint8_t>(i + 1);
+    }
+
+    PLDM_MSG_DEFINE_P(request,
+                      PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES);
+
+    // Build the request using the an appropriate API
+    struct pldm_base_negotiate_transfer_params_req req;
+    req.requester_part_size = requester_part_size;
+    memcpy(req.requester_protocol_support, requester_protocol_support,
+           sizeof(requester_protocol_support));
+
+    size_t req_payload_len = PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES;
+    ASSERT_EQ(encode_pldm_base_negotiate_transfer_params_req(
+                  instance_id, &req, request, &req_payload_len),
+              0);
+
+    struct pldm_base_negotiate_transfer_params_req decoded_req;
+    size_t payload_len = PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES;
+
+    // Successful decode
+    int rc = decode_pldm_base_negotiate_transfer_params_req(
+        request, payload_len, &decoded_req);
+    ASSERT_EQ(rc, 0);
+    EXPECT_EQ(decoded_req.requester_part_size, requester_part_size);
+    for (int i = 0; i < 8; ++i)
+    {
+        EXPECT_EQ(decoded_req.requester_protocol_support[i].byte,
+                  requester_protocol_support[i].byte);
+    }
+}
+#endif
+
+#ifdef LIBPLDM_API_TESTING
+TEST(NegotiateTransferParams, TestDecodeNegotiateTransferParamsReqFail)
+{
+    // Prepare a sample request message
+    uint8_t instance_id = 0x0A;
+    uint16_t requester_part_size = 1024; // 0x0400
+    bitfield8_t requester_protocol_support[8];
+    for (int i = 0; i < 8; ++i)
+    {
+        requester_protocol_support[i].byte = static_cast<uint8_t>(i + 1);
+    }
+
+    PLDM_MSG_DEFINE_P(request,
+                      PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES);
+
+    // Build the request using the an appropriate API
+    struct pldm_base_negotiate_transfer_params_req req;
+    req.requester_part_size = requester_part_size;
+    memcpy(req.requester_protocol_support, requester_protocol_support,
+           sizeof(requester_protocol_support));
+
+    size_t req_payload_len = PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES;
+    ASSERT_EQ(encode_pldm_base_negotiate_transfer_params_req(
+                  instance_id, &req, request, &req_payload_len),
+              0);
+
+    struct pldm_base_negotiate_transfer_params_req decoded_req;
+    size_t payload_len = PLDM_BASE_NEGOTIATE_TRANSFER_PARAMETERS_REQ_BYTES;
+    int rc = 0;
+
+    // Null arguments
+    rc = decode_pldm_base_negotiate_transfer_params_req(nullptr, payload_len,
+                                                        &decoded_req);
+    EXPECT_EQ(rc, -EINVAL);
+    rc = decode_pldm_base_negotiate_transfer_params_req(request, payload_len,
+                                                        nullptr);
+    EXPECT_EQ(rc, -EINVAL);
+
+    // Incorrect payload length - too short
+    rc = decode_pldm_base_negotiate_transfer_params_req(
+        request, payload_len - 1, &decoded_req);
+    EXPECT_EQ(rc, -EOVERFLOW);
+
+    // Incorrect payload length - too long
+    rc = decode_pldm_base_negotiate_transfer_params_req(
+        request, payload_len + 1, &decoded_req);
+    EXPECT_EQ(rc, -EBADMSG);
+}
+#endif

commit 3c522a9c9218c64ce64668cd73e949e795e38069

dsp: base: decode_pldm_base_negotiate_transfer_params_resp() is stable
cb987c5c8863 ("dsp: base: Added
decode_pldm_base_negotiate_transfer_params_req()") accidentally
set `decode_pldm_base_negotiate_transfer_params_resp()` back to
`LIBPLDM_ABI_TESTING` when it was stabilized by 26c8eb2a2f22 ("Stabilize
PLDM File Transfer related APIs").

Further, the existing integration of abi-compliance-checker was unable
to find the fault as the generated dump contained testing symbols, and
so decode_pldm_base_negotiate_transfer_params_resp() wasn't found to
be absent. We rework scripts/pre-submit such that this kind of problem
results in a CI failure.

Change-Id: I27d48e275646bbd75f19a3e1fe309a584d893cf2
Fixes: cb987c5c8863 ("dsp: base: Added decode_pldm_base_negotiate_transfer_params_req()")
Signed-off-by: Chau Ly <chaul@amperecomputing.com>
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

meson.options
@@ -9,6 +9,7 @@ option(
     'abi-compliance-check',
     type: 'boolean',
     description: 'Detect public ABI/API changes',
+    value: false,
 )
 option(
     'oem',

scripts/pre-submit
@@ -29,7 +29,7 @@ fi
 # Ensure the test suite passes in the default configuration. Note
 # that we don't specify -Dabi=... - the default is equivalent to
 # -Dabi=deprecated,stable,testing.
-CC=gcc CXX=g++ CFLAGS=-fanalyzer meson setup -Dabi-compliance-check=false "$BUILD"
+CC=gcc CXX=g++ CFLAGS=-fanalyzer meson setup "$BUILD"
 meson compile -C "$BUILD"
 meson test -C "$BUILD"
 
@@ -42,13 +42,18 @@ meson configure --buildtype=release "$BUILD"
 meson compile -C "$BUILD"
 meson test -C "$BUILD" --timeout-multiplier 10 --wrapper 'valgrind --error-exitcode=1'
 
-# Ensure the test suite links when testing symbols are removed from the ABI
+# Ensure that the test suite doesn't fail to link against the stable ABI, and
+# the stable ABI matches what's recorded
 meson configure --buildtype=debug "$BUILD"
 meson configure -Dabi=deprecated,stable "$BUILD"
+! command -v abi-compliance-checker > /dev/null ||
+  meson configure -Dabi-compliance-check=true "$BUILD"
 meson compile -C "$BUILD"
 meson test -C "$BUILD"
 
 # Ensure the build completes for maintenance purposes. Note that tests are
 # disabled as we don't yet guard them appropriately.
-meson configure -Dabi=stable,testing -Dtests=false "$BUILD"
+meson configure -Dabi=stable,testing "$BUILD"
+meson configure -Dabi-compliance-check=false "$BUILD"
+meson configure -Dtests=false "$BUILD"
 meson compile -C "$BUILD"

src/dsp/base.c
@@ -914,7 +914,7 @@ int decode_pldm_base_negotiate_transfer_params_req(
 	return pldm_msgbuf_complete_consumed(buf);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_pldm_base_negotiate_transfer_params_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_base_negotiate_transfer_params_resp *resp)