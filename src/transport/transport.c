openbmc/libpldm

commit 69b5a9cde88b0aee5a910c0b8094609371297000

transport: Improve time validation in pldm_transport_send_recv_msg()
There is at least one system where the current value returned by
CLOCK_MONOTONIC is 2589793 which fails the overflow test in
`timeval_is_valid`. Upon close inspecting we dont really want to test
the actual end time but rather the difference (remaining) since the
real risk we are trying to avoid is overflowing when the call to
`timeval_to_msec()` is made.

So, move the check to test `remaining` instead of `end`.

Change-Id: If6533855025ff3ba0be19bad791f58ca812b80a7
Fixes: abaf61f45e2a ("transport: Prevent sticking in waiting for response")
Signed-off-by: Amithash Prasad <amithash@meta.com>
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/transport/transport.c
@@ -116,13 +116,15 @@ static long timeval_to_msec(const struct timeval *tv)
 /* If calculations on `tv` don't overflow then operations on derived
  * intervals can't either.
  */
-static bool timeval_is_valid(const struct timeval *tv)
+static bool timeval_validate_for_msec(const struct timeval *tv)
 {
+	/* Must be a normalised, positive interval */
 	if (tv->tv_sec < 0 || tv->tv_usec < 0 || tv->tv_usec >= 1000000) {
 		return false;
 	}
 
-	if (tv->tv_sec > (LONG_MAX - tv->tv_usec / 1000) / 1000) {
+	/* Components must safely convert to msec */
+	if (tv->tv_sec > ((LONG_MAX / 1000) - 1000)) {
 		return false;
 	}
 
@@ -203,14 +205,15 @@ pldm_transport_send_recv_msg(struct pldm_transport *transport, pldm_tid_t tid,
 	}
 
 	timeradd(&now, &max_response_interval, &end);
-	if (!timeval_is_valid(&end)) {
-		return PLDM_REQUESTER_POLL_FAIL;
-	}
 
 	while (timercmp(&now, &end, <)) {
 		pldm_tid_t src_tid;
 
 		timersub(&end, &now, &remaining);
+
+		if (!timeval_validate_for_msec(&remaining)) {
+			return PLDM_REQUESTER_POLL_FAIL;
+		}
 
 		/* 0 <= `timeval_to_msec()` <= 4800, and 4800 < INT_MAX */
 		ret = pldm_transport_poll(transport,

tests/transport/send_recv_one.cpp
@@ -5,8 +5,47 @@
 
 #include <gtest/gtest.h>
 
+extern "C" {
+#include "environ/time.h"
+
+long global_base_time = 0;
+int libpldm_clock_gettime(clockid_t clockid, struct timespec* ts)
+{
+    static struct timespec init_offset{};
+    int rc;
+
+    rc = clock_gettime(clockid, ts);
+    if (rc < 0)
+    {
+        return rc;
+    }
+
+    if (init_offset.tv_sec == 0)
+    {
+        init_offset = *ts;
+    }
+
+    /*
+     * Adjust the time such that the start of the test begins at an artificial
+     * global_base_time which can be controlled from any of the TEST() methods
+     */
+    ts->tv_sec = ts->tv_sec - init_offset.tv_sec + global_base_time;
+
+    return 0;
+}
+}
+
 TEST(Transport, send_recv_one)
 {
+    /*
+     * To test the case when timestamp is closer to the 28 day uptime we would
+     * potentially set this to something closer to 2589793. But unfortunatley,
+     * the systems where the unit tests would be run, could have long be a 64
+     * bit integer thus would pass with this anyway but fail on a standard BMC
+     * 32bit SOC. Hence workaround this by using `LONG_MAX - 10` which would
+     * fail on either condition.
+     */
+    global_base_time = LONG_MAX - 10;
     uint8_t req[] = {0x81, 0x00, 0x01, 0x01};
     uint8_t resp[] = {0x01, 0x00, 0x01, 0x00};
     const struct pldm_transport_test_descriptor seq[] = {

commit 9f6b9b3aca29a022c597a87f445d487d8020fe51

transport: Fix msec overflow test
The value 1000 was left in the wrong spot, leading to an incorrect
inversion of the msec calculation when testing for overflow. An
alternative is to reduce it to 1, but that correlates poorly with the
expression used by timeval_to_msec().

Reported-by: Milton Miller <mdmii@outlook.com>
Fixes: 69b5a9cde88b ("transport: Improve time validation in pldm_transport_send_recv_msg()")
Change-Id: I59816c6f2b773dfd076e393455394f08cfe67198
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/transport/transport.c
@@ -124,7 +124,7 @@ static bool timeval_validate_for_msec(const struct timeval *tv)
 	}
 
 	/* Components must safely convert to msec */
-	if (tv->tv_sec > ((LONG_MAX / 1000) - 1000)) {
+	if (tv->tv_sec > ((LONG_MAX - 1000) / 1000)) {
 		return false;
 	}
 