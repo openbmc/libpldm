openbmc/libpldm

commit abaf61f45e2a49ff668dd5a83a6ec96e02efd972

transport: Prevent sticking in waiting for response
When the connection has an issue, one request may not receive the
response. The pldm_transport_send_recv_msg should not be stuck in
while loop to wait for the response.
As the section "Requirements for requesters" in DSP0240 PLDM Base
specs, the maximum value of "Time-out waiting for a response" PT2max
is 4800ms. This commit uses that time out to prevent
pldm_transport_send_recv_msg from forever waiting for the response.

Signed-off-by: Dung Cao <dung@os.amperecomputing.com>
Signed-off-by: Thu Nguyen <thu@os.amperecomputing.com>
Change-Id: I70b62b1ff30a8a715716db43867b479a5b214dfa

src/transport/transport.c
@@ -3,10 +3,15 @@
 #include "libpldm/requester/pldm.h"
 #include "transport.h"
 
+#include <errno.h>
+#include <limits.h>
 #ifdef PLDM_HAS_POLL
 #include <poll.h>
 #endif
+#include <stdbool.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 
 #ifndef PLDM_HAS_POLL
@@ -98,33 +103,108 @@ pldm_requester_rc_t pldm_transport_recv_msg(struct pldm_transport *transport,
 	return PLDM_REQUESTER_SUCCESS;
 }
 
+static void timespec_to_timeval(const struct timespec *ts, struct timeval *tv)
+{
+	tv->tv_sec = ts->tv_sec;
+	tv->tv_usec = ts->tv_nsec / 1000;
+}
+
+/* Overflow safety must be upheld before call */
+static long timeval_to_msec(const struct timeval *tv)
+{
+	return tv->tv_sec * 1000 + tv->tv_usec / 1000;
+}
+
+/* If calculations on `tv` don't overflow then operations on derived
+ * intervals can't either.
+ */
+static bool timeval_is_valid(const struct timeval *tv)
+{
+	if (tv->tv_sec < 0 || tv->tv_usec < 0 || tv->tv_usec >= 1000000) {
+		return false;
+	}
+
+	if (tv->tv_sec > (LONG_MAX - tv->tv_usec / 1000) / 1000) {
+		return false;
+	}
+
+	return true;
+}
+
+static int clock_gettimeval(clockid_t clockid, struct timeval *tv)
+{
+	struct timespec now;
+	int rc;
+
+	rc = clock_gettime(clockid, &now);
+	if (rc < 0) {
+		return rc;
+	}
+
+	timespec_to_timeval(&now, tv);
+
+	return 0;
+}
+
 pldm_requester_rc_t
 pldm_transport_send_recv_msg(struct pldm_transport *transport, pldm_tid_t tid,
 			     const void *pldm_req_msg, size_t req_msg_len,
 			     void **pldm_resp_msg, size_t *resp_msg_len)
 
 {
+	/**
+	 * Section "Requirements for requesters" in DSP0240, define the Time-out
+	 * waiting for a response of the requester.
+	 * PT2max = PT3min - 2*PT4max = 4800ms
+	 */
+	static const struct timeval max_response_interval = {
+		.tv_sec = 4, .tv_usec = 800000
+	};
+	struct timeval remaining;
+	struct timeval now;
+	struct timeval end;
+	pldm_requester_rc_t rc;
+	int ret;
+
 	if (!resp_msg_len) {
 		return PLDM_REQUESTER_INVALID_SETUP;
 	}
 
-	pldm_requester_rc_t rc = pldm_transport_send_msg(
-		transport, tid, pldm_req_msg, req_msg_len);
+	rc = pldm_transport_send_msg(transport, tid, pldm_req_msg, req_msg_len);
 	if (rc != PLDM_REQUESTER_SUCCESS) {
 		return rc;
 	}
 
-	while (1) {
-		rc = pldm_transport_poll(transport, -1);
+	ret = clock_gettimeval(CLOCK_MONOTONIC, &now);
+	if (ret < 0) {
+		return PLDM_REQUESTER_POLL_FAIL;
+	}
+
+	timeradd(&now, &max_response_interval, &end);
+	if (!timeval_is_valid(&end)) {
+		return PLDM_REQUESTER_POLL_FAIL;
+	}
+
+	do {
+		timersub(&end, &now, &remaining);
+		/* 0 <= `timeval_to_msec()` <= 4800, and 4800 < INT_MAX */
+		rc = pldm_transport_poll(transport,
+					 (int)(timeval_to_msec(&remaining)));
 		if (rc != PLDM_REQUESTER_SUCCESS) {
-			break;
+			return rc;
 		}
+
 		rc = pldm_transport_recv_msg(transport, tid, pldm_resp_msg,
 					     resp_msg_len);
 		if (rc == PLDM_REQUESTER_SUCCESS) {
-			break;
+			return rc;
 		}
-	}
 
-	return rc;
+		ret = clock_gettimeval(CLOCK_MONOTONIC, &now);
+		if (ret < 0) {
+			return PLDM_REQUESTER_POLL_FAIL;
+		}
+	} while (!timercmp(&now, &end, <));
+
+	return PLDM_REQUESTER_RECV_FAIL;
 }

commit 081d0f6e3722c026f32df2fe8253403b41534d5a

transport: Improve time validation in pldm_transport_send_recv_msg()
There is at least one system where the current value returned by
CLOCK_MONOTONIC is 2589793 which fails the overflow test in
`timeval_is_valid`. Upon close inspecting we dont really want to test
the actual end time but rather the difference (remaining) since the
real risk we are trying to avoid is overflowing when the call to
`timeval_to_msec()` is made.

So, move the check to test `remaining` instead of `end`.

Change-Id: If6533855025ff3ba0be19bad791f58ca812b80a7
Fixes: abaf61f45e2a ("transport: Prevent sticking in waiting for response")
Signed-off-by: Amithash Prasad <amithash@meta.com>
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/transport/transport.c
@@ -116,13 +116,15 @@ static long timeval_to_msec(const struct timeval *tv)
 /* If calculations on `tv` don't overflow then operations on derived
  * intervals can't either.
  */
-static bool timeval_is_valid(const struct timeval *tv)
+static bool timeval_validate_for_msec(const struct timeval *tv)
 {
+	/* Must be a normalised, positive interval */
 	if (tv->tv_sec < 0 || tv->tv_usec < 0 || tv->tv_usec >= 1000000) {
 		return false;
 	}
 
-	if (tv->tv_sec > (LONG_MAX - tv->tv_usec / 1000) / 1000) {
+	/* Components must safely convert to msec */
+	if (tv->tv_sec > ((LONG_MAX / 1000) - 1000)) {
 		return false;
 	}
 
@@ -203,14 +205,15 @@ pldm_transport_send_recv_msg(struct pldm_transport *transport, pldm_tid_t tid,
 	}
 
 	timeradd(&now, &max_response_interval, &end);
-	if (!timeval_is_valid(&end)) {
-		return PLDM_REQUESTER_POLL_FAIL;
-	}
 
 	while (timercmp(&now, &end, <)) {
 		pldm_tid_t src_tid;
 
 		timersub(&end, &now, &remaining);
+
+		if (!timeval_validate_for_msec(&remaining)) {
+			return PLDM_REQUESTER_POLL_FAIL;
+		}
 
 		/* 0 <= `timeval_to_msec()` <= 4800, and 4800 < INT_MAX */
 		ret = pldm_transport_poll(transport,

tests/transport/send_recv_one.cpp
@@ -5,8 +5,47 @@
 
 #include <gtest/gtest.h>
 
+extern "C" {
+#include "environ/time.h"
+
+long global_base_time = 0;
+int libpldm_clock_gettime(clockid_t clockid, struct timespec* ts)
+{
+    static struct timespec init_offset{};
+    int rc;
+
+    rc = clock_gettime(clockid, ts);
+    if (rc < 0)
+    {
+        return rc;
+    }
+
+    if (init_offset.tv_sec == 0)
+    {
+        init_offset = *ts;
+    }
+
+    /*
+     * Adjust the time such that the start of the test begins at an artificial
+     * global_base_time which can be controlled from any of the TEST() methods
+     */
+    ts->tv_sec = ts->tv_sec - init_offset.tv_sec + global_base_time;
+
+    return 0;
+}
+}
+
 TEST(Transport, send_recv_one)
 {
+    /*
+     * To test the case when timestamp is closer to the 28 day uptime we would
+     * potentially set this to something closer to 2589793. But unfortunatley,
+     * the systems where the unit tests would be run, could have long be a 64
+     * bit integer thus would pass with this anyway but fail on a standard BMC
+     * 32bit SOC. Hence workaround this by using `LONG_MAX - 10` which would
+     * fail on either condition.
+     */
+    global_base_time = LONG_MAX - 10;
     uint8_t req[] = {0x81, 0x00, 0x01, 0x01};
     uint8_t resp[] = {0x01, 0x00, 0x01, 0x00};
     const struct pldm_transport_test_descriptor seq[] = {