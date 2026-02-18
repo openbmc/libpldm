openbmc/libpldm

commit 9a6ab390fdd7fb5b560711260ee6e91f7c68b4fe

state-set: Add new enums for state sets
Add enum values for several new state sets that are defined
in v1.4.0 of Platform Data Model (PLDM) State Set Specification
DSP0249 under the section 6.3 PLDM State Sets tables, particularly:
Add new enums:
- enum pldm_state_set_predictive_condition_values,
- enum pldm_state_set_configuration_state_values,
- enum pldm_state_set_changed_configuration_values,
- enum pldm_state_set_version_values,
- enum pldm_state_set_communication_leash_status_values,
- enum pldm_state_set_acpi_power_state_values.
Add new values to the following enums:
- enum pldm_state_set_availability_values,
- enum pldm_state_set_boot_progress_state_values.

Change-Id: Ie94cdb58676bda3ee0f6047a7ca78bad1329d543
Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

include/libpldm/state_set.h
@@ -161,7 +161,29 @@ enum pldm_state_set_health_state_values {
 /* @brief List of states for the State Set Availability (ID 2),
  */
 enum pldm_state_set_availability_values {
-	PLDM_STATE_SET_AVAILABILITY_REBOOTING = 8
+	PLDM_STATE_SET_AVAILABILITY_ENABLED = 1,
+	PLDM_STATE_SET_AVAILABILITY_DISABLED = 2,
+	PLDM_STATE_SET_AVAILABILITY_SHUTDOWN = 3,
+	PLDM_STATE_SET_AVAILABILITY_OFFLINE = 4,
+	PLDM_STATE_SET_AVAILABILITY_IN_TEST = 5,
+	PLDM_STATE_SET_AVAILABILITY_DEFERRED = 6,
+	PLDM_STATE_SET_AVAILABILITY_QUIESCENT = 7,
+	PLDM_STATE_SET_AVAILABILITY_REBOOTING = 8,
+	PLDM_STATE_SET_AVAILABILITY_RESETTING = 9,
+	PLDM_STATE_SET_AVAILABILITY_FAILED = 10,
+	PLDM_STATE_SET_AVAILABILITY_NOT_INSTALLED = 11,
+	PLDM_STATE_SET_AVAILABILITY_POWER_SAVE_MODE = 12,
+	PLDM_STATE_SET_AVAILABILITY_PAUSED = 13,
+	PLDM_STATE_SET_AVAILABILITY_SHUTTING_DOWN = 14,
+	PLDM_STATE_SET_AVAILABILITY_STARTING = 15,
+	PLDM_STATE_SET_AVAILABILITY_NOT_RESPONDING = 16,
+};
+
+/* @brief List of states for the Predictive Condition state set (ID 3),
+ */
+enum pldm_state_set_predictive_condition_values {
+	PLDM_STATE_SET_PREDICTIVE_CONDITION_NORMAL = 1,
+	PLDM_STATE_SET_PREDICTIVE_CONDITION_PREDICTIVE_FAILURE = 2,
 };
 
 /* @brief List of states for the Operational Stress status (ID 9).
@@ -196,11 +218,36 @@ enum pldm_state_set_presence_values {
 	PLDM_STATE_SET_PRESENCE_NOT_PRESENT = 2,
 };
 
+/* @brief List of states for the Configuration State state set (ID 15).
+ */
+enum pldm_state_set_configuration_state_values {
+	PLDM_STATE_SET_CONFIGURATION_STATE_VALID_CONFIGURATION = 1,
+	PLDM_STATE_SET_CONFIGURATION_STATE_INVALID_CONFIGURATION = 2,
+	PLDM_STATE_SET_CONFIGURATION_STATE_NOT_CONFIGURED = 3,
+	PLDM_STATE_SET_CONFIGURATION_STATE_MISSING_CONFIGURATION = 4,
+};
+
+/* @brief List of states for the Changed Configuration state set (ID 16).
+ */
+enum pldm_state_set_changed_configuration_values {
+	PLDM_STATE_SET_CHANGED_CONFIGURATION_NORMAL = 1,
+	PLDM_STATE_SET_CHANGED_CONFIGURATION_CONFIGURATION_CHANGE_DETECTED = 2,
+};
+
 /* @brief List of states for the Set Identify state (ID 17).
  */
 enum pldm_state_set_identify_state_values {
 	PLDM_STATE_SET_IDENTIFY_STATE_UNASSERTED = 1,
 	PLDM_STATE_SET_IDENTIFY_STATE_ASSERTED = 2,
+};
+
+/* @brief List of states for the Version state set (ID 18).
+ */
+enum pldm_state_set_version_values {
+	PLDM_STATE_SET_VERSION_NORMAL = 1,
+	PLDM_STATE_SET_VERSION_VERSION_CHANGE_DETECTED_COMPATIBLE = 2,
+	PLDM_STATE_SET_VERSION_VERSION_CHANGE_DETECTED_INCOMPATIBLE = 3,
+	PLDM_STATE_SET_VERSION_VERSION_CHANGE_PENDING = 4,
 };
 
 /* @brief List of states for the Set Thermal Trip state set (ID 21).
@@ -215,6 +262,13 @@ enum pldm_state_set_thermal_trip_values {
 enum pldm_state_set_link_state_values {
 	PLDM_STATE_SET_LINK_STATE_CONNECTED = 1,
 	PLDM_STATE_SET_LINK_STATE_DISCONNECTED = 2,
+};
+
+/* @brief List of states for the Communication Leash Status state set (ID 101).
+ */
+enum pldm_state_set_communication_leash_status_values {
+	PLDM_STATE_SET_COMMUNICATION_LEASH_STATUS_LEASH_CONNECTED = 1,
+	PLDM_STATE_SET_COMMUNICATION_LEASH_STATUS_LEASH_DISCONNECTED = 2,
 };
 
 /* @brief List of states for the Software-related state set (ID 129).
@@ -248,11 +302,28 @@ enum pldm_state_set_boot_progress_state_values {
 	PLDM_STATE_SET_BOOT_PROG_STATE_NOT_ACTIVE = 1,
 	PLDM_STATE_SET_BOOT_PROG_STATE_COMPLETED = 2,
 	PLDM_STATE_SET_BOOT_PROG_STATE_MEM_INITIALIZATION = 3,
+	PLDM_STATE_SET_BOOT_PROG_STATE_HARD_DISK_INITIALIZATION = 4,
 	PLDM_STATE_SET_BOOT_PROG_STATE_SEC_PROC_INITIALIZATION = 5,
-	PLDM_STATE_SET_BOOT_PROG_STATE_PCI_RESORUCE_CONFIG = 9,
+	PLDM_STATE_SET_BOOT_PROG_STATE_USER_AUTHENTICATION = 6,
+	PLDM_STATE_SET_BOOT_PROG_STATE_USER_INITIATED_SYSTEM_SETUP = 7,
+	PLDM_STATE_SET_BOOT_PROG_STATE_USB_RESOURCE_CONFIG = 8,
+	PLDM_STATE_SET_BOOT_PROG_STATE_PCI_RESOURCE_CONFIG = 9,
+	PLDM_STATE_SET_BOOT_PROG_STATE_OPTION_ROM_INITIALIZATION = 10,
+	PLDM_STATE_SET_BOOT_PROG_STATE_VIDEO_INITIALIZATION = 11,
+	PLDM_STATE_SET_BOOT_PROG_STATE_CACHE_INITIALIZATION = 12,
+	PLDM_STATE_SET_BOOT_PROG_STATE_SM_BUS_INITIALIZATION = 13,
+	PLDM_STATE_SET_BOOT_PROG_STATE_KEYBOARD_CONTROLLER_INITIALIZATION = 14,
+	PLDM_STATE_SET_BOOT_PROG_STATE_EC_MC_INITIALIZATION = 15,
+	PLDM_STATE_SET_BOOT_PROG_STATE_DOCKING_STATION_ATTACHEMENT = 16,
+	PLDM_STATE_SET_BOOT_PROG_STATE_ENABLING_DOCKING_STATION = 17,
+	PLDM_STATE_SET_BOOT_PROG_STATE_DOCKING_STATION_EJECTION = 18,
+	PLDM_STATE_SET_BOOT_PROG_STATE_DISABLING_DOCKING_STATION = 19,
 	PLDM_STATE_SET_BOOT_PROG_STATE_OSSTART = 20,
 	PLDM_STATE_SET_BOOT_PROG_STATE_STARTING_OP_SYS = 21,
 	PLDM_STATE_SET_BOOT_PROG_STATE_BASE_BOARD_INITIALIZATION = 22,
+	PLDM_STATE_SET_BOOT_PROG_STATE_FLOPPY_INITIALIZATION = 23,
+	PLDM_STATE_SET_BOOT_PROG_STATE_KEYBOARD_TEST = 24,
+	PLDM_STATE_SET_BOOT_PROG_STATE_POINTING_DEVICE_TEST = 25,
 	PLDM_STATE_SET_BOOT_PROG_STATE_PRIMARY_PROC_INITIALIZATION = 26,
 };
 
@@ -282,6 +353,18 @@ enum pldm_state_set_device_power_state_values {
 	PLDM_STATE_SET_ACPI_DEVICE_POWER_STATE_INTERMEDIATE_1 = 2,
 	PLDM_STATE_SET_ACPI_DEVICE_POWER_STATE_INTERMEDIATE_2 = 3,
 	PLDM_STATE_SET_ACPI_DEVICE_POWER_STATE_OFF = 4,
+};
+
+/* @brief List of states for ACPI Power State set (ID 258).
+ */
+enum pldm_state_set_acpi_power_state_values {
+	PLDM_STATE_SET_ACPI_POWER_STATE_S0 = 1,
+	PLDM_STATE_SET_ACPI_POWER_STATE_S1 = 2,
+	PLDM_STATE_SET_ACPI_POWER_STATE_S2 = 3,
+	PLDM_STATE_SET_ACPI_POWER_STATE_S3 = 4,
+	PLDM_STATE_SET_ACPI_POWER_STATE_S4 = 5,
+	PLDM_STATE_SET_ACPI_POWER_STATE_S5 = 6,
+	PLDM_STATE_SET_ACPI_POWER_STATE_G3 = 7,
 };
 
 /* OEM ranges */

commit d3b1228662c8dce9d6fdf73471a416eada083893

state-set: Return missing enum value
The commit "state-set: Add new enums for state sets" (9a6ab390fdd7)
has fixed the typo (RESORUCE->RESOURCE) in the misspelled
PLDM_STATE_SET_BOOT_PROG_STATE_PCI_RESORUCE_CONFIG enum value name.
Since changing the name of the enum value can result in client break,
for now return the misspelled name to exist along with the corrected
one.

Fixes: 9a6ab390fdd7 ("state-set: Add new enums for state sets")
Change-Id: I549d856b2da2d2916e87f710958cba788a9b4820
Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

include/libpldm/state_set.h
@@ -307,6 +307,7 @@ enum pldm_state_set_boot_progress_state_values {
 	PLDM_STATE_SET_BOOT_PROG_STATE_USER_AUTHENTICATION = 6,
 	PLDM_STATE_SET_BOOT_PROG_STATE_USER_INITIATED_SYSTEM_SETUP = 7,
 	PLDM_STATE_SET_BOOT_PROG_STATE_USB_RESOURCE_CONFIG = 8,
+	PLDM_STATE_SET_BOOT_PROG_STATE_PCI_RESORUCE_CONFIG = 9,
 	PLDM_STATE_SET_BOOT_PROG_STATE_PCI_RESOURCE_CONFIG = 9,
 	PLDM_STATE_SET_BOOT_PROG_STATE_OPTION_ROM_INITIALIZATION = 10,
 	PLDM_STATE_SET_BOOT_PROG_STATE_VIDEO_INITIALIZATION = 11,