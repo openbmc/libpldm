openbmc/libpldm

commit 4ad11f63cf49f6614f38aeb6224915bfed11e506

meson: Simplify testing for poll.h
Unconditionally define HAVE_POLL_H to the result of the cc.has_header()
query.

Change-Id: I1a9b654ae6a071aaa4b45500b7ee62ca0dbf85b0
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

include/libpldm/transport/af-mctp.h
@@ -22,7 +22,7 @@ void pldm_transport_af_mctp_destroy(struct pldm_transport_af_mctp *ctx);
 struct pldm_transport *
 pldm_transport_af_mctp_core(struct pldm_transport_af_mctp *ctx);
 
-#ifdef PLDM_HAS_POLL
+#if HAVE_POLL_H
 struct pollfd;
 /* Init pollfd for async calls */
 int pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t,

include/libpldm/transport/mctp-demux.h
@@ -21,7 +21,7 @@ void pldm_transport_mctp_demux_destroy(struct pldm_transport_mctp_demux *ctx);
 struct pldm_transport *
 pldm_transport_mctp_demux_core(struct pldm_transport_mctp_demux *ctx);
 
-#ifdef PLDM_HAS_POLL
+#if HAVE_POLL_H
 struct pollfd;
 /* Init pollfd for async calls */
 int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport *t,

src/transport/test.c
@@ -28,7 +28,7 @@ struct pldm_transport *pldm_transport_test_core(struct pldm_transport_test *ctx)
 	return &ctx->transport;
 }
 
-#ifdef PLDM_HAS_POLL
+#if HAVE_POLL_H
 #include <poll.h>
 LIBPLDM_ABI_TESTING
 int pldm_transport_test_init_pollfd(struct pldm_transport *ctx,

meson.build
@@ -28,9 +28,7 @@ if cc.has_argument('-Wvla')
 endif
 
 conf = configuration_data()
-if cc.has_header('poll.h')
-    conf.set('PLDM_HAS_POLL', 1)
-endif
+conf.set10('HAVE_POLL_H', cc.has_header('poll.h'))
 
 if cc.has_header('linux/mctp.h')
     conf.set10(

src/transport/test.h
@@ -48,7 +48,7 @@ void pldm_transport_test_destroy(struct pldm_transport_test *ctx);
 struct pldm_transport *
 pldm_transport_test_core(struct pldm_transport_test *ctx);
 
-#if PLDM_HAS_POLL
+#if HAVE_POLL_H
 struct pollfd;
 int pldm_transport_test_init_pollfd(struct pldm_transport *ctx,
 				    struct pollfd *pollfd);

src/transport/transport.c
@@ -9,16 +9,18 @@
 
 #include <errno.h>
 #include <limits.h>
-#ifdef PLDM_HAS_POLL
+
+#if HAVE_POLL_H
 #include <poll.h>
 #endif
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
-#ifndef PLDM_HAS_POLL
+#if !HAVE_POLL_H
 struct pollfd {
 	int fd;	       /* file descriptor */
 	short events;  /* requested events */

commit dde71586ac054c37a6e3d434005448648a4158bb

transport: Eliminate poll guards from headers
We forward-declare struct pollfd so there's no harm in exposing the
functions. The definition of struct pollfd is then a problem for the
implementation. If it's not available, expose a symbol returning an
error.

Fixes: 4ad11f63cf49 ("meson: Simplify testing for poll.h")
Change-Id: Ieb8ddb71af9b442cd1d353929d7b0f5bf2f61cc3
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

include/libpldm/transport/af-mctp.h
@@ -22,12 +22,10 @@ void pldm_transport_af_mctp_destroy(struct pldm_transport_af_mctp *ctx);
 struct pldm_transport *
 pldm_transport_af_mctp_core(struct pldm_transport_af_mctp *ctx);
 
-#if HAVE_POLL_H
 struct pollfd;
 /* Init pollfd for async calls */
 int pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t,
 				       struct pollfd *pollfd);
-#endif
 
 /* Inserts a TID-to-EID mapping into the transport's device map */
 int pldm_transport_af_mctp_map_tid(struct pldm_transport_af_mctp *ctx,

include/libpldm/transport/mctp-demux.h
@@ -21,12 +21,10 @@ void pldm_transport_mctp_demux_destroy(struct pldm_transport_mctp_demux *ctx);
 struct pldm_transport *
 pldm_transport_mctp_demux_core(struct pldm_transport_mctp_demux *ctx);
 
-#if HAVE_POLL_H
 struct pollfd;
 /* Init pollfd for async calls */
 int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport *t,
 					  struct pollfd *pollfd);
-#endif
 
 /* Inserts a TID-to-EID mapping into the transport's device map */
 int pldm_transport_mctp_demux_map_tid(struct pldm_transport_mctp_demux *ctx,

src/transport/af-mctp.c
@@ -6,6 +6,7 @@
 #include "socket.h"
 #include "transport.h"
 
+#include <libpldm/api.h>
 #include <libpldm/base.h>
 #include <libpldm/pldm.h>
 #include <libpldm/transport.h>
@@ -59,6 +60,14 @@ pldm_transport_af_mctp_core(struct pldm_transport_af_mctp *ctx)
 	return &ctx->transport;
 }
 
+#if !HAVE_POLL_H
+LIBPLDM_ABI_STABLE int
+pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t LIBPLDM_CC_UNUSED,
+				   struct pollfd *pollfd LIBPLDM_CC_UNUSED)
+{
+	return PLDM_REQUESTER_POLL_FAIL;
+}
+#else
 LIBPLDM_ABI_STABLE
 int pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t,
 				       struct pollfd *pollfd)
@@ -68,6 +77,7 @@ int pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t,
 	pollfd->events = POLLIN;
 	return 0;
 }
+#endif
 
 static int pldm_transport_af_mctp_lookup_fqe(struct pldm_transport_af_mctp *ctx,
 					     pldm_tid_t tid, uint32_t *network,

src/transport/mctp-demux.c
@@ -5,6 +5,7 @@
 #include "socket.h"
 #include "transport.h"
 
+#include <libpldm/api.h>
 #include <libpldm/base.h>
 #include <libpldm/pldm.h>
 #include <libpldm/transport.h>
@@ -69,6 +70,14 @@ static pldm_requester_rc_t pldm_transport_mctp_demux_open(void)
 	return fd;
 }
 
+#if !HAVE_POLL_H
+int pldm_transport_mctp_demux_init_pollfd(
+	struct pldm_transport *t PLDM_REQUESTER_POLL_FAIL,
+	struct pollfd *pollfd PLDM_REQUESTER_POLL_FAIL)
+{
+	return PLDM_REQUESTER_POLL_FAIL;
+}
+#else
 LIBPLDM_ABI_STABLE
 int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport *t,
 					  struct pollfd *pollfd)
@@ -78,6 +87,7 @@ int pldm_transport_mctp_demux_init_pollfd(struct pldm_transport *t,
 	pollfd->events = POLLIN;
 	return 0;
 }
+#endif
 
 static int
 pldm_transport_mctp_demux_get_eid(struct pldm_transport_mctp_demux *ctx,

src/transport/test.c
@@ -5,6 +5,8 @@
 #include "transport.h"
 #include "test.h"
 
+#include <libpldm/api.h>
+
 #include <errno.h>
 #include <poll.h>
 #include <stdlib.h>
@@ -28,8 +30,14 @@ struct pldm_transport *pldm_transport_test_core(struct pldm_transport_test *ctx)
 	return &ctx->transport;
 }
 
-#if HAVE_POLL_H
-#include <poll.h>
+#if !HAVE_POLL_H
+LIBPLDM_ABI_TESTING
+int pldm_transport_test_init_pollfd(struct pldm_transport *ctx LIBPLDM_CC_UNUSED,
+				    struct pollfd *pollfd LIBPLDM_CC_UNUSED)
+{
+	return PLDM_REQUESTER_POLL_FAIL;
+}
+#else
 LIBPLDM_ABI_TESTING
 int pldm_transport_test_init_pollfd(struct pldm_transport *ctx,
 				    struct pollfd *pollfd)