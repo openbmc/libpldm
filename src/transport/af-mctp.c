d7d4964e0b0aeedb2bd208f37c20791a2b5b210a
Vishnunithyasoundhar S <vishnunithyasoundhar.s@intel.com>
2026-02-27 14:19:39 +0530

    transport: af-mctp: stabilize fqe APIs
    Stabilize the APIs introduced as part of libpldm patch [1].
    
    PLDM patch [2] will consume these APIs to add multi-mctp-network
    support to PLDM.
    
    [1] https://gerrit.openbmc.org/c/openbmc/libpldm/+/85949
    [2] https://gerrit.openbmc.org/c/openbmc/pldm/+/85952
    
    Change-Id: Ia792c44067eac0d290150584a76226f473100938
    Signed-off-by: Vishnunithyasoundhar S <vishnunithyasoundhar.s@intel.com>
    Co-developed-by: Arun P. Mohanan <arun.p.m@intel.com>

--- src/transport/af-mctp.c
+++ src/transport/af-mctp.c
@@ -117,7 +117,7 @@ int pldm_transport_af_mctp_unmap_tid(struct pldm_transport_af_mctp *ctx,
 	return 0;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int pldm_transport_af_mctp_map_tid_fqe(struct pldm_transport_af_mctp *ctx,
 				       pldm_tid_t tid, uint32_t network,
 				       mctp_eid_t eid)
@@ -127,7 +127,7 @@ int pldm_transport_af_mctp_map_tid_fqe(struct pldm_transport_af_mctp *ctx,
 	return 0;
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int pldm_transport_af_mctp_unmap_tid_fqe(struct pldm_transport_af_mctp *ctx,
 					 pldm_tid_t tid)
 {
