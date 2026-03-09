openbmc/libpldm

commit 6b8f2646bc2abbeb7ccb074aa23fb7be929d1f00

transport: af-mctp: Qualify MCTP TID mapping with target network
Add network ID support to send pldm messages on MCTP networks other
than the default Network ID MCTP_NET_ANY.

Affects only af-mctp transport.

Change-Id: I3b0a595ad3d18fd2b212dde4d4508ba25d91f191
Signed-off-by: Vishnunithyasoundhar S <vishnunithyasoundhar.s@intel.com>
Co-developed-by: Arun P. Mohanan <arun.p.m@intel.com>

src/transport/af-mctp.c
@@ -32,10 +32,18 @@ struct pldm_responder_cookie_af_mctp {
 	container_of((c), struct pldm_responder_cookie_af_mctp, req)
 
 #define AF_MCTP_NAME "AF_MCTP"
+
+#if !HAVE_STRUCT_MCTP_FQ_ADDR
+struct mctp_fq_addr {
+	unsigned int net;
+	mctp_eid_t eid;
+};
+#endif
+
 struct pldm_transport_af_mctp {
 	struct pldm_transport transport;
 	int socket;
-	pldm_tid_t tid_eid_map[MCTP_MAX_NUM_EID];
+	struct mctp_fq_addr tid_map[PLDM_MAX_TIDS];
 	struct pldm_socket_sndbuf socket_send_buf;
 	bool bound;
 	struct pldm_responder_cookie cookie_jar;
@@ -61,26 +69,30 @@ int pldm_transport_af_mctp_init_pollfd(struct pldm_transport *t,
 	return 0;
 }
 
-static int pldm_transport_af_mctp_get_eid(struct pldm_transport_af_mctp *ctx,
-					  pldm_tid_t tid, mctp_eid_t *eid)
+static int pldm_transport_af_mctp_lookup_fqe(struct pldm_transport_af_mctp *ctx,
+					     pldm_tid_t tid, uint32_t *network,
+					     mctp_eid_t *eid)
 {
-	int i;
-	for (i = 0; i < MCTP_MAX_NUM_EID; i++) {
-		if (ctx->tid_eid_map[i] == tid) {
-			*eid = i;
-			return 0;
-		}
+	if (tid == 0) {
+		return -1;
 	}
-	*eid = -1;
-	return -1;
+	*network = ctx->tid_map[tid].net;
+	*eid = ctx->tid_map[tid].eid;
+	return 0;
 }
 
 static int pldm_transport_af_mctp_get_tid(struct pldm_transport_af_mctp *ctx,
-					  mctp_eid_t eid, pldm_tid_t *tid)
+					  uint32_t network, mctp_eid_t eid,
+					  pldm_tid_t *tid)
 {
-	if (ctx->tid_eid_map[eid] != 0) {
-		*tid = ctx->tid_eid_map[eid];
-		return 0;
+	if (network != 0 && eid != 0) {
+		for (int i = 0; i < PLDM_MAX_TIDS; i++) {
+			if (ctx->tid_map[i].net == network &&
+			    ctx->tid_map[i].eid == eid) {
+				*tid = i;
+				return 0;
+			}
+		}
 	}
 	return -1;
 }
@@ -89,18 +101,37 @@ LIBPLDM_ABI_STABLE
 int pldm_transport_af_mctp_map_tid(struct pldm_transport_af_mctp *ctx,
 				   pldm_tid_t tid, mctp_eid_t eid)
 {
-	ctx->tid_eid_map[eid] = tid;
-
+	ctx->tid_map[tid].net = MCTP_NET_ANY;
+	ctx->tid_map[tid].eid = eid;
 	return 0;
 }
 
 LIBPLDM_ABI_STABLE
 int pldm_transport_af_mctp_unmap_tid(struct pldm_transport_af_mctp *ctx,
-				     LIBPLDM_CC_UNUSED pldm_tid_t tid,
-				     mctp_eid_t eid)
+				     pldm_tid_t tid,
+				     LIBPLDM_CC_UNUSED mctp_eid_t eid)
 {
-	ctx->tid_eid_map[eid] = 0;
+	ctx->tid_map[tid].net = 0;
+	ctx->tid_map[tid].eid = 0;
+	return 0;
+}
 
+LIBPLDM_ABI_TESTING
+int pldm_transport_af_mctp_map_tid_fqe(struct pldm_transport_af_mctp *ctx,
+				       pldm_tid_t tid, uint32_t network,
+				       mctp_eid_t eid)
+{
+	ctx->tid_map[tid].net = network;
+	ctx->tid_map[tid].eid = eid;
+	return 0;
+}
+
+LIBPLDM_ABI_TESTING
+int pldm_transport_af_mctp_unmap_tid_fqe(struct pldm_transport_af_mctp *ctx,
+					 pldm_tid_t tid)
+{
+	ctx->tid_map[tid].net = 0;
+	ctx->tid_map[tid].eid = 0;
 	return 0;
 }
 
@@ -114,7 +145,6 @@ static pldm_requester_rc_t pldm_transport_af_mctp_recv(struct pldm_transport *t,
 	socklen_t addrlen = sizeof(addr);
 	struct pldm_msg_hdr *hdr;
 	pldm_requester_rc_t res;
-	mctp_eid_t eid = 0;
 	ssize_t length;
 	void *msg;
 	int rc;
@@ -136,8 +166,8 @@ static pldm_requester_rc_t pldm_transport_af_mctp_recv(struct pldm_transport *t,
 		goto cleanup_msg;
 	}
 
-	eid = addr.smctp_addr.s_addr;
-	rc = pldm_transport_af_mctp_get_tid(af_mctp, eid, tid);
+	rc = pldm_transport_af_mctp_get_tid(af_mctp, addr.smctp_network,
+					    addr.smctp_addr.s_addr, tid);
 	if (rc) {
 		res = PLDM_REQUESTER_RECV_FAIL;
 		goto cleanup_msg;
@@ -211,12 +241,15 @@ static pldm_requester_rc_t pldm_transport_af_mctp_send(struct pldm_transport *t,
 		free(cookie);
 	} else {
 		mctp_eid_t eid = 0;
-		if (pldm_transport_af_mctp_get_eid(af_mctp, tid, &eid)) {
+		uint32_t network = 0;
+		if (pldm_transport_af_mctp_lookup_fqe(af_mctp, tid, &network,
+						      &eid)) {
 			return PLDM_REQUESTER_SEND_FAIL;
 		}
 
 		addr.smctp_family = AF_MCTP;
 		addr.smctp_addr.s_addr = eid;
+		addr.smctp_network = network;
 		addr.smctp_type = MCTP_MSG_TYPE_PLDM;
 		addr.smctp_tag = MCTP_TAG_OWNER;
 	}

include/libpldm/transport/af-mctp.h
@@ -37,6 +37,15 @@ int pldm_transport_af_mctp_map_tid(struct pldm_transport_af_mctp *ctx,
 int pldm_transport_af_mctp_unmap_tid(struct pldm_transport_af_mctp *ctx,
 				     pldm_tid_t tid, mctp_eid_t eid);
 
+/* Maps TID-to-<Network, EID> (fully qualified endpoint) */
+int pldm_transport_af_mctp_map_tid_fqe(struct pldm_transport_af_mctp *ctx,
+				       pldm_tid_t tid, uint32_t network,
+				       mctp_eid_t eid);
+
+/* Removes a TID-to-<Network, EID> (fully qualified endpoint) mapping */
+int pldm_transport_af_mctp_unmap_tid_fqe(struct pldm_transport_af_mctp *ctx,
+					 pldm_tid_t tid);
+
 /**
  * @brief Allow the transport to receive requests from remote endpoints
  *

meson.build
@@ -32,6 +32,17 @@ if cc.has_header('poll.h')
     conf.set('PLDM_HAS_POLL', 1)
 endif
 
+if cc.has_header('linux/mctp.h')
+    conf.set10(
+        'HAVE_STRUCT_MCTP_FQ_ADDR',
+        cc.has_type(
+            'struct mctp_fq_addr',
+            prefix: '#include <linux/mctp.h>',
+        ),
+        description: 'Is struct mctp_fq_addr available?',
+    )
+endif
+
 # ABI control
 cc.has_function_attribute('visibility:default', required: true)
 entrypoint = '__attribute__((visibility("default")))'

commit be5928838e20c8c2edbc8b23e236cc79d0954343

transport: af-mctp: Fix TID lookup rejecting MCTP_NET_ANY mappings
The get_tid guard `if (network != 0 && eid != 0)` rejects lookups
where network is MCTP_NET_ANY (0x0). Since map_tid() stores
net=MCTP_NET_ANY for all mappings, recv() always fails with
PLDM_REQUESTER_RECV_FAIL.

Fix by only rejecting eid==0 and treating MCTP_NET_ANY as a wildcard
match for the network field.

Tested with pldmd.

Before:

```
<3> MCTP daemon closed the socket, IO exiting with response code '-8'
free(): invalid pointer
Aborted (core dumped)
```

After:

pldmd doesn't crash

Fixes: 6b8f264 ("transport: af-mctp: Qualify MCTP TID mapping with target network")
Change-Id: I64ab28dc46e8133377c5ed41587ee5da719abf6e
Signed-off-by: Aditya Kurdunkar <adityakurdunkar2@gmail.com>

src/transport/af-mctp.c
@@ -86,13 +86,18 @@ static int pldm_transport_af_mctp_get_tid(struct pldm_transport_af_mctp *ctx,
 					  uint32_t network, mctp_eid_t eid,
 					  pldm_tid_t *tid)
 {
-	if (network != 0 && eid != 0) {
-		for (int i = 0; i < PLDM_MAX_TIDS; i++) {
-			if (ctx->tid_map[i].net == network &&
-			    ctx->tid_map[i].eid == eid) {
-				*tid = i;
-				return 0;
-			}
+	if (eid == 0) {
+		return -1;
+	}
+	for (int i = 0; i < PLDM_MAX_TIDS; i++) {
+		if (ctx->tid_map[i].eid != eid) {
+			continue;
+		}
+		if (ctx->tid_map[i].net == network ||
+		    ctx->tid_map[i].net == MCTP_NET_ANY ||
+		    network == MCTP_NET_ANY) {
+			*tid = i;
+			return 0;
 		}
 	}
 	return -1;