937ae2a5760057cc13fff5be48e87c9203a213b4
Gary Beihl <garybeihl@microsoft.com>
2026-03-10 11:31:41 -0400

    dsp: firmware_update: Add str_len bounds checks to encode functions
    The encode_get_firmware_parameters_resp() and
    encode_get_firmware_parameters_resp_comp_entry() functions pass str_len
    as both the length and buffer size arguments to
    pldm_msgbuf_insert_array(). If str_len exceeds PLDM_FIRMWARE_MAX_STRING
    (64), the call reads past the end of the str_data[64] array.
    
    Check that str_len does not exceed PLDM_FIRMWARE_MAX_STRING
    before the buffer is accessed. The decode side is already protected
    because pldm_msgbuf_extract_array() receives the fixed buffer size.
    
    Change-Id: I85cd29d3254d1cf85cdb516741e0629be4ef3968
    Signed-off-by: Gary Beihl <garybeihl@microsoft.com>

--- tests/dsp/firmware_update.cpp
+++ tests/dsp/firmware_update.cpp
@@ -1509,6 +1509,83 @@ TEST(GetFirmwareParameters, errorPathdecodeResponse)
     EXPECT_EQ(rc, PLDM_ERROR_INVALID_LENGTH);
 }
 
+#if HAVE_LIBPLDM_API_TESTING
+TEST(GetFirmwareParameters, errorPathEncodeResponseActiveStrLenTooLong)
+{
+    PLDM_MSG_DEFINE_P(enc, 1000);
+    size_t enc_payload_len = 1000;
+
+    struct pldm_get_firmware_parameters_resp_full resp_data = {};
+    resp_data.completion_code = PLDM_SUCCESS;
+    resp_data.active_comp_image_set_ver_str.str_type = PLDM_STR_TYPE_ASCII;
+    resp_data.active_comp_image_set_ver_str.str_len =
+        PLDM_FIRMWARE_MAX_STRING + 1;
+    resp_data.pending_comp_image_set_ver_str.str_type = PLDM_STR_TYPE_UNKNOWN;
+    resp_data.pending_comp_image_set_ver_str.str_len = 0;
+
+    int rc = encode_get_firmware_parameters_resp(FIXED_INSTANCE_ID, &resp_data,
+                                                 enc, &enc_payload_len);
+    EXPECT_EQ(rc, -EINVAL);
+}
+#endif
+
+#if HAVE_LIBPLDM_API_TESTING
+TEST(GetFirmwareParameters, errorPathEncodeResponsePendingStrLenTooLong)
+{
+    PLDM_MSG_DEFINE_P(enc, 1000);
+    size_t enc_payload_len = 1000;
+
+    struct pldm_get_firmware_parameters_resp_full resp_data = {};
+    resp_data.completion_code = PLDM_SUCCESS;
+    resp_data.active_comp_image_set_ver_str.str_type = PLDM_STR_TYPE_ASCII;
+    resp_data.active_comp_image_set_ver_str.str_len = 5;
+    memcpy(resp_data.active_comp_image_set_ver_str.str_data, "hello", 5);
+    resp_data.pending_comp_image_set_ver_str.str_type = PLDM_STR_TYPE_ASCII;
+    resp_data.pending_comp_image_set_ver_str.str_len =
+        PLDM_FIRMWARE_MAX_STRING + 1;
+
+    int rc = encode_get_firmware_parameters_resp(FIXED_INSTANCE_ID, &resp_data,
+                                                 enc, &enc_payload_len);
+    EXPECT_EQ(rc, -EINVAL);
+}
+#endif
+
+#if HAVE_LIBPLDM_API_TESTING
+TEST(GetFirmwareParameters, errorPathEncodeCompEntryActiveStrLenTooLong)
+{
+    std::vector<uint8_t> enc_data(1000);
+    size_t enc_payload_len = enc_data.size();
+
+    struct pldm_component_parameter_entry_full comp = {};
+    comp.active_ver.str.str_type = PLDM_STR_TYPE_ASCII;
+    comp.active_ver.str.str_len = PLDM_FIRMWARE_MAX_STRING + 1;
+    comp.pending_ver.str.str_type = PLDM_STR_TYPE_ASCII;
+    comp.pending_ver.str.str_len = 5;
+
+    int rc = encode_get_firmware_parameters_resp_comp_entry(
+        &comp, enc_data.data(), &enc_payload_len);
+    EXPECT_EQ(rc, -EINVAL);
+}
+#endif
+
+#if HAVE_LIBPLDM_API_TESTING
+TEST(GetFirmwareParameters, errorPathEncodeCompEntryPendingStrLenTooLong)
+{
+    std::vector<uint8_t> enc_data(1000);
+    size_t enc_payload_len = enc_data.size();
+
+    struct pldm_component_parameter_entry_full comp = {};
+    comp.active_ver.str.str_type = PLDM_STR_TYPE_ASCII;
+    comp.active_ver.str.str_len = 5;
+    comp.pending_ver.str.str_type = PLDM_STR_TYPE_ASCII;
+    comp.pending_ver.str.str_len = PLDM_FIRMWARE_MAX_STRING + 1;
+
+    int rc = encode_get_firmware_parameters_resp_comp_entry(
+        &comp, enc_data.data(), &enc_payload_len);
+    EXPECT_EQ(rc, -EINVAL);
+}
+#endif
+
 TEST(GetFirmwareParameters, goodPathDecodeComponentParameterEntry)
 {
     // Random value for component classification
