openbmc/libpldm

commit 87382e39ac5d8334a01c372b6ff3ca9b33a96833

msgbuf: Add pldm_msgbuf_field_{begin,end}()
Add functions pldm_msgbuf_field_begin() and pldm_msgbuf_field_end() to
fill the `struct variable_field` variable without ctx modification.

Change-Id: I845be9b61502b315372887e6bb7023154cc7ea65
Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/msgbuf/core.h
@@ -1601,6 +1601,87 @@ pldm__msgbuf_extract_uint32_to_size(struct pldm_msgbuf_ro *ctx, size_t *dst)
 	return 0;
 }
 
+/**
+ * @brief Begin `struct variable_field` fill
+ *
+ * @param[in] cursor - The pointer from the relevant @ref
+ *                     "struct pldm_msgbuf_ro" or @ref "struct pldm_msgbuf_rw"
+ *                     context
+ * @param[in] remaining - The length remaining from the relevant @ref
+ *                        "struct pldm_msgbuf_ro" or @ref
+ *                        "struct pldm_msgbuf_rw" context
+ * @param[out] field - `struct variable_field` to fill
+ *
+ * @return 0 if all buffer accesses were in-bounds, -EOVERFLOW otherwise.
+ */
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+// NOLINTNEXTLINE(bugprone-reserved-identifier,cert-dcl37-c,cert-dcl51-cpp)
+int pldm__msgbuf_field_begin(const void *cursor, intmax_t remaining,
+			     struct variable_field *field)
+{
+	if (remaining >= 0) {
+#if SIZE_MAX < INTMAX_MAX
+		assert(ctx->remaining <= (intmax_t)SIZE_MAX);
+#endif
+
+		field->ptr = (const uint8_t *)cursor;
+		field->length = remaining;
+		return 0;
+	}
+
+	field->ptr = NULL;
+	field->length = 0;
+
+	return -EOVERFLOW;
+}
+
+/**
+ * @brief End `struct variable_field` fill from the pldm_msgbuf_ro instance
+ *
+ * @param[in] cursor - The pointer from the relevant @ref
+ *                     "struct pldm_msgbuf_ro" or @ref "struct pldm_msgbuf_rw"
+ *                     context
+ * @param[in] remaining - The length remaining from the relevant @ref
+ *                        "struct pldm_msgbuf_ro" or @ref
+ *                        "struct pldm_msgbuf_rw" context
+ * @param[out] field - `struct variable_field` to fill
+ *
+ * @return 0 if all buffer accesses were in-bounds, -EOVERFLOW otherwise.
+ */
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+LIBPLDM_CC_WARN_UNUSED_RESULT
+// NOLINTNEXTLINE(bugprone-reserved-identifier,cert-dcl37-c,cert-dcl51-cpp)
+int pldm__msgbuf_field_end(const void *cursor LIBPLDM_CC_UNUSED,
+			   intmax_t remaining, struct variable_field *field)
+{
+	if (!field->ptr) {
+		return -EINVAL;
+	}
+
+#if INTMAX_MAX < SIZE_MAX
+	if (field->length > INTMAX_MAX) {
+		return -EINVAL;
+	}
+#endif
+
+	if (remaining >= 0) {
+		if ((intmax_t)field->length < remaining) {
+			return -EINVAL;
+		}
+
+		if (field->ptr + (field->length - remaining) != cursor) {
+			return -EINVAL;
+		}
+
+		field->length -= remaining;
+		return 0;
+	}
+
+	return -EOVERFLOW;
+}
+
 #ifdef __cplusplus
 }
 #endif

src/msgbuf.h
@@ -193,4 +193,16 @@
 		char: pldm_msgbuf_insert_array_char)(dst, count, src,          \
 						     src_count)
 
+/**
+ * Functions to fill `struct variable_field` variable without ctx modification
+ *
+ * @param ctx - The msgbuf context object
+ * @param field - The `struct variable_field` variable to fill
+ */
+#define pldm_msgbuf_field_begin(ctx, field)                                    \
+	pldm__msgbuf_field_begin((ctx)->cursor, (ctx)->remaining, &(field))
+
+#define pldm_msgbuf_field_end(ctx, field)                                      \
+	pldm__msgbuf_field_end((ctx)->cursor, (ctx)->remaining, &(field))
+
 #endif /* BUF_H */

tests/msgbuf.cpp
@@ -1666,3 +1666,68 @@ TEST(msgbuf, extract_over_uint32_to_size)
     EXPECT_NE(pldm_msgbuf_extract_uint32_to_size(ctx, val), 0);
     EXPECT_EQ(pldm_msgbuf_complete(ctx), -EOVERFLOW);
 }
+
+TEST(msgbuf, pldm_msgbuf_field_begin_end)
+{
+    struct pldm_msgbuf_rw _ctx;
+    struct pldm_msgbuf_rw* ctx = &_ctx;
+    uint8_t src[7] = {0x11, 0x22, 0x44, 0x55, 0x66, 0x77, 0x88};
+    uint8_t buf[7] = {0};
+    uint8_t testVal;
+    const size_t offset = 2;
+    const size_t required = 4;
+    struct variable_field data;
+
+    ASSERT_EQ(pldm_msgbuf_init_errno(ctx, 0, buf, sizeof(buf)), 0);
+    EXPECT_EQ(
+        pldm_msgbuf_insert_array_uint8(ctx, sizeof(src), src, sizeof(src)), 0);
+
+    struct pldm_msgbuf_ro _ctxExtract;
+    struct pldm_msgbuf_ro* ctxExtract = &_ctxExtract;
+
+    ASSERT_EQ(pldm_msgbuf_init_errno(ctxExtract, 0, buf, sizeof(buf)), 0);
+    EXPECT_EQ(pldm_msgbuf_span_required(ctxExtract, offset, NULL), 0);
+
+    ASSERT_EQ(pldm_msgbuf_field_begin(ctxExtract, data), 0);
+    EXPECT_EQ(pldm_msgbuf_span_required(ctxExtract, required, NULL), 0);
+    EXPECT_EQ(pldm_msgbuf_field_end(ctxExtract, data), 0);
+
+    EXPECT_EQ(data.length, required);
+    EXPECT_EQ(0, memcmp(data.ptr, &src[offset], required));
+
+    EXPECT_EQ(pldm_msgbuf_extract_uint8(ctxExtract, testVal), 0);
+    EXPECT_EQ(testVal, 0x88);
+
+    EXPECT_EQ(pldm_msgbuf_complete(ctxExtract), 0);
+    EXPECT_EQ(pldm_msgbuf_complete(ctx), 0);
+}
+
+TEST(msgbuf, pldm_msgbuf_field_begin_errors)
+{
+    {
+        // nullptr for ctx->cursor
+        struct variable_field field{};
+        struct pldm_msgbuf_ro ctx{};
+        EXPECT_NE(pldm_msgbuf_field_begin(&ctx, field), 0);
+    }
+
+    {
+        // Overflow
+        std::array<std::uint8_t, 1> buf{};
+        struct variable_field field{};
+        struct pldm_msgbuf_ro ctx{buf.data(), -1};
+        EXPECT_EQ(pldm_msgbuf_field_begin(&ctx, field), -EOVERFLOW);
+    }
+}
+
+TEST(msgbuf, pldm_msgbuf_field_end_errors)
+{
+    {
+        std::array<std::uint8_t, 1> buf{};
+        struct variable_field field{};
+        struct pldm_msgbuf_ro ctx{buf.data(), buf.size()};
+        ASSERT_EQ(pldm_msgbuf_field_begin(&ctx, field), 0);
+        field.ptr = nullptr;
+        EXPECT_NE(pldm_msgbuf_field_end(&ctx, field), 0);
+    }
+}

tests/msgbuf.hpp
@@ -187,6 +187,58 @@ LIBPLDM_CC_ALWAYS_INLINE int pldm_msgbuf_span_until(struct pldm_msgbuf_rw* ctx,
     return pldm_msgbuf_rw_span_until(ctx, trailer, cursor, length);
 }
 
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+int pldm_msgbuf_field_begin(struct pldm_msgbuf_ro* ctx,
+                            struct variable_field& field)
+{
+    if (!ctx->cursor)
+    {
+        return -EINVAL;
+    }
+
+    return pldm__msgbuf_field_begin(ctx->cursor, ctx->remaining, &field);
+}
+
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+int pldm_msgbuf_field_end(struct pldm_msgbuf_ro* ctx,
+                          struct variable_field& field)
+{
+    if (!ctx->cursor)
+    {
+        return -EINVAL;
+    }
+
+    return pldm__msgbuf_field_end(ctx->cursor, ctx->remaining, &field);
+}
+
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+int pldm_msgbuf_field_begin(struct pldm_msgbuf_rw* ctx,
+                            struct variable_field& field)
+{
+    if (!ctx->cursor)
+    {
+        return -EINVAL;
+    }
+
+    return pldm__msgbuf_field_begin(ctx->cursor, ctx->remaining, &field);
+}
+
+LIBPLDM_CC_NONNULL
+LIBPLDM_CC_ALWAYS_INLINE
+int pldm_msgbuf_field_end(struct pldm_msgbuf_rw* ctx,
+                          struct variable_field& field)
+{
+    if (!ctx->cursor)
+    {
+        return -EINVAL;
+    }
+
+    return pldm__msgbuf_field_end(ctx->cursor, ctx->remaining, &field);
+}
+
 #define pldm_msgbuf_extract_typecheck(ty, fn, dst, ...)                        \
     pldm_msgbuf_typecheck_##ty<decltype(dst)>(__VA_ARGS__)
 

commit 971c43b9dc91a319294425454e3a57b67b20c982

msgbuf: Fix assertion check
There's no `ctx` in this context - assuming check was meant for
parameter `remaining` instead.

Fixes: 87382e39ac5d ("msgbuf: Add pldm_msgbuf_field_{begin,end}()")
Change-Id: Idaf518f2407ff427525e4937f9b8ad64b3d6c4b6
Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>
Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

src/msgbuf/core.h
@@ -1622,7 +1622,7 @@ int pldm__msgbuf_field_begin(const void *cursor, intmax_t remaining,
 {
 	if (remaining >= 0) {
 #if SIZE_MAX < INTMAX_MAX
-		assert(ctx->remaining <= (intmax_t)SIZE_MAX);
+		assert(remaining <= (intmax_t)SIZE_MAX);
 #endif
 
 		field->ptr = (const uint8_t *)cursor;