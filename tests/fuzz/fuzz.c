a7abc880b1655ba2a06953ddaa10eb13216abe90
Andrew Jeffery <andrew@codeconstruct.com.au>
2026-05-17 17:07:46 +0930

    wip: tests: fuzz: Firmware update package parsing
    Change-Id: I8be86c59e604fbff4e4af7ac52f0614174a2216a
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

--- tests/fuzz/fuzz.c
+++ tests/fuzz/fuzz.c
@@ -1,9 +1,106 @@
+#include <libpldm/firmware_update.h>
 #include <libpldm/fru.h>
 #include <libpldm/platform.h>
 #include <stdlib.h>
 
 #include "array.h"
 #include "msgbuf.h"
+
+int pldm_edac_crc32_validate(uint32_t expected LIBPLDM_CC_UNUSED,
+                             const void* data LIBPLDM_CC_UNUSED,
+                             size_t size LIBPLDM_CC_UNUSED)
+{
+    return 0;
+}
+
+static int fuzz_decode_pldm_firmware_update_package(const uint8_t* data,
+                                                    size_t size)
+{
+    static const uint8_t package_header_identifiers[][16] = {
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_0,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_1,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_2,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_3,
+    };
+
+    struct pldm_package_downstream_device_id_record ddrec;
+    struct pldm_package_component_image_information info;
+    struct pldm_package_firmware_device_id_record fdrec;
+    DEFINE_PLDM_PACKAGE_FORMAT_PIN_FR04H(pin);
+    pldm_package_header_information_pad hdr;
+    struct pldm_package pkg = {0};
+    PLDM_MSGBUF_RO_DEFINE_P(buf);
+    const uint8_t (*id)[16];
+    uint8_t id_idx;
+    void* package;
+    int rc;
+
+    if (size < sizeof(*id))
+    {
+        return -1;
+    }
+
+    package = calloc(size, 1);
+    if (!package)
+    {
+        return 0;
+    }
+
+    rc = pldm_msgbuf_init_errno(buf, 1, data, size);
+    if (rc)
+    {
+        rc = -1;
+        goto cleanup_package;
+    }
+
+    pldm_msgbuf_extract(buf, id_idx);
+
+    rc = pldm_msgbuf_complete(buf);
+    if (rc)
+    {
+        return -1;
+    }
+
+    memcpy(package, data, size);
+
+    id_idx %= ARRAY_SIZE(package_header_identifiers);
+    id = &package_header_identifiers[id_idx];
+    memcpy(package, package_header_identifiers[id_idx], sizeof(*id));
+
+    rc =
+        decode_pldm_firmware_update_package(package, size, &pin, &hdr, &pkg, 0);
+    if (rc < 0)
+    {
+        rc = 0;
+        goto cleanup_package;
+    }
+
+    foreach_pldm_package_firmware_device_id_record(pkg, fdrec, rc)
+    {
+        (void)fdrec;
+    }
+
+    foreach_pldm_package_downstream_device_id_record(pkg, ddrec, rc)
+    {
+        struct pldm_descriptor desc;
+
+        foreach_pldm_package_downstream_device_id_record_descriptor(pkg, ddrec,
+                                                                    desc, rc)
+        {
+            (void)desc;
+        }
+    }
+
+    foreach_pldm_package_component_image_information(pkg, info, rc)
+    {
+        (void)info;
+    }
+
+    rc = 0;
+cleanup_package:
+    free(package);
+    return rc;
+}
 
 static int fuzz_decode_pldm_platform_set_numeric_sensor_enable_resp(
     const uint8_t* data, size_t size)
@@ -108,6 +205,7 @@ static int fuzz_get_fru_record_by_option(const uint8_t* data, size_t size)
 }
 
 static int (*const fuzz_tests[])(const uint8_t*, size_t) = {
+    fuzz_decode_pldm_firmware_update_package,
     fuzz_decode_pldm_platform_set_numeric_sensor_enable_resp,
     fuzz_encode_pldm_platform_set_numeric_sensor_enable_req,
     fuzz_get_fru_record_by_option,
