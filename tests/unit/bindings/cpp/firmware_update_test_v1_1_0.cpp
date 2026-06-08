a4d1748c5d301148c2cd252840e10279b74c8bdf
Alexander Hansen <alexander.hansen@9elements.com>
2026-06-08 13:34:33 +0200

    libpldm++: support fw update pkg v1.1.0
    Introduce PackagePin::v1_1_0 to indicate support for this revision.
    
    Tested: newly written unit test pass
    
    Change-Id: I0b4ccc4e47fb752196d7db1e26d79f4865804ec6
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ tests/unit/bindings/cpp/firmware_update_test_v1_1_0.cpp
@@ -0,0 +1,247 @@
+#include <libpldm/api.h>
+#include <libpldm/edac.h>
+
+#include <expected>
+#include <libpldm++/firmware_update.hpp>
+#include <libpldm++/types.hpp>
+#include <span>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace pldm::fw_update;
+
+namespace pldm
+{
+namespace fw_update
+{
+
+#if HAVE_LIBPLDM_API_TESTING
+
+TEST(PackageParserTest, ValidPkgV1_1_0_Minimal)
+{
+    // The focus of this test is to check for things newly added between
+    // package format revisions v1.0.0 and v1.1.0
+
+    const std::vector<uint8_t> headerUUID{
+        // clang-format off
+    0x12, 0x44, 0xd2, 0x64, 0x8d, 0x7d, 0x47, 0x18,
+    0xa0, 0x30, 0xfc, 0x8a, 0x56, 0x58, 0x7d, 0x5a, // UUID
+        // clang-format on
+    };
+
+    const std::vector<uint8_t> header{
+        // clang-format off
+    0x00, 0x00, // pkg header size, this is updated later
+
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x19, 0x0C, 0xE5, 0x07,
+    0x00,       // pkg release date time (13 bytes, timestamp104)
+
+    0x08, 0x00, // component bitmap bit length
+
+    0x01,       // package version string type
+
+    0x01,       // package version string length
+
+    'v', // package version string
+        // clang-format on
+    };
+
+    std::vector<uint8_t> fwdevidarea{
+        // Firmware Device Identification Area
+
+        // clang-format off
+    0x00, 0x00, // record 0: record length
+
+    0x01,                   // record 0: descriptor count
+    0x00, 0x00, 0x00, 0x00, // record 0: device update options flags
+
+    0x01,       // record 0: component image set version string type
+    0x01,       // record 0: component image set version string length
+    0x00, 0x00, // record 0: firmware device package data length
+
+    0x01,       // applicable components
+
+    'v',       // component image set version string
+
+    // record descriptors below
+    0x02, 0x00, // record 0: descriptor type: UUID
+
+    0x10, 0x00, // record 0: InitialDescriptorLength (16 bytes)
+
+    0x16, 0x20, 0x23, 0xC9, 0x3E, 0xC5, 0x41,
+    0x15, 0x95, 0xF4, 0x48, 0x70, 0x1D, 0x49,
+    0xD6, 0x75, // record 0: InitialDescriptorData (UUID)
+
+    // firmware device package data (empty here)
+        // clang-format on
+    };
+
+    {
+        // set Recordlength
+        const uint16_t recordLength = fwdevidarea.size();
+
+        std::cout << "firmware device area length: " << recordLength
+                  << std::endl;
+
+        fwdevidarea[0] = recordLength & 0xff;
+        fwdevidarea[1] = (recordLength >> 8) & 0xff;
+    }
+
+    std::vector<uint8_t> downstreamdevidarea{
+        // clang-format off
+    // Downstream Device Identification Area
+    0x00, 0x00, // DownstreamDeviceRecordLength, this is updated later
+    0x01, // DownstreamDeviceDescriptorCount
+    0x01, 0x00, 0x00, 0x00, // DownstreamDeviceUpdateOptionFlags
+
+    0x01,       // DownstreamDeviceSelfContainedActivationMinVersionStringType
+    0x01,       // DownstreamDeviceSelfContainedActivationMinVersionStringLength
+
+    0x00, 0x00, // DownstreamDevicePackageDataLength
+
+    0x01, // DownstreamDeviceApplicableComponents
+
+    'v', // DownstreamDeviceSelfContainedActivationMinVersionString
+
+    // DownstreamDeviceSelfContainedActivationMinVersionComparisonStamp
+    0x06, 0x07, 0x08, 0x09,
+
+    // DownstreamDeviceRecordDescriptors
+    // record descriptors below
+    0x02, 0x00, // record 0: descriptor type: UUID
+
+    0x10, 0x00, // record 0: InitialDescriptorLength (16 bytes)
+
+    0x16, 0x20, 0x23, 0xC9, 0x3E, 0xC5, 0x41,
+    0x15, 0x95, 0xF4, 0x48, 0x70, 0x1D, 0x49,
+    0xD6, 0x75, // record 0: InitialDescriptorData (UUID)
+
+    // DownstreamDevicePackageData (empty)
+    //
+        // clang-format on
+    };
+
+    {
+        // set DownstreamDeviceRecordLength
+        const uint16_t downstreamdevidareaLength = downstreamdevidarea.size();
+
+        std::cout << "downstream device area length: "
+                  << downstreamdevidareaLength << std::endl;
+
+        downstreamdevidarea[0] = downstreamdevidareaLength & 0xff;
+        downstreamdevidarea[1] = (downstreamdevidareaLength >> 8) & 0xff;
+    }
+
+    const std::vector<uint8_t> componentimageinfoarea{
+        // clang-format off
+    // Component Image Information Area
+    0x01, 0x00,             // component image count
+
+    0x0A, 0x00,             // component classification
+    0x64, 0x00,             // component identifier
+    0xFF, 0xFF, 0xFF, 0xFF, // component comparison stamp
+    0x00, 0x00,             // component options
+
+    0x00, 0x00,             // requested component activation method
+    0x8B, 0x00, 0x00, 0x00, // component location offset
+    0x01, 0x00, 0x00, 0x00, // component size
+    0x01,                   // component version string type
+    0x0E,                   // component version string length
+
+    0x56, 0x65, 0x72, 0x73, 0x69, 0x6F,
+    0x6E, 0x53, 0x74, 0x72, 0x69, 0x6E,
+    0x67, 0x33,             // component version string
+        // clang-format on
+    };
+
+    std::vector<uint8_t> pkg{};
+
+    pkg.append_range(headerUUID);
+    pkg.push_back(0x02); // pkg header format revision
+    const size_t packageHeaderSizeOffset = pkg.size();
+    pkg.append_range(header);
+
+    // firmware device record count
+    pkg.push_back(0x01);
+    pkg.append_range(fwdevidarea);
+
+    // DownstreamDeviceIDRecordCount
+    pkg.push_back(0x01);
+    pkg.append_range(downstreamdevidarea);
+
+    pkg.append_range(componentimageinfoarea);
+
+    // count in the checksum bytes still to be added
+    const uint16_t finalSize = pkg.size() + 4;
+    // set PackageHeaderSize
+    pkg[packageHeaderSizeOffset] = finalSize & 0xff;
+    pkg[packageHeaderSizeOffset + 1] = (finalSize >> 8) & 0xff;
+
+    // actual checksum
+    const uint32_t check = pldm_edac_crc32(pkg.data(), pkg.size());
+
+    // PackageHeaderChecksum
+    pkg.append_range(
+        std::vector<uint8_t>{static_cast<uint8_t>(check & 0xff),
+                             static_cast<uint8_t>((check >> 8) & 0xff),
+                             static_cast<uint8_t>((check >> 16) & 0xff),
+                             static_cast<uint8_t>((check >> 24) & 0xff)});
+
+    // end of package header
+
+    pkg.push_back(0x00); // Component Image
+
+    uintmax_t pkgSize = pkg.size();
+
+    std::cout << "package size: " << pkgSize << std::endl;
+
+    auto res = PackageParser::parse(pkg, PackagePin::v1_1_0);
+
+    if (!res.has_value())
+    {
+        std::cout << res.error().msg << std::endl;
+        if (res.error().rc.has_value())
+        {
+            std::cout << res.error().rc.value() << std::endl;
+        }
+    }
+
+    ASSERT_TRUE(res.has_value());
+
+    const auto dsDeviceIdRecords = res.value()->downstreamDeviceIdRecords;
+
+    ASSERT_EQ(dsDeviceIdRecords.size(), 1);
+
+    const auto& dsDevice = dsDeviceIdRecords[0];
+
+    EXPECT_EQ(dsDevice.downstreamDeviceUpdateOptionFlags, 0x01);
+
+    EXPECT_EQ(dsDevice.downstreamDeviceSelfContainedActivationMinVersionString,
+              "v");
+
+    EXPECT_EQ(
+        dsDevice
+            .downstreamDeviceSelfContainedActivationMinVersionComparisonStamp,
+        0x09080706);
+
+    EXPECT_EQ(dsDevice.applicableComponents.size(), 1);
+    EXPECT_EQ(dsDevice.applicableComponents, std::vector<size_t>{0});
+
+    EXPECT_EQ(dsDevice.recordDescriptors.size(), 1);
+    for (auto& [k, v] : dsDevice.recordDescriptors)
+    {
+        EXPECT_EQ(k, 0x02);
+        EXPECT_EQ(v->data.size(), 16);
+        EXPECT_EQ(v->data[0], 0x16);
+        EXPECT_EQ(v->data[1], 0x20);
+    }
+
+    EXPECT_EQ(dsDevice.downstreamDevicePackageData.size(), 0);
+}
+
+#endif
+
+} // namespace fw_update
+} // namespace pldm
