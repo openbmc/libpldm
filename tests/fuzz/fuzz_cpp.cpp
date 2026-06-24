832804412628546b6837feaa861152f880cd92ed
Alexander Hansen <alexander.hansen@9elements.com>
2026-06-24 16:27:00 +0200

    tests: fuzz: libpldm++ PackageParser fuzzing
    Create separate fuzzing binary and build target for libpldm++ fuzzing.
    
    Reused some of the logic to have the fuzzing data choose among the
    various package header magics, as the 16 bytes of magic may be hard to
    guess.
    
    Applied the symbol override for pldm_edac_crc32_validate to prevent
    fuzzing problems due to incorrect CRC. This required static linking to
    get right.
    
    Tested: it looks like fuzzing at least reaches at least some of the
    libpldm functions that are called, like
    `pldm_package_firmware_device_id_record_iter_init`.
    
    Overall, some valid packages were generated with fuzzing since the
    fuzzer reached the `consume` function
    
    ```
    |       NEW_FUNC[2/6]: 0x5607e0d381d0 in std::unexpected<pldm::fw_update::PackageParserError>::unexpected<pldm::fw_update::PackageParserError>(T&&) /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/16.1.1/../../../../include/c++/16.1.1/expected:209
     #1680338|       NEW    cov: 112 ft: 115 corp: 22/1124b lim: 1000 exec/s: 1680338 rss: 26Mb L: 213/213 MS: 4 CrossOver-InsertByte-ChangeBit-CMP- DE: "\204\003\342\014"-
     #2097152|       pulse  cov: 112 ft: 115 corp: 22/1124b lim: 1000 exec/s: 1048576 rss: 26Mb
    |       NEW_FUNC[1/3]: 0x5607e0d3f720 in decode_pldm_package_firmware_device_id_record_errno /home/alexander/upstreaming/libpldm/build-fuzz/../src/dsp/firmware_update.c:702
    |       NEW_FUNC[2/3]: 0x5607e0d534d0 in decode_pldm_package_firmware_device_id_record_from_iter /home/alexander/upstreaming/libpldm/build-fuzz/../src/dsp/firmware_update.c:3409
    ```
    
    Tested: the crc check override works when using static linking
    
    ```
    fuzz-19.log:    NEW_FUNC[1/10]: 0x560abeb88dc8 in pldm_edac_crc32_validate /home/alexander/up
    streaming/libpldm/build-fuzz/../tests/fuzz/fuzz_cpp.cpp:15
    ```
    
    Change-Id: I034506101f60fdbc3e9be6c4a75115d2615c401d
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ tests/fuzz/fuzz_cpp.cpp
@@ -0,0 +1,159 @@
+#include <libpldm/api.h>
+#include <libpldm/firmware_update.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <libpldm++/firmware_update.hpp>
+
+#include "array.h"
+
+extern "C" int pldm_edac_crc32_validate(uint32_t expected
+                                        __attribute__((unused)),
+                                        const void* data
+                                        __attribute__((unused)),
+                                        size_t size __attribute__((unused)))
+{
+    return 0;
+}
+
+static volatile uint64_t sink = 0;
+
+template <typename T>
+static void consumeCopy(T value)
+{
+    sink ^= static_cast<uint64_t>(value);
+}
+
+static void consume(const std::string& s)
+{
+    for (const char c : s)
+    {
+        consumeCopy(c);
+    }
+}
+
+static void consume(const variable_field& vf)
+{
+    for (size_t i = 0; i < vf.length; i++)
+    {
+        consumeCopy(vf.ptr[i]);
+    }
+}
+
+template <typename T>
+static void consume(const std::optional<T>& opt)
+{
+    if (opt.has_value())
+    {
+        consume(opt.value());
+    }
+}
+
+template <typename Range>
+static void consumeRange(const Range& range)
+{
+    for (const auto& item : range)
+    {
+        consumeCopy(item);
+    }
+}
+
+static void
+    consumePackage(const std::unique_ptr<pldm::fw_update::Package>& parsed)
+{
+    // attempt to read the returned struct to catch e.g. null pointers
+    // and broken non-owning objects.
+
+    for (const auto& fwdevidrecord : parsed->firmwareDeviceIdRecords)
+    {
+        consumeRange(fwdevidrecord.getDescriptorTypes());
+        consumeCopy(fwdevidrecord.deviceUpdateOptionFlags.to_ulong());
+        consumeRange(fwdevidrecord.applicableComponents);
+        consume(fwdevidrecord.componentImageSetVersionString);
+        for (const auto& [key, value] : fwdevidrecord.recordDescriptors)
+        {
+
+            consumeCopy(key);
+            const pldm::fw_update::DescriptorData* dd = value.get();
+            consume(dd->vendorDefinedDescriptorTitle);
+            consumeRange(dd->data);
+        }
+        consumeRange(fwdevidrecord.firmwareDevicePackageData);
+    }
+
+    for (const auto& comp : parsed->componentImageInformation)
+    {
+        consumeCopy(comp.componentClassification);
+        consumeCopy(comp.componentIdentifier);
+        consumeCopy(comp.compComparisonStamp);
+        consumeCopy(comp.componentOptions.to_ulong());
+        consumeCopy(comp.requestedComponentActivationMethod.to_ulong());
+        consume(comp.componentLocation);
+        consume(comp.componentVersion);
+    }
+}
+
+static int fuzz_package_parser_parse(const uint8_t* data, size_t size)
+{
+    static const uint8_t package_header_identifiers[][16] = {
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_0,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_1,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_2,
+        PLDM_PACKAGE_HEADER_IDENTIFIER_V1_3,
+    };
+
+    const uint8_t (*id)[16];
+    uint8_t* package;
+
+    if (size < sizeof(*id))
+    {
+        return -1;
+    }
+
+    package = (uint8_t*)calloc(size, 1);
+    if (!package)
+    {
+        return 0;
+    }
+
+    memcpy(package, data, size);
+
+    // the fuzzing data will choose which package header to use
+    const uint8_t id_idx = data[0] % ARRAY_SIZE(package_header_identifiers);
+
+    id = &package_header_identifiers[id_idx];
+
+    memcpy(package, package_header_identifiers[id_idx], sizeof(*id));
+
+    const std::span<uint8_t> p(package, size);
+
+    std::expected<std::unique_ptr<pldm::fw_update::Package>,
+                  pldm::fw_update::PackageParserError>
+        res = pldm::fw_update::PackageParser::parse(
+            p, pldm::fw_update::PackagePin::v1);
+
+    if (!res.has_value())
+    {
+        free(package);
+        return 0;
+    }
+
+    consumePackage(res.value());
+
+    free(package);
+    return 0;
+}
+
+static int (*const fuzz_tests[])(const uint8_t*, size_t) = {
+    fuzz_package_parser_parse,
+};
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    int rc = 0;
+    for (const auto& fuzz_test : fuzz_tests)
+    {
+        rc += fuzz_test(data, size);
+    }
+    return -rc == ARRAY_SIZE(fuzz_tests) ? -1 : 0;
+}
