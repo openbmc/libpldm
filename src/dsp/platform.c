a13cbd03c3f1fa6b47874955862f969f5074b81b
JY Voon <jvoon@nvidia.com>
2026-07-28 16:56:43 +0900

    platform: Stabilise SetStateSensorEnables APIs
    Mark the requester-side APIs as LIBPLDM_ABI_STABLE and drop their
    HAVE_LIBPLDM_API_TESTING guards:
    encode_pldm_platform_set_state_sensor_enables_req() and
    decode_pldm_platform_set_state_sensor_enables_resp().
    
    The APIs are consumed by the pldm change "platform-mc: enable and
    poll state sensors":
    https://gerrit.openbmc.org/c/openbmc/pldm/+/93308
    
    Change-Id: Ia0a0a4360d8d17c4e1496db58f79da31d14041f9
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- src/dsp/platform.c
+++ src/dsp/platform.c
@@ -2524,7 +2524,7 @@ int decode_pldm_platform_set_numeric_sensor_enable_resp(
 	return pldm_msgbuf_complete_consumed(buf);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int encode_pldm_platform_set_state_sensor_enables_req(
 	uint8_t instance_id,
 	const struct pldm_platform_set_state_sensor_enables_req *req,
@@ -2586,7 +2586,7 @@ int encode_pldm_platform_set_state_sensor_enables_req(
 	return pldm_msgbuf_complete_used(buf, *payload_length, payload_length);
 }
 
-LIBPLDM_ABI_TESTING
+LIBPLDM_ABI_STABLE
 int decode_pldm_platform_set_state_sensor_enables_resp(
 	const struct pldm_msg *msg, size_t payload_length,
 	struct pldm_platform_set_state_sensor_enables_resp *resp)
