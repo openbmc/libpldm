b469aac19024fa4cc333a5337e8c746e7e8e992e
JY Voon <jvoon@nvidia.com>
2026-07-30 18:44:10 +0900

    platform: Add SetStateSensorEnables req decoder
    Add the responder-side request decoder for SetStateSensorEnables
    (command 0x20) as defined in DSP0248 v1.3.0, replacing
    decode_set_state_sensor_enables_req() and its associated types.
    
    Change-Id: Ida23ed626c2afabf7ec16df8cb00a6486030e13b
    Signed-off-by: JY Voon <jvoon@nvidia.com>

--- tests/fuzz/fuzz.c
+++ tests/fuzz/fuzz.c
@@ -293,6 +293,17 @@ static int fuzz_decode_pldm_platform_set_state_sensor_enables_resp(
     return 0;
 }
 
+static int fuzz_decode_pldm_platform_set_state_sensor_enables_req(
+    const struct pldm_msg* msg, size_t payload_length)
+{
+    struct pldm_platform_set_state_sensor_enables_req req;
+
+    decode_pldm_platform_set_state_sensor_enables_req(msg, payload_length,
+                                                      &req);
+
+    return 0;
+}
+
 static int fuzz_decode_pldm_file_df_heartbeat_req(const struct pldm_msg* msg,
                                                   size_t payload_length)
 {
@@ -319,6 +330,7 @@ static int (*const decode_pldm_msg_tests[])(const struct pldm_msg*, size_t) = {
     fuzz_decode_pldm_base_get_pldm_types_resp,
     fuzz_decode_pldm_platform_set_numeric_sensor_enable_resp,
     fuzz_decode_pldm_platform_set_state_sensor_enables_resp,
+    fuzz_decode_pldm_platform_set_state_sensor_enables_req,
     fuzz_decode_pldm_file_df_heartbeat_req,
     fuzz_decode_pldm_base_multipart_receive_req,
 };
